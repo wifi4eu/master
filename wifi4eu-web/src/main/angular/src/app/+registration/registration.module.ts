705176f045ebcd683b57028867f5bee9f34b236e
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2017-01-26 21:12:11 +0100

    Add new import
    Add beneficiary component to module.

--- wifi4eu-web/src/main/angular/src/app/+registration/registration.module.ts
+++ wifi4eu-web/src/main/angular/src/app/+registration/registration.module.ts
@@ -3,13 +3,14 @@ import {SharedModule} from '../shared/shared.module';
 import {RegistrationRoutingModule} from './registration-routing.module';
 import {RegistrationComponent} from './registration.component';
 import {EntityComponent} from '../+entity/entity.component';
+import {BeneficiaryComponent} from '../+beneficiary/beneficiary.component';
 
 @NgModule({
   imports: [
     SharedModule, RegistrationRoutingModule
   ],
   declarations: [
-    RegistrationComponent, EntityComponent
+    RegistrationComponent, EntityComponent, BeneficiaryComponent
   ],
   bootstrap: [RegistrationComponent]
 })
