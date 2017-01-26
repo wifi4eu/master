4d3be7f6c5e72a2fa3401a9a9f9b81544d80d457
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2017-01-26 21:09:56 +0100

    Comment remote data access
    Comment remote calls to rely on local data to show progress.

--- wifi4eu-web/src/main/angular/src/app/+entity/entity.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+entity/entity.component.ts
@@ -1,34 +1,84 @@
 import {Component} from '@angular/core';
 import {EntityService} from './entity.service';
+import {CountryList} from './country-list.model';
+import {CountryDetails} from './country-details.model';
 
-@Component({selector: 'entity-component', template: require('./entity.component.html'), providers: [EntityService]})
+@Component({selector: 'entity-component', templateUrl: 'entity.component.html', providers: [EntityService]})
 export class EntityComponent {
 
   country : any;
   countries : any[];
-  suggestions : any[];
+  countrySuggestions : any[];
+
+  municipality : any;
+  municipalities : any[];
+  municipalitySuggestions : any[];
 
   constructor(private entityService : EntityService) {}
 
-  filterCountrySingle(event) {
+  filterCountry(event) {
     let query = event.query;
+    let countryList = new CountryList()
+    this.countrySuggestions = this.filterCountries(query, countryList.getAll());
+    // TODO - In a real application, make a request to a remote url with the query
+    // and return results, for demo we get it at client side.
+    /*
     this
       .entityService
       .getCountries()
       .subscribe(countries => {
         console.log(countries);
-        this.suggestions = this.filterCountry(query, countries);
+        this.suggestions = this.filterCountries(query, countries);
       });
+    */
   }
 
-  filterCountry(query, countries : any[]) : any[] {
-    // In a real application, make a request to a remote url with the query and
-    // return filtered results, for demo we filter at client side
-    let filtered : any[] = [];
+  filterCountries(query, countries : CountryDetails[]) : CountryDetails[] {
+    // TODO - In a real application, make a request to a remote url with the query
+    // and return filtered results, for demo we filter at client side.
+    let filtered : CountryDetails[] = [];
     for (let i = 0; i < countries.length; i++) {
       let country = countries[i];
       if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
         filtered.push(country);
+      }
+    }
+    return filtered;
+  }
+
+  filterMunicipality(event) {
+    let query = event.query;
+    let municipalityList = [
+      {
+        name: "Brussels"
+      }, {
+        name: "Flanders"
+      }, {
+        name: "Wallonia"
+      }
+    ];
+    this.municipalitySuggestions = this.filterMunicipalities(query, municipalityList);
+    // TODO - In a real application, make a request to a remote url with the query
+    // and return results, for demo we get it at client side.
+    /*
+    this
+      .entityService
+      .getCountries()
+      .subscribe(countries => {
+        console.log(countries);
+        this.suggestions = this.filterCountries(query, countries);
+      });
+    */
+  }
+
+  filterMunicipalities(query, municipalities : any[]) : any[] {
+    // TODO - In a real application, make a request to a remote url with the query
+    // and return filtered results, for demo we filter at client side.
+    let filtered : any[] = [];
+    for (let i = 0; i < municipalities.length; i++) {
+      let municipality = municipalities[i];
+      if (municipality.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(municipality);
       }
     }
     return filtered;
