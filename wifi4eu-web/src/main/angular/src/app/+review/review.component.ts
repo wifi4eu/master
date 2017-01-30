93406a2f778d6ac4fcd94025c3d3e672a0c2889d
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-01-30 15:19:30 +0100

    Add new component
    Add new component for review of the registration.

---
+++ wifi4eu-web/src/main/angular/src/app/+review/review.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({selector: 'review-component', templateUrl: 'review.component.html'})
+export class ReviewComponent {
+
+  treatment: any;
+  
+  constructor() {}
+}
