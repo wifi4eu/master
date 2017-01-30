1c6dbc432113a5b37e0c98afce0bd38ce52587ea
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-01-30 14:51:24 +0100

    Add new component
    Add new component for beneficiary registration.

--- wifi4eu-web/src/main/angular/src/app/+beneficiary/beneficiary.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+beneficiary/beneficiary.component.ts
@@ -2,5 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({selector: 'beneficiary-component', templateUrl: 'beneficiary.component.html'})
 export class BeneficiaryComponent {
+
+  treatment: any;
+  
   constructor() {}
 }
