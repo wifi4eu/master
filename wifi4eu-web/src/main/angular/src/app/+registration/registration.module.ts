c51a6ee53f0001ac0381b2c7ab54c3f45fba7740
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-01-31 11:18:39 +0100

    Add new declaration
    Add new declaration for successful registration component.

--- wifi4eu-web/src/main/angular/src/app/+registration/registration.module.ts
+++ wifi4eu-web/src/main/angular/src/app/+registration/registration.module.ts
@@ -5,13 +5,14 @@ import {RegistrationComponent} from './registration.component';
 import {EntityComponent} from '../+entity/entity.component';
 import {BeneficiaryComponent} from '../+beneficiary/beneficiary.component';
 import {ReviewComponent} from '../+review/review.component';
+import {SuccessRegistrationComponent} from './success.registration.component';
 
 @NgModule({
   imports: [
     SharedModule, RegistrationRoutingModule
   ],
   declarations: [
-    RegistrationComponent, EntityComponent, BeneficiaryComponent, ReviewComponent
+    RegistrationComponent, EntityComponent, BeneficiaryComponent, ReviewComponent, SuccessRegistrationComponent
   ],
   bootstrap: [RegistrationComponent]
 })
