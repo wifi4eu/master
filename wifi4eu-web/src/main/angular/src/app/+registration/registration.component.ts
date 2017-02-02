b1b4e61302e297fa489e2af48d92f1e245cf94e7
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-02 17:41:44 +0100

    Add two-way data binding for component
    Add two-way data binding for component, so child components can get data from the parent registration component.

--- wifi4eu-web/src/main/angular/src/app/+registration/registration.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+registration/registration.component.ts
@@ -1,4 +1,20 @@
 import {Component} from '@angular/core';
+import {EntityDetails} from '../+entity/entity-details.model';
+import {BeneficiaryDetails} from '../+beneficiary/beneficiary-details.model';
 
 @Component({templateUrl: 'registration.component.html'})
-export class RegistrationComponent {}
+ export class RegistrationComponent {
+    private entityDetails: EntityDetails;
+    private beneficiaryDetails : BeneficiaryDetails;
+    private completedSteps : boolean[];
+    private activeSteps : boolean[];
+    private currentStep : number;
+
+    constructor() {
+        this.entityDetails = new EntityDetails();
+        this.beneficiaryDetails = new BeneficiaryDetails();
+        this.completedSteps = [false, false, false];
+        this.activeSteps = [false, false, false];
+        this.currentStep = 1;
+    }
+ }
