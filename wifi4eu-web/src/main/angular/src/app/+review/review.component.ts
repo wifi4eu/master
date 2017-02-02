7180ea5d0bdc222f3315f00c9d2f405aa07298ee
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-02 17:34:05 +0100

    Add two-way data binding for component
    Add two-way data binding for component to desplay review details in final step.

--- wifi4eu-web/src/main/angular/src/app/+review/review.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+review/review.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {EntityDetails} from '../+entity/entity-details.model';
+import {BeneficiaryDetails} from '../+beneficiary/beneficiary-details.model';
 
 @Component({selector: 'review-component', templateUrl: 'review.component.html'})
 export class ReviewComponent {
+    @Input('entityDetails') entityDetails: EntityDetails;
+    @Input('beneficiaryDetails') beneficiaryDetails: BeneficiaryDetails;
 
-  treatment: any;
-  
-  constructor() {}
+    constructor() {
+    }
 }
