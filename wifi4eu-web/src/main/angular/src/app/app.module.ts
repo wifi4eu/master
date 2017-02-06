3ee20be96a465a391802ef438c99a43648a0b70f
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2017-02-06 17:46:28 +0100

    Add new module
    Add FormModule and formating.

--- wifi4eu-web/src/main/angular/src/app/app.module.ts
+++ wifi4eu-web/src/main/angular/src/app/app.module.ts
@@ -1,17 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Http } from '@angular/http';
-import { TranslateModule, TranslateLoader, TranslateStaticLoader, TranslateService } from 'ng2-translate/ng2-translate';
-
-import { UxService} from '@ec-digit-uxatec/eui-angular2-ux-commons';
-
-import { CoreModule } from './core/core.module';
-import { CoreService } from './core/core.service';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {Http} from "@angular/http";
+import {TranslateModule, TranslateLoader, TranslateStaticLoader} from "ng2-translate/ng2-translate";
+import {UxService} from "@ec-digit-uxatec/eui-angular2-ux-commons";
+import {CoreModule} from "./core/core.module";
+import {CoreService} from "./core/core.service";
+import {AppComponent} from "./app.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {HomeComponent} from "./home/home.component";
 
 export function translateFactory(http: Http) {
     return new TranslateStaticLoader(http, './assets/i18n', '.json');
@@ -30,7 +27,8 @@ export function translateFactory(http: Http) {
             useFactory: translateFactory,
             deps: [Http]
         }),
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule
     ],
     providers: [
         UxService,
