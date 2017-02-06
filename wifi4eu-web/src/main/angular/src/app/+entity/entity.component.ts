08b81a64df0204598a8e8b965f8bad126857abb8
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-06 17:46:30 +0100

    Add country validation
    Add country validation for entity component.

--- wifi4eu-web/src/main/angular/src/app/+entity/entity.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+entity/entity.component.ts
@@ -1,5 +1,5 @@
-import {Component,Input,Output,EventEmitter} from '@angular/core';
-import {Http, Response } from '@angular/http';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Http, Response} from '@angular/http';
 import {EntityService} from './entity.service';
 import {CountryList} from './country-list.model';
 import {EntityDetails} from './entity-details.model';
@@ -12,92 +12,107 @@ export class EntityComponent {
     @Input('completedSteps') completedSteps: boolean[];
     @Input('activeSteps') activeSteps: boolean[];
     @Input('currentStep') currentStep: number;
-    wrongDetails: string [];
+
     countrySuggestions: any[];
     municipalitySuggestions: any[];
-
-  country : CountryDetails;
-  countries : any[];
-
-  municipality : any;
-  municipalities : MunicipalityDetails[];
+    municipalities: MunicipalityDetails[];
     @Output() onNext = new EventEmitter<number>();
 
-  constructor(private entityService : EntityService,private http:Http) {}
+    constructor(private entityService: EntityService, private http: Http) {
+    }
 
     nextStep(step: number) {
-        this.onNext.emit(step);
+        this.validateInputs();
+        //this.onNext.emit(step);
     }
-  filterCountry(event) {
-    let query = event.query;
-    let countryList = new CountryList()
-    this.countrySuggestions = this.filterCountries(query, countryList.getAll());
-    // TODO - In a real application, make a request to a remote url with the query
-    // and return results, for demo we get it at client side.
-    /*
-    this
-      .entityService
-      .getCountries()
-      .subscribe(countries => {
-        console.log(countries);
-        this.suggestions = this.filterCountries(query, countries);
-      });
-    */
-  }
 
-  filterCountries(query, countries : CountryDetails[]) : CountryDetails[] {
-    // TODO - In a real application, make a request to a remote url with the query
-    // and return filtered results, for demo we filter at client side.
-    let filtered : CountryDetails[] = [];
-    for (let i = 0; i < countries.length; i++) {
-      let country = countries[i];
-      if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-        filtered.push(country);
-      }
-    }
-    return filtered;
-  }
-
-  getMunicipalities(query){
-    let that = this;
-    if(this.country != null){
-    this.entityService.getMunicipalities(this.country.code).subscribe(function(res){
-        that.municipalities = res;
-        if(that.municipalities != null){
-          that.municipalitySuggestions = that.filterMunicipalities(query,res);;
+    validateInputs() {
+        if (typeof this.entityDetails.country == "string") {
+            let countryString = JSON.stringify(this.entityDetails.country);
+            countryString = countryString.substring(1, countryString.length - 1);
+            countryString = countryString.toLowerCase();
+            countryString = countryString.charAt(0).toUpperCase() + countryString.slice(1);
+            let countryList = new CountryList().getAll();
+            for (let i = 0; i < countryList.length; i++) {
+                if (countryList[i].name === countryString) {
+                    this.entityDetails.country = countryList[i];
+                    break;
+                }
+            }
         }
-      }); 
     }
-  }
 
-  filterMunicipality(event) {
-    let query = event.query;
+    filterCountry(event) {
+        let query = event.query;
+        let countryList = new CountryList()
+        this.countrySuggestions = this.filterCountries(query, countryList.getAll());
+        // TODO - In a real application, make a request to a remote url with the query
+        // and return results, for demo we get it at client side.
+        /*
+         this
+         .entityService
+         .getCountries()
+         .subscribe(countries => {
+         console.log(countries);
+         this.suggestions = this.filterCountries(query, countries);
+         });
+         */
+    }
 
-/*    this.http.get('lau.json').map(function(res:Response){
-          municipalityList = res.data;        
-        });
-
-    this.municipalitySuggestions = this.filterMunicipalities(query, municipalityList);*/
-    // TODO - In a real application, make a request to a remote url with the query
-    // and return results, for demo we get it at client side.
-
-    this.getMunicipalities(query);
-
-  }
-
-  filterMunicipalities(query, municipalities : MunicipalityDetails[]) : MunicipalityDetails[] {
-    // TODO - In a real application, make a request to a remote url with the query
-    // and return filtered results, for demo we filter at client side.
-
-    let filtered : any[] = [];
-    for (let i = 0; i < municipalities.length; i++) {
-      let municipality = municipalities[i];
-      if (municipality.NAME_1 != null){
-        if(municipality.NAME_1.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-          filtered.push(municipality);
+    filterCountries(query, countries: CountryDetails[]): CountryDetails[] {
+        // TODO - In a real application, make a request to a remote url with the query
+        // and return filtered results, for demo we filter at client side.
+        let filtered: CountryDetails[] = [];
+        for (let i = 0; i < countries.length; i++) {
+            let country = countries[i];
+            if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+                filtered.push(country);
+            }
         }
-      }
+        return filtered;
     }
-    return filtered;
-  }
+
+    getMunicipalities(query) {
+        let that = this;
+        if (this.entityDetails.country != null) {
+            this.entityService.getMunicipalities(this.entityDetails.country.code).subscribe(function (res) {
+                that.municipalities = res;
+                if (that.municipalities != null) {
+                    that.municipalitySuggestions = that.filterMunicipalities(query, res);
+                    ;
+                }
+            });
+        }
+    }
+
+    filterMunicipality(event) {
+        let query = event.query;
+
+        /*    this.http.get('lau.json').map(function(res:Response){
+         municipalityList = res.data;
+         });
+
+         this.municipalitySuggestions = this.filterMunicipalities(query, municipalityList);*/
+        // TODO - In a real application, make a request to a remote url with the query
+        // and return results, for demo we get it at client side.
+
+        this.getMunicipalities(query);
+
+    }
+
+    filterMunicipalities(query, municipalities: MunicipalityDetails[]): MunicipalityDetails[] {
+        // TODO - In a real application, make a request to a remote url with the query
+        // and return filtered results, for demo we filter at client side.
+
+        let filtered: any[] = [];
+        for (let i = 0; i < municipalities.length; i++) {
+            let municipality = municipalities[i];
+            if (municipality.NAME_1 != null) {
+                if (municipality.NAME_1.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+                    filtered.push(municipality);
+                }
+            }
+        }
+        return filtered;
+    }
 }
