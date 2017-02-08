53feae4b59d9398b5002291a0fadef33e0736cc4
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-08 12:15:22 +0100

    Add primeNg dialog module.
    Add primeNg dialog module to list of shared imports/exports.

--- wifi4eu-web/src/main/angular/src/app/shared/shared.module.ts
+++ wifi4eu-web/src/main/angular/src/app/shared/shared.module.ts
@@ -6,45 +6,48 @@ import {UxModule, UX_DIRECTIVES} from '@ec-digit-uxatec/eui-angular2-ux-commons'
 import {UxLanguageSelectorComponent} from '@ec-digit-uxatec/eui-angular2-ux-language-selector';
 import {UxSearchInputComponent} from '@ec-digit-uxatec/eui-angular2-ux-search-input';
 
-import {CheckboxModule, GrowlModule, BlockUIModule, AutoCompleteModule} from 'primeng/primeng';
+import {CheckboxModule, GrowlModule, BlockUIModule, AutoCompleteModule, DialogModule} from 'primeng/primeng';
 
 import {APP_DIRECTIVES} from './components/index';
 
-export function httpFactory(backend : XHRBackend, defaultOptions : RequestOptions) {
-  return new UxHttp(backend, defaultOptions);
+export function httpFactory(backend: XHRBackend, defaultOptions: RequestOptions) {
+    return new UxHttp(backend, defaultOptions);
 }
 
 @NgModule({
-  imports: [
-    RouterModule,
-    UxModule,
-    CheckboxModule,
-    GrowlModule,
-    BlockUIModule,
-    HttpModule,
-    AutoCompleteModule
-  ],
-  declarations: [
-    UX_DIRECTIVES, UxLanguageSelectorComponent, UxSearchInputComponent, APP_DIRECTIVES
-  ],
-  exports: [
-    UX_DIRECTIVES,
-    UxLanguageSelectorComponent,
-    UxSearchInputComponent,
-    UxModule,
-    APP_DIRECTIVES,
-    CheckboxModule,
-    GrowlModule,
-    BlockUIModule,
-    HttpModule,
-    AutoCompleteModule
-  ],
-  providers: [
-    {
-      provide: Http,
-      useFactory: httpFactory,
-      deps: [XHRBackend, RequestOptions]
-    }
-  ]
+    imports: [
+        RouterModule,
+        UxModule,
+        CheckboxModule,
+        GrowlModule,
+        BlockUIModule,
+        HttpModule,
+        AutoCompleteModule,
+        DialogModule
+    ],
+    declarations: [
+        UX_DIRECTIVES, UxLanguageSelectorComponent, UxSearchInputComponent, APP_DIRECTIVES
+    ],
+    exports: [
+        UX_DIRECTIVES,
+        UxLanguageSelectorComponent,
+        UxSearchInputComponent,
+        UxModule,
+        APP_DIRECTIVES,
+        CheckboxModule,
+        GrowlModule,
+        BlockUIModule,
+        HttpModule,
+        AutoCompleteModule,
+        DialogModule
+    ],
+    providers: [
+        {
+            provide: Http,
+            useFactory: httpFactory,
+            deps: [XHRBackend, RequestOptions]
+        }
+    ]
 })
-export class SharedModule {}
+export class SharedModule {
+}
