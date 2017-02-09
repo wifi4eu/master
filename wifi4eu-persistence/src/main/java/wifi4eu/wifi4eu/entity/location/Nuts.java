0da0490d24005d4171fa48981fad23826b291ed4
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2017-02-09 22:51:35 +0100

    Init service objects

---
+++ wifi4eu-persistence/src/main/java/wifi4eu/wifi4eu/entity/location/Nuts.java
@@ -0,0 +1,7 @@
+package wifi4eu.wifi4eu.entity.location;
+
+/**
+ * Created by rgarcita on 08/02/2017.
+ */
+public class Nuts {
+}
---
+++ wifi4eu-service/src/main/java/wifi4eu/wifi4eu/service/security/UserService.java
@@ -0,0 +1,7 @@
+package wifi4eu.wifi4eu.service.security;
+
+/**
+ * Created by rgarcita on 09/02/2017.
+ */
+public class UserService {
+}
