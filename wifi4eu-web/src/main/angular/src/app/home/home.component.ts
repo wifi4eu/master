7a8038f3dc0839df76cc5b2de4b0e34705b8629c
Roger Fernandez Guri <rfguri@gmail.com>
2017-02-09 22:13:12 +0100

    Add temporal mock login service call
    Add timeout mocking login service call that redirects to the same
    page.

--- wifi4eu-web/src/main/angular/src/app/home/home.component.ts
+++ wifi4eu-web/src/main/angular/src/app/home/home.component.ts
@@ -1,4 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {UserDetails} from './user-details.module'
 
 @Component({selector: 'app-home', templateUrl: 'home.component.html'})
-export class HomeComponent {}
+export class HomeComponent {
+    @Input('userDetails') userDetails: UserDetails = new UserDetails();
+    displayConfirmingData: boolean = false;
+
+    onSubmit() {
+        let that = this;
+        this.displayConfirmingData = true;
+        setTimeout(function () {
+            that.displayConfirmingData = false;
+        }, 2000);
+    }
+}
---
+++ wifi4eu-web/src/main/angular/src/app/home/user-details.module.ts
@@ -0,0 +1,4 @@
+export class UserDetails {
+    public email: string;
+    public password: string;
+}
