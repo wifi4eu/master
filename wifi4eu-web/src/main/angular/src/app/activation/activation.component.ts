5a07453122508c2adfc91cc787b66b51065f30c6
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-02-14 17:09:05 +0100

    Change activation component
    Change activation component to standar.

--- wifi4eu-web/src/main/angular/src/app/activation/activation.component.ts
+++ wifi4eu-web/src/main/angular/src/app/activation/activation.component.ts
@@ -1,7 +1,29 @@
 import {Component, Input} from '@angular/core';
+import {ActivationDetails} from "./activation-details.model";
+import {print} from "util";
 
-
-@Component({selector: 'app-activation', templateUrl: 'activation.component.html'})
+@Component({templateUrl: 'activation.component.html'})
 export class ActivationComponent {
+    @Input('activationDetails') activationDetails: ActivationDetails=new ActivationDetails();
+
+    activationEmailMatches: boolean = false;
+
+    constructor(){
+    }
+
+    checkIfPasswordActivationMatches() {
+        if (this.activationDetails.newPsswd === this.activationDetails.newPsswd) {
+            this.activationEmailMatches = true;
+        } else {
+            this.activationEmailMatches = false;
+        }
+    }
+
+    onSubmit(){
+       console.log(this.activationDetails.email);
+        console.log(this.activationDetails.code);
+        console.log(this.activationDetails.psswd);
+        console.log(this.activationDetails.newPsswd);
+    }
 
 }
