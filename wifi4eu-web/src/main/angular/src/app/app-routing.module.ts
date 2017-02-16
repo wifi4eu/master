e7d6a2939e815ef28536f75b8a5dfa70ccc3be35
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-16 13:36:21 +0100

    Add voucher route
    Add voucher route that corresponds to the voucher component.

--- wifi4eu-web/src/main/angular/src/app/app-routing.module.ts
+++ wifi4eu-web/src/main/angular/src/app/app-routing.module.ts
@@ -1,24 +1,30 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {VoucherComponent} from './+voucher/voucher.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-      }, {
-        path: 'index.jsp',
-        redirectTo: 'home'
-      }, {
-        path: 'home',
-        component: HomeComponent
-      }, {
-        path: 'registration',
-        loadChildren: 'app/+registration/registration.module#RegistrationModule'
-      }
-    ],{ useHash: true })],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot([
+        {
+            path: '',
+            redirectTo: 'home',
+            pathMatch: 'full'
+        }, {
+            path: 'index.jsp',
+            redirectTo: 'home'
+        }, {
+            path: 'home',
+            component: HomeComponent
+        }, {
+            path: 'registration',
+            loadChildren: 'app/+registration/registration.module#RegistrationModule'
+        }, {
+            path: 'voucher',
+            component: VoucherComponent
+        }
+
+    ], {useHash: true})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
