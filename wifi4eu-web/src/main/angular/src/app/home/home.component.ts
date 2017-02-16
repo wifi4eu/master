04d7d454fd1e5fc4deab89fdf38c82252562726c
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2017-02-16 15:23:20 +0100

    Remove login markup

--- wifi4eu-web/src/main/angular/src/app/home/home.component.ts
+++ wifi4eu-web/src/main/angular/src/app/home/home.component.ts
@@ -1,16 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {UserDetails} from './user-details.module'
+import {Component} from "@angular/core";
 
 @Component({selector: 'app-home', templateUrl: 'home.component.html'})
 export class HomeComponent {
-    @Input('userDetails') userDetails: UserDetails = new UserDetails();
-    displayConfirmingData: boolean = false;
-
-    onSubmit() {
-        let that = this;
-        this.displayConfirmingData = true;
-        setTimeout(function () {
-            that.displayConfirmingData = false;
-        }, 2000);
-    }
 }
