7d600c9b2b75b2c49b9cc2e76e88a53ad0958cce
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-02-17 10:56:38 +0100

    Create bebefeciaryProfile archves

---
+++ wifi4eu-web/src/main/angular/src/app/beneficiaryProfile/beneficiaryProfile-details.model.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by lviverof on 16/02/2017.
+ */
+export class beneficiaryProfiledetails {
+
+
+}
---
+++ wifi4eu-web/src/main/angular/src/app/beneficiaryProfile/beneficiaryProfile.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by lviverof on 16/02/2017.
+ */
+import {Component, Input} from '@angular/core';
+import {beneficiaryProfiledetails} from "./beneficiaryProfile-details.model";
+import {UxService} from "@ec-digit-uxatec/eui-angular2-ux-commons";
+
+
+
+export class BeneficiaryProfileComponent{
+
+
+}
