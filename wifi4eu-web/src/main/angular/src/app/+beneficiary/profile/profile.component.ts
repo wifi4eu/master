f81b211ea686595ec5887e5360faac27a267fa23
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-02-20 17:24:52 +0100

    Add beneficiary profile screen

--- wifi4eu-web/src/main/angular/src/app/+beneficiary/profile/profile.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+beneficiary/profile/profile.component.ts
@@ -1,14 +1,34 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {BeneficiaryDetails} from "../beneficiary-details.model";
 
 @Component({
     moduleId: module.id,
     selector: 'beneficiary-profile-component',
     templateUrl: 'profile.component.html'
 })
-export class BeneficiaryProfileComponent implements OnInit {
+export class BeneficiaryProfileComponent {
+
+    @Input('beneficiaryDetails') beneficiaryDetails: BeneficiaryDetails;
+
     constructor() {
+        this.beneficiaryDetails = new BeneficiaryDetails();
+
+        console.log("Repeat new password:", this.beneficiaryDetails.repeatNewPassword);
     }
 
-    ngOnInit() {
+    display: boolean = false;
+
+    changingPassword() {
+        this.display = true;
     }
+
+    checkPassword() {
+        console.log("Checkihng password..");
+        console.log(this.beneficiaryDetails.newPassword);
+        console.log(this.beneficiaryDetails.repeatNewPassword);
+        console.log(this.beneficiaryDetails.newPassword === this.beneficiaryDetails.repeatNewPassword);
+        return this.beneficiaryDetails.newPassword === this.beneficiaryDetails.repeatNewPassword;
+
+    }
+
 }
