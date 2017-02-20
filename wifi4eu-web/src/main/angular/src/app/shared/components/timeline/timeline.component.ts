24a4c63d05362041db3a802cfdf54ef41b822c1d
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-20 12:22:11 +0100

    Add timeline functionality
    Add timeline component functionality and markup, with expand/hide
    button.

--- wifi4eu-web/src/main/angular/src/app/shared/components/timeline/timeline.component.ts
+++ wifi4eu-web/src/main/angular/src/app/shared/components/timeline/timeline.component.ts
@@ -2,4 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({selector: 'timeline-component', templateUrl: 'timeline.component.html'})
 export class TimelineComponent {
+    private expandHideString: string;
+    private expandHideClass: string;
+    private timelineVisible: boolean;
+
+    constructor() {
+        this.expandHideString = "Expand";
+        this.expandHideClass = "fa fa-2x fa-angle-down";
+        this.timelineVisible = false;
+    }
+
+    displayTimeline() {
+        this.timelineVisible = !this.timelineVisible;
+        if (this.timelineVisible) {
+            this.expandHideString = "Hide timeline";
+            this.expandHideClass = "fa fa-2x fa-angle-up";
+        } else {
+            this.expandHideString = "Expand";
+            this.expandHideClass = "fa fa-2x fa-angle-down";
+        }
+    }
 }
