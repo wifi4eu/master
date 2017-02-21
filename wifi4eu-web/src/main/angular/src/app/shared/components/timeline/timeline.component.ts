44feab7af85b8abcc973e6ceab98fd3ca2d4807a
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-21 11:53:03 +0100

    Add timelineElement class and variables
    Add timelineElement class and variables to work with ngFor in the
    markup.

--- wifi4eu-web/src/main/angular/src/app/shared/components/timeline/timeline.component.ts
+++ wifi4eu-web/src/main/angular/src/app/shared/components/timeline/timeline.component.ts
@@ -5,11 +5,18 @@ export class TimelineComponent {
     private expandHideString: string;
     private expandHideClass: string;
     private timelineVisible: boolean;
+    private timelineElements: TimelineElement[];
 
     constructor() {
         this.expandHideString = "Expand";
         this.expandHideClass = "fa fa-2x fa-angle-down";
         this.timelineVisible = false;
+        this.timelineElements = [
+            new TimelineElement("30/04/2017", "Opening", "Registration open for Mayors, Representative and Suppliers", "success"),
+            new TimelineElement("15/06/2017", "Closing", "Registration closed for Mayors, Representative and Suppliers", "success"),
+            new TimelineElement("30/09/2017", "Opening", "Opening the call", "info"),
+            new TimelineElement("30/09/2017", "Payment", "First round of payments", "")
+        ];
     }
 
     displayTimeline() {
@@ -21,5 +28,19 @@ export class TimelineComponent {
             this.expandHideString = "Expand";
             this.expandHideClass = "fa fa-2x fa-angle-down";
         }
+    }
+}
+
+export class TimelineElement {
+    private date: string;
+    private label: string;
+    private subLabel: string;
+    private styleClass: string;
+
+    constructor(date?: string, label?: string, subLabel?: string, styleClass?: string) {
+        this.date = date;
+        this.label = label;
+        this.subLabel = subLabel;
+        this.styleClass = styleClass;
     }
 }
