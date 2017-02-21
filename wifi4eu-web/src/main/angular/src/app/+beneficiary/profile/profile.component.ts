07c13f091cf6723a6c3819eaf01bb8823232dbf8
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-02-21 09:33:14 +0100

    Delete comments and change variable order

--- wifi4eu-web/src/main/angular/src/app/+beneficiary/profile/profile.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+beneficiary/profile/profile.component.ts
@@ -9,26 +9,18 @@ import {BeneficiaryDetails} from "../beneficiary-details.model";
 export class BeneficiaryProfileComponent {
 
     @Input('beneficiaryDetails') beneficiaryDetails: BeneficiaryDetails;
+    display: boolean = false;
 
     constructor() {
         this.beneficiaryDetails = new BeneficiaryDetails();
-
-        console.log("Repeat new password:", this.beneficiaryDetails.repeatNewPassword);
     }
-
-    display: boolean = false;
 
     changingPassword() {
         this.display = true;
     }
 
     checkPassword() {
-        console.log("Checkihng password..");
-        console.log(this.beneficiaryDetails.newPassword);
-        console.log(this.beneficiaryDetails.repeatNewPassword);
-        console.log(this.beneficiaryDetails.newPassword === this.beneficiaryDetails.repeatNewPassword);
         return this.beneficiaryDetails.newPassword === this.beneficiaryDetails.repeatNewPassword;
-
     }
 
 }
