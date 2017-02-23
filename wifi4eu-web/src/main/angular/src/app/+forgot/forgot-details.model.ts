c2351d41490fc9c64dad07d90c3470a7c54135d9
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2017-02-23 17:23:50 +0100

    Add token to request body

--- wifi4eu-web/src/main/angular/src/app/+forgot/forgot-details.model.ts
+++ wifi4eu-web/src/main/angular/src/app/+forgot/forgot-details.model.ts
@@ -1,10 +1,12 @@
 export class ForgotDetails {
     public email: string;
+    public token: string;
     public password: string;
     public confirmPassword: string;
 
-    public constructor(email?: string, password?: string, confirmPassword?: string) {
+    public constructor(email?: string, token?: string, password?: string, confirmPassword?: string) {
         this.email = email;
+        this.token = token;
         this.password = password;
         this.confirmPassword = confirmPassword;
     }
