0b0dbfe9429883be4e60f2d99a4706014e3a4522
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-02-27 16:36:48 +0100

    Add declaration of HelpdeskFormComponent
    Add HelpdeskFormComponent to list of import and declarations of
    registration module.

--- wifi4eu-web/src/main/angular/src/app/+beneficiary-registration/registration.module.ts
+++ wifi4eu-web/src/main/angular/src/app/+beneficiary-registration/registration.module.ts
@@ -8,6 +8,7 @@ import {ReviewComponent} from './+beneficiary-registration-step3/review.componen
 import {SuccessRegistrationComponent} from '../shared/components/success-registration/success.registration.component';
 import {SuccessComponent} from '../shared/components/success/success.component';
 import {FailureComponent} from '../shared/components/failure/failure.component';
+import {HelpdeskFormComponent} from '../shared/components/helpdesk-form/helpdesk-form.component';
 import {Ng2GoogleRecaptchaModule} from 'ng2-google-recaptcha';
 
 @NgModule({
@@ -15,7 +16,7 @@ import {Ng2GoogleRecaptchaModule} from 'ng2-google-recaptcha';
         SharedModule, RegistrationRoutingModule, Ng2GoogleRecaptchaModule
     ],
     declarations: [
-        RegistrationComponent, EntityComponent, BeneficiaryComponent, ReviewComponent, SuccessRegistrationComponent, SuccessComponent, FailureComponent
+        RegistrationComponent, EntityComponent, BeneficiaryComponent, ReviewComponent, SuccessRegistrationComponent, SuccessComponent, FailureComponent, HelpdeskFormComponent
     ],
     bootstrap: [RegistrationComponent]
 })
