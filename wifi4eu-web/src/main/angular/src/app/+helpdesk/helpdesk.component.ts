c98f8ee215f6548a73c3859311cc92ec8ded18dc
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-03-06 18:38:29 +0100

    Update component
    Update component to work with modified 'date' property.

--- wifi4eu-web/src/main/angular/src/app/+helpdesk/helpdesk.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+helpdesk/helpdesk.component.ts
@@ -15,14 +15,13 @@ export class HelpdeskComponent {
 
     constructor() {
         this.issues = [
-            new HelpdeskIssue("Supplier", "Password", "Austria", "12/12/2017", "00:00", "DG Connect", "Resolved", "john.smith@mail.com", "Hi, I can't login as a mayor into Beneficiary portal with my temporary password that you sent on my email address. Thanks.", "", ""),
-            new HelpdeskIssue("Supplier", "Login", "Austria", "12/12/2017", "00:00", "Member State", "Resolved", "doomguy@idsoftware.com", "Gonna get mine, get outta my way!", "it's gonna be", "gonna be"),
-            new HelpdeskIssue("Beneficiary", "Registration", "Austria", "12/12/2017", "00:00", "DG Connect", "Pending"),
-            new HelpdeskIssue("Member State", "Approval", "Austria", "12/12/2017", "00:00", "Member State", "Pending"),
-            new HelpdeskIssue("Beneficiary", "Login", "Austria", "12/12/2017", "00:00", "DG Connect", "Resolved"),
-            new HelpdeskIssue("Beneficiary", "Registration", "Austria", "12/12/2017", "00:00", "DG Connect", "Resolved"),
-            new HelpdeskIssue("Supplier", "Password", "Austria", "12/12/2017", "00:00", "DG Connect", "Pending"),
-            new HelpdeskIssue("Supplier", "Password", "Austria", "12/12/2017", "00:00", "Member State", "Pending")
+            new HelpdeskIssue("Supplier", "Password", "Austria", new Date(1488788967 * 1000), "DG Connect", "Resolved", "john.smith@mail.com", "Hi, I can't login as a mayor into Beneficiary portal with my temporary password that you sent on my email address. Thanks.", "", ""),
+            new HelpdeskIssue("Supplier", "Login", "Austria", new Date(1488788967 * 1000), "Member State", "Resolved", "doomguy@idsoftware.com", "Gonna get mine, get outta my way!", "it's gonna be", "gonna be"),
+            new HelpdeskIssue("Beneficiary", "Registration", "Austria", new Date(1488694526 * 1000), "DG Connect", "Pending"),
+            new HelpdeskIssue("Member State", "Approval", "Austria", new Date(1488694526 * 1000), "Member State", "Pending"),
+            new HelpdeskIssue("Beneficiary", "Login", "Austria", new Date(1510726655 * 1000), "DG Connect", "Resolved"),
+            new HelpdeskIssue("Beneficiary", "Registration", "Austria", new Date(1510726655 * 1000), "DG Connect", "Resolved"),
+            new HelpdeskIssue("Supplier", "Password", "Austria", new Date(793840955 * 1000), "DG Connect", "Pending")
         ];
         this.issueSelected = new HelpdeskIssue();
         this.display = false;
@@ -62,7 +61,7 @@ export class HelpdeskComponent {
     }
 
     makeIssueSelectedCopy(issue: HelpdeskIssue) {
-        this.issueSelectedOriginal = new HelpdeskIssue(issue.getPortal(), issue.getTopic(), issue.getMemberState(), issue.getDate(), issue.getTime(), issue.getAssignedTo(), issue.getStatus(), issue.getFrom(), issue.getIssueSummary(), issue.getMemberStateComments(), issue.getDgConnectComments());
+        this.issueSelectedOriginal = new HelpdeskIssue(issue.getPortal(), issue.getTopic(), issue.getMemberState(), issue.getDate(), issue.getAssignedTo(), issue.getStatus(), issue.getFrom(), issue.getIssueSummary(), issue.getMemberStateComments(), issue.getDgConnectComments());
     }
 }
 
