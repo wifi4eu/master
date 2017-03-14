8132cfc73b3c51cbe57e94a79082ca389ab04c34
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-03-14 13:27:07 +0100

    Update timeline component
    Quick modifications of timeline component and HTML.

--- wifi4eu-web/src/main/angular/src/app/shared/components/timeline/timeline.component.ts
+++ wifi4eu-web/src/main/angular/src/app/shared/components/timeline/timeline.component.ts
@@ -1,20 +1,32 @@
 import {Component, ViewChild} from '@angular/core';
-import {TimelineDTO,TimelineDTOBase} from '../../swagger/model/TimelineDTO';
+//import {TimelineDTO, TimelineDTOBase} from '../../swagger/model/TimelineDTO';
 import {CustomUxAccordionBoxComponent} from './custom-ux-accordion-box.component';
 import {TimelineApi} from '../../swagger/api/TimelineApi';
+import {TimelineElement} from "../../models/timeline-element.model";
 
-@Component({selector: 'timeline-component', templateUrl: 'timeline.component.html',providers:[TimelineApi]})
+@Component({selector: 'timeline-component', templateUrl: 'timeline.component.html', providers: [TimelineApi]})
 export class TimelineComponent {
     private expandHideString: string;
     private expandHideClass: string;
     private timelineVisible: boolean;
-    private timelineElements: TimelineDTO[];
+    private timelineElements: TimelineElement[];
 
-    constructor(private timelineApi:TimelineApi) {
+    constructor(private timelineApi: TimelineApi) {
         this.expandHideString = "Expand";
         this.expandHideClass = "fa fa-2x fa-angle-down";
         this.timelineVisible = false;
-       this.timelineApi.allTimelines().subscribe(elements => this.timelineElements = elements);
+        this.timelineElements = [new TimelineElement(), new TimelineElement()];
+        this.timelineElements[0].createTimelineForDgconn('Registration of Mayor and Supplier', '00:01', '01/01/2017', '31/12/2017', '23:59');
+        this.timelineElements[1].createTimelineForDgconn('Opening of the call', '20:01', '12/01/2017', '31/12/2017', '23:59');
+        // this.timelineElements[0].timelineId = 1;
+        // this.timelineElements[0].eventTitle = "Opening";
+        // this.timelineElements[0].startDate = new Date(1489489763000);
+        // this.timelineElements[0].endDate = new Date(1490353814000);
+        // this.timelineElements[1].timelineId = 2;
+        // this.timelineElements[1].eventTitle = "Closing";
+        // this.timelineElements[1].startDate = new Date(1490353814000);
+        // this.timelineElements[1].endDate = new Date(1490440214000);
+        // this.timelineApi.allTimelines().subscribe(elements => this.timelineElements = elements);
     }
 
     displayTimeline() {
