39b6aac4b3f0e7731079bc7a38be763e6231661f
Roger Fernandez Guri <rfguri@gmail.com>
2017-03-16 18:36:50 +0100

    Move profile into ear module
    Move profile to build after target folder is created.

--- wifi4eu-scripts/src/ear/upload.sh
+++ wifi4eu-scripts/src/ear/upload.sh
@@ -6,17 +6,17 @@ TARGET=${DIR}/../../../wifi4eu-ear/target
 EAR=${TARGET}/wifi4eu.ear
 
 if [ ! -f ${PEM} ]; then
-  echo "[INFO] Copying wifi4eu-test.pem into ~/.ssh directory .."
+  echo "[INFO] Copying wifi4eu-test.pem into ~/.ssh directory"
   cp ${DIR}/wifi4eu-test.pem ~/.ssh/
-  echo "[INFO] Adding rules to ~/.ssh/config .."
+  echo "[INFO] Adding rules to ~/.ssh/config"
   echo -e "Host *wifi4eu.everisdigitalchannels.com\nIdentityFile ~/.ssh/wifi4eu-test.pem\nUser ec2-user" >> ~/.ssh/config
 fi
 
 scp ${EAR} ec2-user@wifi4eu.everisdigitalchannels.com:~/ears >> ${DIR}/upload.log.tmp & PID=$!
 echo "[INFO] ------------------------------------------------------------------------"
-echo "[INFO] Uploading EAR to server..."
+echo "[INFO] Uploading EAR to server"
 echo "[INFO] ------------------------------------------------------------------------"
-echo "[INFO] This may take a while, please be patient while is running."
+echo "[INFO] This may take a while, please be patient while is running"
 printf "["
 while kill -0 $PID 2> /dev/null; do
     printf  "▓"
