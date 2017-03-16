e5a44e1d6bee0714418e84964be5e5f2275a739a
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-03-16 15:38:27 +0100

    Add component for supplier installation

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/installation/supplier-installation.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/installation/supplier-installation.component.ts
@@ -1,8 +1,8 @@
 import {Component} from "@angular/core";
 
-@Component({templateUrl: 'supplier-countdown.component.html'})
+@Component({templateUrl: 'supplier-installation.component.html'})
 
-export class CountDownSupplier {
+export class InstallationSupplier {
 
 
 }
