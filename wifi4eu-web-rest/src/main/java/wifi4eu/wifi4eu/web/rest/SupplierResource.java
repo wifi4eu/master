9a810a986b7f055f8165819aa6dd1883fa5e45c7
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-03-17 14:51:50 +0100

    Add web-rest files for Supplier pages
    Add web-rest files for both BeneficiarySupplierPublication and Supplier.

---
+++ wifi4eu-web-rest/src/main/java/wifi4eu/wifi4eu/web/rest/SupplierResource.java
@@ -0,0 +1,60 @@
+package wifi4eu.wifi4eu.web.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import wifi4eu.wifi4eu.common.dto.model.SupplierDTO;
+import wifi4eu.wifi4eu.common.dto.rest.ErrorDTO;
+import wifi4eu.wifi4eu.common.dto.rest.ResponseDTO;
+import wifi4eu.wifi4eu.service.supplier.SupplierService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@Api(value = "/supplier", description = "Supplier services")
+@RequestMapping("supplier")
+public class SupplierResource {
+
+    Logger _log = LoggerFactory.getLogger(SupplierResource.class);
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @ApiOperation(value = "Get all the suppliers")
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<SupplierDTO> allSuppliers() {
+        return supplierService.getAllSuppliers();
+    }
+
+    @ApiOperation(value = "Get supplier by supplierId")
+    @RequestMapping(value = "/{supplierId}", method = RequestMethod.GET, produces = "application/JSON")
+    @ResponseBody
+    public SupplierDTO getSupplier(@PathVariable("supplierId") final Long supplierId, final HttpServletResponse response) {
+        _log.info("getSupplier " + supplierId);
+
+        return supplierService.getSupplier(supplierId);
+
+    }
+
+    @ApiOperation(value = "create supplier")
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public ResponseDTO createSupplier(@RequestBody final SupplierDTO supplierDTO, final HttpServletResponse response) {
+        try {
+            _log.info("----> SupplierDTO: " + supplierDTO);
+            SupplierDTO resSupplier = supplierService.createSupplier(supplierDTO);
+            return new ResponseDTO(true, resSupplier, null);
+        } catch (Exception e) {
+            ErrorDTO errorDTO = new ErrorDTO(0, e.getMessage());
+            return new ResponseDTO(false, null, errorDTO);
+        }
+    }
+}
