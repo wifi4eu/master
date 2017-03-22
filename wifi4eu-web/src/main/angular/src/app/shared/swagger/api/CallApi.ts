8589429e9808196a96cbba26a38f5274cc8a9762
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-03-22 11:28:02 +0100

    Change basePath for the API to localhost

--- wifi4eu-web/src/main/angular/src/app/shared/swagger/api/CallApi.ts
+++ wifi4eu-web/src/main/angular/src/app/shared/swagger/api/CallApi.ts
@@ -51,7 +51,7 @@ export interface ICallApi {
 
 @Injectable()
 export class CallApi implements ICallApi {
-    protected basePath = 'http://wifi4eu.everisdigitalchannels.com:7001/wifi4eu/api';
+    protected basePath = 'http://localhost:7001/wifi4eu/api';
     public defaultHeaders: Headers = new Headers();
     public configuration: Configuration = new Configuration();
 
