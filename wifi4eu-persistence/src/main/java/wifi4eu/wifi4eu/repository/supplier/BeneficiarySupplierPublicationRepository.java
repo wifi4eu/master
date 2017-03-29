c2ac5df54babcd8be6042149b8a9587f1b291c80
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2017-03-29 17:22:41 +0200

    Changes to persist supplier registration

--- wifi4eu-persistence/src/main/java/wifi4eu/wifi4eu/repository/supplier/BeneficiarySupplierPublicationRepository.java
+++
@@ -1,9 +0,0 @@
-package wifi4eu.wifi4eu.repository.supplier;
-
-import org.springframework.data.repository.CrudRepository;
-import wifi4eu.wifi4eu.entity.supplier.BeneficiarySupplierPublication;
-
-import java.util.List;
-
-public interface BeneficiarySupplierPublicationRepository extends CrudRepository<BeneficiarySupplierPublication, Long> {
-}
---
+++ wifi4eu-service/src/main/java/wifi4eu/wifi4eu/service/beneficiary/BeneficiaryService.java
@@ -0,0 +1,7 @@
+package wifi4eu.wifi4eu.service.beneficiary;
+
+/**
+ * Created by rgarcita on 29/03/2017.
+ */
+public class BeneficiaryService {
+}
