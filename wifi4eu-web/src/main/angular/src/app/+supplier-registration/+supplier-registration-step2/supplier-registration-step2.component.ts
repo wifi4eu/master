3217ef6784a4e305eb0bcf13a7c1131dff0d8ae4
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2017-04-06 12:17:36 +0200

    Resolve bug with nuts0 go back in supplier registration

--- wifi4eu-web/src/main/angular/src/app/+supplier-registration/+supplier-registration-step2/supplier-registration-step2.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-registration/+supplier-registration-step2/supplier-registration-step2.component.ts
@@ -27,25 +27,46 @@ export class SupplierRegistrationComponentStep2 implements OnInit {
         this.onBack = new EventEmitter<number>();
 
         this.allCountries = [];
+        this.selectedCountries = [];
     }
 
     ngOnInit() {
         this.nutsApi.findNutsByLevel(0).subscribe(
             (nuts: NutsDTO[]) => {
                 for (let nut of nuts) {
-                    this.allCountries.push({
+                    let selectedItem = {
                         label: ' ' + nut.name,
                         value: nut
-                    });
+                    };
+                    this.allCountries.push(selectedItem);
+                    if(this.nuts0.some(function(e) {return e.name == nut.name;})){
+                        this.selectedCountries.push(selectedItem.value);
+                    }
                 }
             }
         );
+/*
+        for(let country of this.nuts0){
+            this.selectedCountries.push({
+                        label: ' ' + country.name,
+                        value: country
+                    });
+        }
+*/
         console.log(this.allCountries, this.nuts0, this.selectedCountries);
     }
 
     onMultiSelectChange(event) {
         if (event.value.length > 0) {
             let country: NutsDTO = event.value[event.value.length - 1];
+            //update this.nuts0 values with the selected countries in multiselect input
+            if(this.nuts0.length>0){
+                this.nuts0.splice(0,this.nuts0.length);
+            }
+            for(let country of event.value){
+                this.nuts0.push(country);
+            }
+            //retrieve nuts3 (Communes) level for selected nuts0 (Countries)
             this.nutsApi.findNutsByLevel(3).subscribe(
                 (nuts: NutsDTO[]) => {
                     this.provinces[country.name.toUpperCase()] = [];
