efcb9e38ed9550d5ff46760c378728c7053c761c
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-04-06 12:51:46 +0200

    Add app-guard logic depending on user type

--- wifi4eu-web/src/main/angular/src/app/app.guard.ts
+++ wifi4eu-web/src/main/angular/src/app/app.guard.ts
@@ -1,14 +1,72 @@
 import {Injectable} from '@angular/core';
-import {Router, CanActivate} from '@angular/router';
-//import { UserService } from './user.service';
+import {Router, CanActivate, ActivatedRouteSnapshot} from '@angular/router';
+import {UserDTO} from "./shared/swagger/model/UserDTO";
+import {LocalStorageService} from "angular-2-local-storage";
 
 @Injectable()
 export class AppGuard implements CanActivate {
-    constructor() {
+    private user: UserDTO;
+
+    constructor(private localStorage: LocalStorageService) {
+        let u = this.localStorage.get('user');
+        this.user = u ? JSON.parse(u.toString()) : null;
     }
 
-    canActivate() {
-        //return this.user.isLoggedIn();
-        return true;
+    canActivate(route: ActivatedRouteSnapshot) {
+        let allow = false;
+        switch (route.url[0].path) {
+            case "map":
+                allow = this.canActivateDgConn();
+                break;
+            case "beneficiary-portal":
+                allow = this.canActivateBeneficiary();
+                break;
+            case "helpdesk":
+                allow = this.canActivateMemberState() || this.canActivateDgConn();
+                break;
+            case "dgconn-portal":
+                allow = this.canActivateDgConn();
+                break;
+            case "supplier-portal":
+                allow = this.canActivateSupplier();
+                break;
+        }
+        return allow;
+    }
+
+    canActivateBeneficiary() {
+        console.log("ActivateBeneficiary");
+        console.log(this.user);
+        if (this.user === null) {
+            return false;
+        }
+        return (this.user.userType == 2 || this.user.userType == 3) ? true : false;
+    }
+
+    canActivateSupplier() {
+        console.log("ActivateSupplier");
+        console.log(this.user);
+        if (this.user === null) {
+            return false;
+        }
+        return (this.user.userType == 1) ? true : false;
+    }
+
+    canActivateMemberState() {
+        console.log("ActivateMemberState");
+        console.log(this.user);
+        if (this.user === null) {
+            return false;
+        }
+        return (this.user.userType == 4) ? true : false;
+    }
+
+    canActivateDgConn() {
+        console.log("ActivateDgConn");
+        console.log(this.user);
+        if (this.user === null) {
+            return false;
+        }
+        return (this.user.userType == 5) ? true : false;
     }
 }
