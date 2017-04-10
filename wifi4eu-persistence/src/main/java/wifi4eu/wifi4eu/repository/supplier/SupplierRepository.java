3ff55ab38af44276259c2246974a63c9f50a24d9
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-04-10 14:01:17 +0200

    Add 'findAllAwarded' mapper

--- wifi4eu-persistence/src/main/java/wifi4eu/wifi4eu/repository/supplier/SupplierRepository.java
+++ wifi4eu-persistence/src/main/java/wifi4eu/wifi4eu/repository/supplier/SupplierRepository.java
@@ -1,9 +1,12 @@
 package wifi4eu.wifi4eu.repository.supplier;
 
 import org.springframework.data.repository.CrudRepository;
+import wifi4eu.wifi4eu.common.dto.model.NutsDTO;
+import wifi4eu.wifi4eu.entity.location.Nuts;
 import wifi4eu.wifi4eu.entity.supplier.Supplier;
 
 import java.util.List;
 
 public interface SupplierRepository extends CrudRepository<Supplier, Long> {
+    List<NutsDTO> findAllAwarded();
 }
