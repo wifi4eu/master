a39b53949dd35a1e3b455e554186f3e5e5f11d28
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-04-10 11:40:55 +0200

    Initialize component within supplier portal

---
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'selected-by-municipality', templateUrl: 'selected-by-municipality.component.html', providers: []
+})
+export class SelectedByMunicipalityComponent {
+
+}
