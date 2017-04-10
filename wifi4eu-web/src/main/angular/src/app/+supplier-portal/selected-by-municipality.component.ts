9f034509568427ec2ffbcc256d704a249e54473c
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-04-10 16:06:22 +0200

    Add supplierApi.ts on constructor

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
@@ -1,17 +1,19 @@
 import {Component} from "@angular/core";
 import {UserDTO} from "../shared/swagger/model/UserDTO";
 import {LocalStorageService} from "angular-2-local-storage";
+import {SupplierApi} from "../shared/swagger/api/SupplierApi";
 
 @Component({
-    selector: 'selected-by-municipality', templateUrl: 'selected-by-municipality.component.html', providers: []
+    selector: 'selected-by-municipality',
+    templateUrl: 'selected-by-municipality.component.html',
+    providers: [SupplierApi]
 })
 export class SelectedByMunicipalityComponent {
     private user: UserDTO;
 
-    constructor(private localStorage: LocalStorageService) {
+    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi) {
         let u = this.localStorage.get('user');
         this.user = u ? JSON.parse(u.toString()) : null;
-
     }
 
 
