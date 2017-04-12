bbc0732f3dd1a1989803cd3afa98ea64ee5ee3b7
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-04-12 15:28:19 +0200

    Link data response with markup

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/awarded-municipalities.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/awarded-municipalities.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {LocalStorageService} from "angular-2-local-storage";
 import {UserDTO} from "../shared/swagger/model/UserDTO";
 import {SupplierApi} from "../shared/swagger/api/SupplierApi";
+import {LegalEntityDTO} from "../+beneficiary-registration/+beneficiary-registration-step3/legalEntityDTO.model";
 
 @Component({
     selector: 'awarded-municipalities',
@@ -10,9 +11,22 @@ import {SupplierApi} from "../shared/swagger/api/SupplierApi";
 })
 export class AwardedMunicipalitiesComponent {
     private user: UserDTO;
+    private municipalities: LegalEntityDTO[];
 
     constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi) {
         let u = this.localStorage.get('user');
         this.user = u ? JSON.parse(u.toString()) : null;
+
+
+        // this.supplierApi.getAwardedMunicipalities().subscribe(
+        //     data => {
+        //         this.municipalities = data;
+        //     },
+        //     error => {
+        //         console.log(error);
+        //     }
+        // );
+
+
     }
 }
