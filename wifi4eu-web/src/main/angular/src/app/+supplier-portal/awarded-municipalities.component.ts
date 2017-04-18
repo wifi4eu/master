a8be482c35aa554c8bff4d4fdc0e9c38d178690e
Roger Fernandez Guri <rfguri@gmail.com>
2017-04-18 17:43:36 +0200

    Fix deprecated angular 2 package and wrong api usage

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/awarded-municipalities.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/awarded-municipalities.component.ts
@@ -3,6 +3,7 @@ import {LocalStorageService} from "angular-2-local-storage";
 import {UserDTO} from "../shared/swagger/model/UserDTO";
 import {SupplierApi} from "../shared/swagger/api/SupplierApi";
 import {LegalEntityDTO} from "../+beneficiary-registration/+beneficiary-registration-step3/legalEntityDTO.model";
+import {BeneficiaryApi} from "../shared/swagger/api/BeneficiaryApi";
 
 @Component({
     selector: 'awarded-municipalities',
@@ -13,20 +14,18 @@ export class AwardedMunicipalitiesComponent {
     private user: UserDTO;
     private municipalities: LegalEntityDTO[];
 
-    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi) {
+    constructor(private localStorage: LocalStorageService, private beneficiaryApi: BeneficiaryApi) {
         let u = this.localStorage.get('user');
         this.user = u ? JSON.parse(u.toString()) : null;
 
-
-        this.supplierApi.getAwardedMunicipalities().subscribe(
-            data => {
+        this.beneficiaryApi.getAwardedMunicipalities().subscribe(
+            (data: LegalEntityDTO[]) => {
                 this.municipalities = data;
             },
             error => {
                 console.log(error);
             }
         );
-
 
     }
 }
