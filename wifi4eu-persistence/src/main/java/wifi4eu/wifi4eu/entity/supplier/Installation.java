bf006cef91e96d0d5ec594d2f55e07b2ad68bf0a
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-04-18 17:28:25 +0200

    Create new entity and DTO for 'Installation'.

---
+++ wifi4eu-persistence/src/main/java/wifi4eu/wifi4eu/entity/supplier/Installation.java
@@ -0,0 +1,113 @@
+package wifi4eu.wifi4eu.entity.supplier;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "SUPP_INSTALLATION_T")
+public class Installation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "INSTALLATION_ID")
+    private Long installationId;
+
+    @Column(name = "NIP")
+    private String nip;
+
+    @Column(name = "OUTDOOR_PRICE")
+    private Long outdoorPrice;
+
+    @Column(name = "INDOOR_PRICE")
+    private Long indoorPrice;
+
+    @Column(name = "ACCESS_POINT_NAME")
+    private String accessPointName;
+
+    @Column(name = "SERIAL_NUMBER")
+    private String serialNumber;
+
+    @Column(name = "PRODUCT_NAME")
+    private String productName;
+
+    @Column(name = "MODEL_NUMBER")
+    private String modelNumber;
+
+    public Installation() {
+    }
+
+    public Installation(Long installationId, String nip, Long outdoorPrice, Long indoorPrice, String accessPointName, String serialNumber, String productName, String modelNumber) {
+        this.installationId = installationId;
+        this.nip = nip;
+        this.outdoorPrice = outdoorPrice;
+        this.indoorPrice = indoorPrice;
+        this.accessPointName = accessPointName;
+        this.serialNumber = serialNumber;
+        this.productName = productName;
+        this.modelNumber = modelNumber;
+    }
+
+    public Long getInstallationId() {
+        return installationId;
+    }
+
+    public void setInstallationId(Long installationId) {
+        this.installationId = installationId;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public Long getOutdoorPrice() {
+        return outdoorPrice;
+    }
+
+    public void setOutdoorPrice(Long outdoorPrice) {
+        this.outdoorPrice = outdoorPrice;
+    }
+
+    public Long getIndoorPrice() {
+        return indoorPrice;
+    }
+
+    public void setIndoorPrice(Long indoorPrice) {
+        this.indoorPrice = indoorPrice;
+    }
+
+    public String getAccessPointName() {
+        return accessPointName;
+    }
+
+    public void setAccessPointName(String accessPointName) {
+        this.accessPointName = accessPointName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+}
