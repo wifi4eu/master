21b3df4d6b931b83b9584bcd3f66f5233532156b
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-04-25 13:57:23 +0200

    Update HTML markup for 'supplier-installation'.

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/installation/supplier-installation.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/installation/supplier-installation.component.ts
@@ -1,7 +1,30 @@
 import {Component} from "@angular/core";
+import {InstallationDTOBase} from "../../shared/swagger/model/InstallationDTO";
+import {SupplierApi} from "../../shared/swagger/api/SupplierApi";
+import {LocalStorageService} from "angular-2-local-storage";
 
-@Component({templateUrl: 'supplier-installation.component.html'})
+@Component({templateUrl: 'supplier-installation.component.html', providers: [SupplierApi]})
 
 export class SupplierInstallationComponent {
+    private installations: InstallationDTOBase[];
+    private selectedInstallations: InstallationDTOBase[];
+    private bidding: InstallationDTOBase;
+    private user;
+
+    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi) {
+        this.bidding = new InstallationDTOBase();
+        let u = this.localStorage.get('user');
+        this.user = u ? JSON.parse(u.toString()) : null;
+        if (this.user != null) {
+            this.supplierApi.getInstallationBySupplierId(this.user.userTypeId).subscribe(
+                installation => {
+                    this.bidding = installation;
+                },
+                error => {
+                    console.log(error);
+                }
+            );
+        }
+    }
 }
 
