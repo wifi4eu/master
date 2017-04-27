13e5e52b7aa1104543b886a4891246360eae87e2
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-04-27 17:25:38 +0200

    Update 'select supplier' component and markup
    Update 'select supplier' component ant HTML markup to be able to work
    with the backend services.

--- wifi4eu-web/src/main/angular/src/app/+beneficiary-portal/+voucher/select-supplier/select-supplier.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+beneficiary-portal/+voucher/select-supplier/select-supplier.component.ts
@@ -1,21 +1,29 @@
 import {Component} from "@angular/core";
 import {SupplierDTO, SupplierDTOBase} from "../../../shared/swagger/model/SupplierDTO";
 import {SupplierApi} from "../../../shared/swagger/api/SupplierApi";
+import {BeneficiaryApi} from "../../../shared/swagger/api/BeneficiaryApi";
+import {LocalStorageService} from "angular-2-local-storage";
 
-@Component({templateUrl: 'select-supplier.component.html', providers: [SupplierApi]})
+@Component({templateUrl: 'select-supplier.component.html', providers: [SupplierApi, BeneficiaryApi]})
 export class SelectSupplierComponent {
     private suppliers: SupplierDTOBase[];
     private selectedSuppliers: SupplierDTOBase[];
     private selectedSupplier: SupplierDTOBase;
     private display: boolean;
+    private publicationId;
+    private user;
 
-    constructor(private supplierApi: SupplierApi) {
+    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi, private beneficiaryApi: BeneficiaryApi) {
+        let u = this.localStorage.get('user');
+        this.user = u ? JSON.parse(u.toString()) : null;
+        // The Publication Call id is hardcoded now, but we should remove it later.
+        this.publicationId = 201;
+        this.selectedSupplier = new SupplierDTOBase();
         this.display = false;
         this.supplierApi.allSuppliers().subscribe(
             suppliers => this.suppliers = this.checkIfNull(suppliers),
             error => console.log(error)
         );
-        this.selectedSupplier = new SupplierDTOBase();
     }
 
     openModal() {
@@ -33,8 +41,21 @@ export class SelectSupplierComponent {
     }
 
     checkIfNull(suppliers: SupplierDTOBase[]) {
-
         return suppliers;
+    }
+
+    selectSupplier() {
+        if (this.user != null) {
+            this.beneficiaryApi.selectSupplier(this.user.userTypeId, this.publicationId, this.selectedSupplier.supplierId).subscribe(
+                data => {
+                    console.log(data);
+                    this.display = false;
+                }, error => {
+                    console.log(error);
+                    this.display = false;
+                }
+            );
+        }
     }
 
 }
