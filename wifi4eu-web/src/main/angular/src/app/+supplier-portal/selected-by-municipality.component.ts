20dd092d877742acde9f23bd5c3745618f7e64fa
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-05-09 11:16:37 +0200

    View button within supplier portal municipalities

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
@@ -3,29 +3,56 @@ import {UserDTO} from "../shared/swagger/model/UserDTO";
 import {LocalStorageService} from "angular-2-local-storage";
 import {SupplierApi} from "../shared/swagger/api/SupplierApi";
 import {LegalEntityDTO} from "../shared/swagger/model/LegalEntityDTO";
+import {BenPubSupDTO} from "../shared/swagger/model/BenPubSupDTO";
+import {BeneficiaryApi} from "../shared/swagger/api/BeneficiaryApi";
+import {CallApi} from "../shared/swagger/api/CallApi";
+import {CallDTO} from "../shared/swagger/model/CallDTO";
 
 @Component({
     selector: 'selected-by-municipality',
     templateUrl: 'selected-by-municipality.component.html',
-    providers: [SupplierApi]
+    providers: [SupplierApi, BeneficiaryApi, CallApi]
 })
 export class SelectedByMunicipalityComponent {
     private user: UserDTO;
     private municipalities: LegalEntityDTO[];
+    private currentCall: CallDTO;
+    private installationIds: number[];
 
-    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi) {
+    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi, private beneficiaryApi: BeneficiaryApi, private callApi: CallApi) {
+        this.installationIds = [];
         let u = this.localStorage.get('user');
         this.user = u ? JSON.parse(u.toString()) : null;
+
+        this.callApi.allCalls().subscribe(
+            calls => {
+                this.currentCall = calls[0];
+            }, error => {
+                console.log(error);
+                this.currentCall = null;
+            }
+        );
+
 
         if (this.user != null) {
             this.supplierApi.getSelectedMeBySupplierId(this.user.userTypeId).subscribe(
                 data => {
                     this.municipalities = data;
+                    for(let i = 0; i < this.municipalities.length; i++){
+                        this.beneficiaryApi.findByBeneficiaryIdAndPublicationId(this.municipalities[i].legalEntityId, this.currentCall.callId).subscribe(
+                            (result: BenPubSupDTO) => {
+                                this.installationIds[i] = result.benPubSubId;
+                            }, error =>{
+                                console.log(error);
+                            }
+                        );
+                    }
                 },
                 error => {
                     console.log(error);
                 }
             );
+
         } else {
             this.municipalities = [];
         }
