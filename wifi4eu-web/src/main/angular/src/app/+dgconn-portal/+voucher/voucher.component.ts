e3987c6a34c323028a2eb1682438992ebd1f8424
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-05-10 19:59:50 +0200

    Add voucher header and table

---
+++ wifi4eu-web/src/main/angular/src/app/+dgconn-portal/+voucher/voucher.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input, EventEmitter, OnInit, Output} from "@angular/core";
+import {DgConnDetails} from "../dgconnportal-details.model";
+import {BeneficiaryDTOBase} from "../../shared/swagger/model/BeneficiaryDTO";
+
+@Component({
+    templateUrl: 'voucher.component.html'
+})
+
+export class DgConnVoucherComponent {
+
+}
