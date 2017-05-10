4ef1ac3fbd681d1ac0e34ff02381622858bf5631
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-05-10 11:37:45 +0200

    Fix supplier portal tables display of codes
    Fix supplier portal tables from the list of municipalities, displaying
    the country and municipality code, instead of the specific name.

--- wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+supplier-portal/selected-by-municipality.component.ts
@@ -7,20 +7,29 @@ import {BenPubSupDTO} from "../shared/swagger/model/BenPubSupDTO";
 import {BeneficiaryApi} from "../shared/swagger/api/BeneficiaryApi";
 import {CallApi} from "../shared/swagger/api/CallApi";
 import {CallDTO} from "../shared/swagger/model/CallDTO";
+import {LauApi} from "../shared/swagger/api/LauApi";
+import {NutsApi} from "../shared/swagger/api/NutsApi";
+import {NutsDTOBase} from "../shared/swagger/model/NutsDTO";
+import {LauDTOBase} from "../shared/swagger/model/LauDTO";
 
 @Component({
     selector: 'selected-by-municipality',
     templateUrl: 'selected-by-municipality.component.html',
-    providers: [SupplierApi, BeneficiaryApi, CallApi]
+    providers: [SupplierApi, BeneficiaryApi, CallApi, NutsApi, LauApi]
 })
 export class SelectedByMunicipalityComponent {
     private user: UserDTO;
     private municipalities: LegalEntityDTO[];
     private currentCall: CallDTO;
     private installationIds: number[];
+    private countryNames: string[];
+    private municipalitiesNames: string[];
 
-    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi, private beneficiaryApi: BeneficiaryApi, private callApi: CallApi) {
+    constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi, private beneficiaryApi: BeneficiaryApi, private callApi: CallApi, private nutsApi: NutsApi, private lauApi: LauApi) {
         this.installationIds = [];
+        this.countryNames = [];
+        this.municipalitiesNames = [];
+
         let u = this.localStorage.get('user');
         this.user = u ? JSON.parse(u.toString()) : null;
 
@@ -33,17 +42,38 @@ export class SelectedByMunicipalityComponent {
             }
         );
 
-
         if (this.user != null) {
             this.supplierApi.getSelectedMeBySupplierId(this.user.userTypeId).subscribe(
                 data => {
                     this.municipalities = data;
-                    for(let i = 0; i < this.municipalities.length; i++){
+                    for (let i = 0; i < this.municipalities.length; i++) {
                         this.beneficiaryApi.findByBeneficiaryIdAndPublicationId(this.municipalities[i].legalEntityId, this.currentCall.callId).subscribe(
                             (result: BenPubSupDTO) => {
                                 this.installationIds[i] = result.benPubSubId;
-                            }, error =>{
+                            }, error => {
                                 console.log(error);
+                            }
+                        );
+                        this.nutsApi.findNutsByLevel(0).subscribe(
+                            (nuts: NutsDTOBase[]) => {
+                                for (let i = 0; i < nuts.length; i++) {
+                                    if (this.municipalities[i].countryCode == nuts[i].countryCode) {
+                                        this.countryNames.push(nuts[i].name);
+                                        break;
+                                    }
+                                }
+                            }, error => {
+                                console.log(error);
+                                this.countryNames.push("");
+                            }
+                        );
+                        this.lauApi.findLauByNuts3(this.municipalities[i].municipalityCode).subscribe(
+                            (result: LauDTOBase[]) => {
+                                this.municipalitiesNames.push(result[0].name1);
+                            },
+                            error => {
+                                console.log(error);
+                                this.municipalitiesNames.push("");
                             }
                         );
                     }
