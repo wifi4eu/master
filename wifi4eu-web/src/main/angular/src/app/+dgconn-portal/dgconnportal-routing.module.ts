c4e9d64feac99de75ca1edd1db1848ea282566e3
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-05-23 11:37:58 +0200

    Create a gallery component for graphical elements

--- wifi4eu-web/src/main/angular/src/app/+dgconn-portal/dgconnportal-routing.module.ts
+++ wifi4eu-web/src/main/angular/src/app/+dgconn-portal/dgconnportal-routing.module.ts
@@ -1,9 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
 import {DgConnPortalComponent} from "./dgconnportal.component";
 import {DgConnTimelineComponent} from "./+timeline/timeline.component";
 import {DgConnPublicationComponent} from "./+publication/publication.component";
 import {DgConnVoucherComponent} from "./+voucher/voucher.component";
+import {DgConnStatisticsComponent} from "./+statistics/statistics.component";
 
 @NgModule({
     imports: [RouterModule.forChild([
@@ -19,6 +20,9 @@ import {DgConnVoucherComponent} from "./+voucher/voucher.component";
         },{
             path: 'voucher',
             component: DgConnVoucherComponent
+        },{
+            path: 'statistics',
+            component: DgConnStatisticsComponent
         }
     ])],
     exports: [RouterModule]
