ef3a5c99eeedf2cfc50a0fc2982c38fb0a6c934b
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-05-23 15:52:37 +0200

    Add first report element

--- wifi4eu-web/src/main/angular/src/app/+dgconn-portal/dgconnportal-routing.module.ts
+++ wifi4eu-web/src/main/angular/src/app/+dgconn-portal/dgconnportal-routing.module.ts
@@ -1,9 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
 import {DgConnPortalComponent} from "./dgconnportal.component";
 import {DgConnTimelineComponent} from "./+timeline/timeline.component";
 import {DgConnPublicationComponent} from "./+publication/publication.component";
 import {DgConnVoucherComponent} from "./+voucher/voucher.component";
+import {DgConnFirstReportComponent} from "./+statistics/first-report/firstReport.component";
 
 @NgModule({
     imports: [RouterModule.forChild([
@@ -16,9 +17,12 @@ import {DgConnVoucherComponent} from "./+voucher/voucher.component";
         }, {
             path: 'publication',
             component: DgConnPublicationComponent
-        },{
+        }, {
             path: 'voucher',
             component: DgConnVoucherComponent
+        }, {
+            path: 'statistics/first-report',
+            component: DgConnFirstReportComponent
         }
     ])],
     exports: [RouterModule]
--- wifi4eu-web/src/main/angular/src/app/+dgconn-portal/dgconnportal.module.ts
+++ wifi4eu-web/src/main/angular/src/app/+dgconn-portal/dgconnportal.module.ts
@@ -5,13 +5,14 @@ import {DgConnPortalComponent} from "./dgconnportal.component";
 import {DgConnTimelineComponent} from "./+timeline/timeline.component";
 import {DgConnVoucherComponent} from "./+voucher/voucher.component";
 import {DgConnPublicationComponent} from "./+publication/publication.component";
+import {DgConnFirstReportComponent} from "./+statistics/first-report/firstReport.component";
 
 @NgModule({
     imports: [
         SharedModule, DgConnectPortalRoutingModule
     ],
     declarations: [
-        DgConnPortalComponent, DgConnTimelineComponent, DgConnPublicationComponent, DgConnVoucherComponent
+        DgConnPortalComponent, DgConnTimelineComponent, DgConnPublicationComponent, DgConnVoucherComponent, DgConnFirstReportComponent
     ],
     bootstrap: [DgConnPortalComponent]
 })
