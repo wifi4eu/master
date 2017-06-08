94d66d9123018ec2528d6dc05f1202933544d146
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-06-08 17:54:04 +0200

    Update beneficiary portal select supplier
    Update beneficiary portal select supplier screen to only show the
    suppliers of your country, not all of them. Also fixed the beneficiary
    profile to not be able to edit the email data of the user.

--- wifi4eu-web/src/main/angular/src/app/+beneficiary-portal/+voucher/select-supplier/select-supplier.component.ts
+++ wifi4eu-web/src/main/angular/src/app/+beneficiary-portal/+voucher/select-supplier/select-supplier.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input} from "@angular/core";
 import {UxService} from "@ec-digit-uxatec/eui-angular2-ux-commons";
+import {LegalEntityDTO, LegalEntityDTOBase} from "../../../shared/swagger/model/LegalEntityDTO";
 import {SupplierDTO, SupplierDTOBase} from "../../../shared/swagger/model/SupplierDTO";
+import {NutsDTO} from "../../../shared/swagger/model/NutsDTO";
 import {SupplierApi} from "../../../shared/swagger/api/SupplierApi";
 import {BeneficiaryApi} from "../../../shared/swagger/api/BeneficiaryApi";
 import {CallApi} from "../../../shared/swagger/api/CallApi";
@@ -9,21 +11,34 @@ import {LocalStorageService} from "angular-2-local-storage";
 @Component({selector: 'select-supplier-component', templateUrl: 'select-supplier.component.html', providers: [SupplierApi, BeneficiaryApi, CallApi]})
 export class SelectSupplierComponent {
     private suppliers: SupplierDTOBase[];
-    //private selectedSuppliers: SupplierDTOBase[];
     private selectedSupplier: SupplierDTOBase;
     private display: boolean;
     private user;
     @Input('publicationId') private publicationId;
-    @Input('municipalityId') private municipalityId;
+    @Input('myMunicipality') private myMunicipality;
+    //@Input('municipalityId') private municipalityId;
 
     constructor(private localStorage: LocalStorageService, private supplierApi: SupplierApi, private beneficiaryApi: BeneficiaryApi, private callApi: CallApi, private uxService: UxService) {
         let u = this.localStorage.get('user');
         this.user = u ? JSON.parse(u.toString()) : null;
+        this.suppliers = [];
         this.selectedSupplier = new SupplierDTOBase();
         this.display = false;
         this.supplierApi.allSuppliers().subscribe(
-            suppliers => this.suppliers = suppliers,
-            error => console.log(error)
+            (suppliers: SupplierDTOBase[]) => {
+                for (let i = 0; i < suppliers.length; i++) {
+                    let countriesPart = suppliers[i].nutsIds.split(";");
+                    for (let f = 0; f < countriesPart.length; f++) {
+                        let regionPart = countriesPart[f].split(',');
+                        for (let g = 0; g < regionPart.length; g++) {
+                            if (regionPart[g] == this.myMunicipality.countryCode) {
+                                this.suppliers.push(suppliers[i]);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }, error => console.log(error)
         );
     }
 
@@ -45,7 +60,7 @@ export class SelectSupplierComponent {
             });
         } else {
             if (this.user != null) {
-                this.beneficiaryApi.selectSupplier(this.municipalityId, this.publicationId, this.selectedSupplier.supplierId).subscribe(
+                this.beneficiaryApi.selectSupplier(this.myMunicipality.legalEntityId, this.publicationId, this.selectedSupplier.supplierId).subscribe(
                     data => {
                         this.uxService.growl({
                             severity: 'success',
