c4e087f69984896d25a865df99a78f0550e4084c
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2017-11-02 16:41:57 +0100

    Add DGConn Portal
    Add Timeline, Publication, Statistics and Voucher html components.

---
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+dgconn-portal/+publication/publication.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: 'publication.component.html'
+})
+
+export class DgConnPublicationComponent {
+
+}
---
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+dgconn-portal/dgconnportal.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from "@angular/core";
+import {SharedModule} from "../shared/shared.module";
+import {DgConnectPortalRoutingModule} from "./dgconnportal-routing.module";
+import {DgConnPortalComponent} from "./dgconnportal.component";
+import {DgConnTimelineComponent} from "./+timeline/timeline.component";
+import {DgConnPublicationComponent} from "./+publication/publication.component";
+import {DgConnVoucherComponent} from "app/+dgconn-portal/+voucher/voucher.component";
+import {DgConnStatisticsComponent} from "./+statistics/statistics.component";
+
+@NgModule({
+    imports: [
+        SharedModule, DgConnectPortalRoutingModule
+    ],
+    declarations: [
+        DgConnPortalComponent, DgConnTimelineComponent, DgConnPublicationComponent, DgConnVoucherComponent, DgConnStatisticsComponent
+    ],
+    bootstrap: [DgConnPortalComponent]
+})
+export class DgConnPortalModule {
+}
