a78bcd90c910b5ce92e9c5bac293aef5cb46a7d2
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2017-11-10 21:01:06 +0100

    Enable functionalities in DGConn Portal and Helpdesk

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/app.module.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/app.module.ts
@@ -15,7 +15,7 @@ import {ActivationComponent} from "./+activation/activation.component";
 import {LoginComponent} from "./+login/login.component";
 // import {ForgotComponent} from "./+forgot/forgot.component";
 import {Ng2Bs3ModalModule} from "ng2-bs3-modal/ng2-bs3-modal";
-// import {HelpdeskComponent} from "./+helpdesk/helpdesk.component";
+import {HelpdeskComponent} from "./+helpdesk/helpdesk.component";
 import {NotFoundComponent} from "./not-found/not-found.component"
 // import {AppGuard} from "./app.guard";
 import {LocalStorageModule} from 'angular-2-local-storage';
@@ -36,7 +36,7 @@ export function translateFactory(http: Http) {
         ActivationComponent,
         LoginComponent,
         // ForgotComponent,
-        // HelpdeskComponent,
+        HelpdeskComponent,
         // AbacComponent,
         // EcasComponent,
         NotFoundComponent
@@ -46,7 +46,7 @@ export function translateFactory(http: Http) {
         ActivationComponent,
         LoginComponent,
         // ForgotComponent,
-        // HelpdeskComponent,
+        HelpdeskComponent,
         // AbacComponent,
         // EcasComponent,
         NotFoundComponent
