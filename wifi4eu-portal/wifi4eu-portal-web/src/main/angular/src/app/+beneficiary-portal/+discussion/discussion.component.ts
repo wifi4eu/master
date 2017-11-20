7722a975077500158892548fcd204298427b8da6
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2017-11-20 18:19:56 +0100

    Add delete application & ask for mediation service

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+discussion/discussion.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+discussion/discussion.component.ts
@@ -9,7 +9,7 @@ import {RegistrationApi} from "../../shared/swagger/api/RegistrationApi";
 import {RegistrationDTOBase} from "../../shared/swagger/model/RegistrationDTO";
 import {ThreadMessageDTOBase} from "../../shared/swagger/model/ThreadMessageDTO";
 import {ThreadmessagesApi} from "../../shared/swagger/api/ThreadmessagesApi";
-import {ResponseDTO} from "../../shared/swagger/model/ResponseDTO";
+import {ResponseDTOBase} from "../../shared/swagger/model/ResponseDTO";
 import {UxService} from "@ec-digit-uxatec/eui-angular2-ux-commons/dist/shared/ux.service";
 import {LocalStorageService} from "angular-2-local-storage";
 import {TranslateService} from "ng2-translate";
@@ -102,19 +102,19 @@ export class DiscussionComponent {
         }
     }
 
-    newMessage() {
+    private newMessage() {
         this.displayMessage = true;
         this.message = "";
     }
 
-    sendMessage() {
+    private sendMessage() {
         let newMessage = new ThreadMessageDTOBase();
         newMessage.createDate = new Date().getTime();
         newMessage.message = this.message;
         newMessage.authorId = this.user.id;
         newMessage.threadId = this.thread.id;
         this.threadMessagesApi.createThreadMessage(newMessage).subscribe(
-            (response: ResponseDTO) => {
+            (response: ResponseDTOBase) => {
                 if (response.success) {
                     this.thread.messages.push(response.data);
                     this.uxService.growl({
@@ -143,22 +143,58 @@ export class DiscussionComponent {
     }
 
 
-    closeModal() {
+    private closeModal() {
         this.displayMessage = false;
         this.displayMediation = false;
     }
 
-    newMediation() {
-        this.displayMediation = true;
-    }
+    // newMediation() {
+    //     this.displayMediation = true;
+    // }
 
-    askMediation() {
+    private askMediation() {
         this.mediationButton = true;
         this.showAlert = true;
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
+        this.registrationApi.getRegistrationsByUserId(this.user.id).subscribe(
+            (registrations: RegistrationDTOBase[]) => {
+                for (let registration of registrations) {
+                    registration.status = 0;
+                    this.registrationApi.createRegistration(registration).subscribe(
+                        (data: ResponseDTOBase) => {
+                            if (data.success) {
+                                let translatedString = 'Your request for mediation has been submited successfully. WIFI4EU mediation service will soon intervene in this conversation.';
+                                this.translateService.get('discussion.growl').subscribe(
+                                    (translation: string) => {
+                                        translatedString = translation;
+                                    }
+                                );
+                                this.uxService.growl({
+                                    severity: 'success',
+                                    summary: 'SUCCESS',
+                                    detail: translatedString
+                                });
+                            }
+                        }, error => {
+                            let translatedString = 'Your request for mediation could not be submited due to an error. Please, try again later.';
+                            this.translateService.get('discussion.growl.error').subscribe(
+                                (translation: string) => {
+                                    translatedString = translation;
+                                }
+                            );
+                            this.uxService.growl({
+                                severity: 'error',
+                                summary: 'ERROR',
+                                detail: translatedString
+                            });
+                        }
+                    );
+                }
+            }
+        );
     }
 
-    displayAuthorData(authorId: number, field: string) {
+    private displayAuthorData(authorId: number, field: string) {
         for (let user of this.users) {
             if (user.id = authorId) {
                 return user[field];
@@ -166,4 +202,46 @@ export class DiscussionComponent {
         }
     }
 
+    private deleteApplication() {
+        this.registrationApi.getRegistrationsByUserId(this.user.id).subscribe(
+            (registrations: RegistrationDTOBase[]) => {
+                let registrationCount = 0;
+                for (let registration of registrations) {
+                    registration.status = 1;
+                    this.registrationApi.createRegistration(registration).subscribe(
+                        (data: ResponseDTOBase) => {
+                            if (data.success) {
+                                registrationCount++;
+                                if (registrationCount >= registrations.length) {
+                                    let translatedString = 'Your applications were succesfully deleted.';
+                                    this.translateService.get('beneficiary.deleteApplication.Success').subscribe(
+                                        (translation: string) => {
+                                            translatedString = translation;
+                                        }
+                                    );
+                                    this.uxService.growl({
+                                        severity: 'success',
+                                        summary: 'SUCCESS',
+                                        detail: translatedString
+                                    });
+                                }
+                            }
+                        }, error => {
+                            let translatedString = 'Your applications were succesfully deleted.';
+                            this.translateService.get('beneficiary.deleteApplication.Failure').subscribe(
+                                (translation: string) => {
+                                    translatedString = translation;
+                                }
+                            );
+                            this.uxService.growl({
+                                severity: 'error',
+                                summary: 'ERROR',
+                                detail: translatedString
+                            });
+                        }
+                    );
+                }
+            }
+        );
+    }
 }
