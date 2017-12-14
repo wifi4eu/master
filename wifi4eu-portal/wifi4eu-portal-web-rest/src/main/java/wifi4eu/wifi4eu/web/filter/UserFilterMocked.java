5f3b9f24cc0aed3376b697554519a22c56dc318d
dsebastm <ramon.david.sebastian.manjon@everis.com>
2017-12-14 20:28:11 +0100

    Added profiles to deploy in environment with/without ECAS

--- wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/filter/UserFilterMocked.java
+++ wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/filter/UserFilterMocked.java
@@ -2,9 +2,7 @@ package wifi4eu.wifi4eu.web.filter;
 
 
 import org.springframework.web.filter.OncePerRequestFilter;
-import wifi4eu.wifi4eu.common.Constant;
 import wifi4eu.wifi4eu.common.dto.security.RoleDTO;
-import wifi4eu.wifi4eu.common.ecas.UserHolder;
 import wifi4eu.wifi4eu.common.exception.AppException;
 import wifi4eu.wifi4eu.common.security.UserContext;
 
@@ -35,29 +33,16 @@ public class UserFilterMocked extends OncePerRequestFilter {
             user.setEmail("tester@test.com");
             user.setDomain("TemporalDom");
             user.setPerId(1L);
-            user.setDetailedUser( UserHolder.getDetailedUser() );
+            user.setDetailedUser( null );
             user.setFirstName("Tester Name");
             user.setLastName("Test LastName");
 
             user.setRoleList(new LinkedList<RoleDTO>());
 
-            /*
-            String userId = (String) request.getSession(true).getAttribute(Constant.USER);;
-            UserContext user = new UserContext(userId);
-            */
-
-            if (user == null) {
-                user = UserHolder.getUser();
-                request.getSession().setAttribute(Constant.USER, user);
-            }
-
-            UserHolder.setUser(user);
             filterChain.doFilter(request, response);
 
         } catch (Exception e) {
             throw new AppException(e);
-        } finally {
-            UserHolder.clearUser();
         }
     }
 }
