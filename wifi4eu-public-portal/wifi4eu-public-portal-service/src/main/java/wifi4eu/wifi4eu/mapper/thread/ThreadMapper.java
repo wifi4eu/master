515f106abdd3bdf90abd71cb721595405db9a555
Lucia Vivero Ferreirua <lviverof@everis.com>
2017-12-21 18:46:10 +0100

    Fix develop bugs after merge conflict
    WIFIFOREU-1352

--- wifi4eu-public-portal/wifi4eu-public-portal-service/src/main/java/wifi4eu/wifi4eu/mapper/thread/ThreadMapper.java
+++ wifi4eu-public-portal/wifi4eu-public-portal-service/src/main/java/wifi4eu/wifi4eu/mapper/thread/ThreadMapper.java
@@ -12,20 +12,26 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ThreadMapper {
-    @Mapping(source = "entity.lau.id", target = "lauId")
+    //    @Mapping(source = "entity.lau.id", target = "lauId")
     ThreadDTO toDTO(Thread entity);
-    @Mapping(source = "vo.lauId", target = "lau.id")
+
+    //    @Mapping(source = "vo.lauId", target = "lau.id")
     Thread toEntity(ThreadDTO vo);
+
     @Mappings({
             @Mapping(source = "entity.thread.id", target = "threadId"),
             @Mapping(source = "entity.author.id", target = "authorId")
     })
     ThreadMessageDTO toDTO(ThreadMessage entity);
+
     @Mappings({
             @Mapping(source = "vo.threadId", target = "thread.id"),
             @Mapping(source = "vo.authorId", target = "author.id")
     })
     ThreadMessage toEntity(ThreadMessageDTO vo);
+
     List<ThreadDTO> toDTOList(List<Thread> list);
+
     List<Thread> toEntityList(List<ThreadDTO> list);
 }
+
