e064ab931a843f0c4b1ea5122bacd224281c7beb
Xavier Pandis Garcia <xavier.pandis.garcia@everis.com>
2018-01-10 17:20:23 +0100

    fix step 1 beneficiary registration

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/beneficiary-registration/+beneficiary-registration-step1/beneficiary-registration-step1.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/beneficiary-registration/+beneficiary-registration-step1/beneficiary-registration-step1.component.ts
@@ -79,6 +79,7 @@ export class BeneficiaryRegistrationStep1Component {
       this.organizationSelected = true;
       if(this.organization == "municipality"){
         this.chooseSingleMunicipality(true);
+        this.organizationChange.emit(this.organization);
       }
       else{
         this.chooseMultipleMunicipality(true);
