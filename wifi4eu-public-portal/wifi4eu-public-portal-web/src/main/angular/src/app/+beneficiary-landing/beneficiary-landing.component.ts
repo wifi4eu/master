0e8ac61e2507a07f935f1d86580c433b54417c74
Xavier Pandis Garcia <xavier.pandis.garcia@everis.com>
2018-01-16 12:22:06 +0100

    buttons aligned in beneficiary and supplier landing

--- wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/app/+beneficiary-landing/beneficiary-landing.component.ts
+++ wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/app/+beneficiary-landing/beneficiary-landing.component.ts
@@ -2,7 +2,14 @@ import {Component} from "@angular/core";
 import {SharedModule} from "../shared/shared.module";
 
 @Component({
-    selector: 'beneficiary-landing', templateUrl: 'beneficiary-landing.component.html'
+    selector: 'beneficiary-landing', templateUrl: 'beneficiary-landing.component.html', 
+    styles: [`
+      @media (max-width: 768px) {
+        button.cancel-button, button.publish-button{
+          width: 100%;
+        }
+      }
+    `]
 })
 
 export class BeneficiaryLandingComponent {
--- wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/app/+supplier-landing/supplier-landing.component.ts
+++ wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/app/+supplier-landing/supplier-landing.component.ts
@@ -2,7 +2,14 @@ import {Component} from "@angular/core";
 import {SharedModule} from "../shared/shared.module";
 
 @Component({
-    selector: 'supplier-landing', templateUrl: 'supplier-landing.component.html'
+    selector: 'supplier-landing', templateUrl: 'supplier-landing.component.html', 
+    styles: [`
+      @media (max-width: 768px) {
+        button.cancel-button, button.publish-button{
+          width: 100%;
+        }
+      }
+    `]
 })
 
 export class SupplierLandingComponent {
