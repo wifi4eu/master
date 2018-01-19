f58ecaaeb14479b7d41c110d8f171344c8b60a62
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2018-01-19 06:45:19 +0100

    fix label

---
+++ wifi4eu-scripts/src/translations master file/translations/copy_to_i18.sh
@@ -0,0 +1,4 @@
+rm ../../../../wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/assets/i18n/*.json
+rm ../../../../wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/assets/i18n/*.json
+cp ./*.json ../../../../wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/assets/i18n
+cp ./*.json ../../../../wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/assets/i18n
