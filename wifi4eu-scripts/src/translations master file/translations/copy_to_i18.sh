9546bfb870fbb11c54ad4540fd1797bc7223309d
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2018-01-28 09:46:08 +0100

    move script to a different folder

--- wifi4eu-scripts/src/translations master file/translations/copy_to_i18.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm ../../../../wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/assets/i18n/*.json
-rm ../../../../wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/assets/i18n/*.json
-rm ../../../../wifi4eu-scripts/src/i18n/en.json
-
-cp ./*.json ../../../../wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/assets/i18n
-cp ./*.json ../../../../wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/assets/i18n
-cp ./en.json ../../../../wifi4eu-scripts/src/i18n/en.json
