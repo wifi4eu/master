0d8414357448f807b2811708332d57ce4c4a7d45
Lucía Vivero <lucia.vivero.ferreirua@everis.com>
2018-02-21 12:04:27 +0100

    add voucher application automate

--- wifi4eu-test/protractor.conf.js
+++ wifi4eu-test/protractor.conf.js
@@ -1,12 +1,15 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/single-beneficiary.e2e-spec.ts'
+    // './e2e/single-beneficiary.e2e-spec.ts',
+    // './e2e/supplier-registration.e2e-spec.ts',
+    // './e2e/multiple-beneficiary.e2e-spec.ts',
+    './e2e/voucher-application.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
@@ -16,13 +19,14 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+    defaultTimeoutInterval: 2500000,
+    print: function () {
+    }
   },
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
   }
 };
