6b9705d65b51a190d5199b46f4b341921d112373
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2018-03-04 13:39:15 +0100

    Fix bugs in lau files and update master file

---
+++ wifi4eu-scripts/src/laus/EE.sql
@@ -0,0 +1,87 @@
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE01','1','no',N'Alutaguse vald',N'ALUTAGUSE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE02','2','no',N'Anija vald',N'ANIJA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE03','3','no',N'Antsla vald',N'ANTSLA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE04','4','no',N'Elva vald',N'ELVA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE05','5','no',N'Häädemeeste vald',N'HÄÄDEMEESTE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE06','6','no',N'Haapsalu linn',N'HAAPSALU LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE07','7','no',N'Haljala vald',N'HALJALA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE08','8','no',N'Harku vald',N'HARKU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE09','9','no',N'Hiiumaa vald',N'HIIUMAA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE10','10','no',N'Järva vald',N'JÄRVA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE11','11','no',N'Jõelähtme vald',N'JÕELÄHTME VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE12','12','no',N'Jõgeva vald',N'JÕGEVA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE13','13','no',N'Jõhvi vald',N'JÕHVI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE14','14','no',N'Kadrina vald',N'KADRINA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE15','15','no',N'Kambja vald',N'KAMBJA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE16','16','no',N'Kanepi vald',N'KANEPI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE17','17','no',N'Kastre vald',N'KASTRE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE18','18','no',N'Kehtna vald',N'KEHTNA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE19','19','no',N'Keila linn',N'KEILA LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE20','20','no',N'Kihnu vald',N'KIHNU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE21','21','no',N'Kiili vald',N'KIILI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE22','22','no',N'Kohila vald',N'KOHILA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE23','23','no',N'Kohtla-Järve linn',N'KOHTLA-JÄRVE LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE24','24','no',N'Kose vald',N'KOSE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE25','25','no',N'Kuusalu vald',N'KUUSALU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE26','26','no',N'Lääne-Harju vald',N'LÄÄNE-HARJU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE27','27','no',N'Lääne-Nigula vald',N'LÄÄNE-NIGULA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE28','28','no',N'Lääneranna vald',N'LÄÄNERANNA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE29','29','no',N'Loksa linn',N'LOKSA LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE30','30','no',N'Lüganuse vald',N'LÜGANUSE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE31','31','no',N'Luunja vald',N'LUUNJA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE32','32','no',N'Maardu linn',N'MAARDU LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE33','33','no',N'Märjamaa vald',N'MÄRJAMAA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE34','34','no',N'Muhu vald',N'MUHU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE35','35','no',N'Mulgi vald',N'MULGI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE36','36','no',N'Mustvee vald',N'MUSTVEE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE37','37','no',N'Narva linn',N'NARVA LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE38','38','no',N'Narva-Jõesuu linn',N'NARVA-JÕESUU LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE39','39','no',N'Nõo vald',N'NÕO VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE40','40','no',N'Otepää vald',N'OTEPÄÄ VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE41','41','no',N'Paide linn',N'PAIDE LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE42','42','no',N'Pärnu linn',N'PÄRNU LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE43','43','no',N'Peipsiääre vald',N'PEIPSIÄÄRE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE44','44','no',N'Põhja-Pärnumaa vald',N'PÕHJA-PÄRNUMAA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE45','45','no',N'Põhja-Sakala vald',N'PÕHJA-SAKALA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE46','46','no',N'Põltsamaa vald',N'PÕLTSAMAA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE47','47','no',N'Põlva vald',N'PÕLVA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE48','48','no',N'Raasiku vald',N'RAASIKU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE49','49','no',N'Rae vald',N'RAE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE50','50','no',N'Rakvere linn',N'RAKVERE LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE51','51','no',N'Rakvere vald',N'RAKVERE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE52','52','no',N'Räpina vald',N'RÄPINA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE53','53','no',N'Rapla vald',N'RAPLA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE54','54','no',N'Rõuge vald',N'RÕUGE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE55','55','no',N'Ruhnu vald',N'RUHNU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE56','56','no',N'Saarde vald',N'SAARDE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE57','57','no',N'Saaremaa vald',N'SAAREMAA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE58','58','no',N'Saku vald',N'SAKU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE59','59','no',N'Saue vald',N'SAUE VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE60','60','no',N'Setomaa vald',N'SETOMAA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE61','61','no',N'Sillamäe linn',N'SILLAMÄE LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE62','62','no',N'Tallinn',N'TALLINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE63','63','no',N'Tapa vald',N'TAPA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE64','64','no',N'Tartu linn
+(NB! Olemasolev Tartu linn säilib, Tähtvere liitub)',N'TARTU LINN
+(NB! OLEMASOLEV TARTU LINN SÄILIB, TÄHTVERE LIITUB)');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE65','65','no',N'Tartu vald',N'TARTU VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE66','66','no',N'Toila vald',N'TOILA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE67','67','no',N'Tori vald',N'TORI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE68','68','no',N'Tõrva vald',N'TÕRVA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE69','69','no',N'Türi vald',N'TÜRI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE70','70','no',N'Väike-Maarja vald
+(NB! Olemasolev Väike-Maarja vald säilib, Rakke liitub)',N'VÄIKE-MAARJA VALD
+(NB! OLEMASOLEV VÄIKE-MAARJA VALD SÄILIB, RAKKE LIITUB)');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE71','71','no',N'Valga vald',N'VALGA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE72','72','no',N'Viimsi vald',N'VIIMSI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE73','73','no',N'Viljandi linn',N'VILJANDI LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE74','74','no',N'Viljandi vald 
+(NB! Olemasolev Viljandi vald säilib, teised liituvad)',N'VILJANDI VALD 
+(NB! OLEMASOLEV VILJANDI VALD SÄILIB, TEISED LIITUVAD)');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE75','75','no',N'Vinni vald 
+(NB! Olemasolev Vinni vald säilib, teised liituvad)',N'VINNI VALD 
+(NB! OLEMASOLEV VINNI VALD SÄILIB, TEISED LIITUVAD)');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE76','76','no',N'Viru-Nigula vald',N'VIRU-NIGULA VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE77','77','no',N'Vormsi vald',N'VORMSI VALD');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE78','78','no',N'Võru linn',N'VÕRU LINN');
+INSERT INTO dbo.laus (country_code,nuts3,lau1,_change,name1,name2) VALUES ('EE','EE79','79','no',N'Võru vald',N'VÕRU VALD');
