8d3744536c1261a069480cfc2d35f5014116fcc7
Xavier Pandis <xavier.pandis.garcia@everis.com>
2018-03-12 13:55:40 +0100

    WIFIFOREU-1782 - Helpdesk integration with EDCC

--- wifi4eu-portal/wifi4eu-portal-service/src/main/java/wifi4eu/wifi4eu/util/ScheduledTasks.java
+++ wifi4eu-portal/wifi4eu-portal-service/src/main/java/wifi4eu/wifi4eu/util/ScheduledTasks.java
@@ -36,8 +36,7 @@ public class ScheduledTasks {
 
     private static final Logger _log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    @Scheduled(cron = "0 0/30 * * * ?") // Every 30 minutes
-    //@Scheduled(cron = "*/10 * * * * ?") //Every 10 seconds
+    @Scheduled(cron = "0 0/30 * * * ?")
     public void scheduleHelpdeskIssues() {
 
         List<HelpdeskIssueDTO> helpdeskIssueDTOS = helpdeskService.getAllHelpdeskIssueNoSubmited();
@@ -67,7 +66,6 @@ public class ScheduledTasks {
     public static String executePost(String targetURL, String urlParameters) {
         HttpURLConnection connection = null;
         try {
-            //Create connection
             URL url = new URL(targetURL);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
@@ -86,13 +84,11 @@ public class ScheduledTasks {
             connection.setUseCaches(false);
             connection.setDoOutput(true);
 
-            //Send request
             DataOutputStream wr = new DataOutputStream (
                     connection.getOutputStream());
             wr.writeBytes(urlParameters);
             wr.close();
 
-            //Get Response
             InputStream is = connection.getInputStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
             StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
