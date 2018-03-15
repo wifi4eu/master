d8efb74fdd26bf472dd38a77465e578cde5e98aa
dsebastm <lucky8619>
2018-03-15 13:01:27 +0100

    Add new message azure queue https://docs.microsoft.com/es-es/azure/storage/queues/storage-java-how-to-use-queue-storage#how-to-add-a-message-to-a-queue

--- wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/azure/queue/Queue.java
+++ wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/azure/queue/Queue.java
@@ -4,6 +4,7 @@ import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.queue.CloudQueue;
 import com.microsoft.azure.storage.queue.CloudQueueClient;
+import com.microsoft.azure.storage.queue.CloudQueueMessage;
 import wifi4eu.wifi4eu.azure.constants.ConectionConstants;
 import wifi4eu.wifi4eu.azure.constants.QueueConstants;
 
@@ -31,5 +32,27 @@ public class Queue {
 
         // Create the queue if it doesn't already exist.
         queue.createIfNotExists();
+    }
+
+    public void addMessageAzureQueue(final String messageContent) throws URISyntaxException,
+                                                                            StorageException,
+                                                                            InvalidKeyException {
+
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount =
+                    CloudStorageAccount.parse(ConectionConstants.STORAGE_CONNECTION_STRING);
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.getQueueReference(QueueConstants.QUEUE_NAME);
+
+            // Create the queue if it doesn't already exist.
+            queue.createIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(messageContent);
+            queue.addMessage(message);
     }
 }
