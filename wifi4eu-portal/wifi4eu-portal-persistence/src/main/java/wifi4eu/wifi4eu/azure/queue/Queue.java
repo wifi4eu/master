50110b40d86c5ff638bd0dddf834bc7e96f6c505
dsebastm <lucky8619>
2018-03-15 14:16:43 +0100

    update message azure queue https://docs.microsoft.com/es-es/azure/storage/queues/storage-java-how-to-use-queue-storage#how-to-change-the-contents-of-a-queued-message

--- wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/azure/queue/Queue.java
+++ wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/azure/queue/Queue.java
@@ -5,11 +5,13 @@ import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.queue.CloudQueue;
 import com.microsoft.azure.storage.queue.CloudQueueClient;
 import com.microsoft.azure.storage.queue.CloudQueueMessage;
+import com.microsoft.azure.storage.queue.MessageUpdateFields;
 import wifi4eu.wifi4eu.azure.constants.ConectionConstants;
 import wifi4eu.wifi4eu.azure.constants.QueueConstants;
 
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
+import java.util.EnumSet;
 
 public class Queue {
 
@@ -72,5 +74,41 @@ public class Queue {
 
         // Peek at the next message.
         return queue.peekMessage();
+    }
+
+    public void updateMessageAzureQueue(final String searchMessageContent, final String updateMessageContent) throws StorageException,
+                                                                                                                        URISyntaxException,
+                                                                                                                        InvalidKeyException {
+
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount =
+                    CloudStorageAccount.parse(ConectionConstants.STORAGE_CONNECTION_STRING);
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.getQueueReference(QueueConstants.QUEUE_NAME);
+
+            // The maximum number of messages that can be retrieved is 32.
+            final int MAX_NUMBER_OF_MESSAGES_TO_PEEK = 32;
+
+            // Loop through the messages in the queue.
+            for (CloudQueueMessage message : queue.retrieveMessages(MAX_NUMBER_OF_MESSAGES_TO_PEEK,1,null,null)) {
+
+                // Check for a specific string.
+                if (message.getMessageContentAsString().equals(searchMessageContent)) {
+
+                    // Modify the content of the first matching message.
+                    message.setMessageContent(updateMessageContent);
+                    // Set it to be visible in 30 seconds.
+                    EnumSet<MessageUpdateFields> updateFields =
+                            EnumSet.of(MessageUpdateFields.CONTENT,
+                                    MessageUpdateFields.VISIBILITY);
+                    // Update the message.
+                    queue.updateMessage(message, 30, updateFields, null, null);
+                    break;
+                }
+            }
     }
 }
