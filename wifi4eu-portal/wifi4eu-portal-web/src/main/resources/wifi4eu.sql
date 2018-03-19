e01ceb0fe2e3e5c612d27288f125ad7007c8f617
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2018-03-19 10:57:05 +0100

    Fix minor bugs on application context and database

--- wifi4eu-portal/wifi4eu-portal-web/src/main/resources/wifi4eu.sql
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/resources/wifi4eu.sql
@@ -2,65 +2,67 @@
 -- -----------------------------------------------------
 -- Table `dbo`.`users`
 -- -----------------------------------------------------
-CREATE TABLE dbo.users (
-  [id]            INT          NOT NULL IDENTITY,
-  [treatment]     VARCHAR(255) NULL,
-  [name]          VARCHAR(255) NULL,
-  [surname]       VARCHAR(255) NULL,
-  [address]       VARCHAR(255) NULL,
-  [address_num]   VARCHAR(255) NULL,
-  [postal_code]   VARCHAR(255) NULL,
-  [email]         VARCHAR(255) NULL,
-  [password]      VARCHAR(255) NULL,
-  [create_date]   BIGINT       NULL,
-  [access_date]   BIGINT       NULL,
-  [ecas_email]    VARCHAR(255) NULL,
+CREATE TABLE dbo.users
+(
+  [id] INT NOT NULL IDENTITY,
+  [treatment] VARCHAR(255) NULL,
+  [name] VARCHAR(255) NULL,
+  [surname] VARCHAR(255) NULL,
+  [address] VARCHAR(255) NULL,
+  [address_num] VARCHAR(255) NULL,
+  [postal_code] VARCHAR(255) NULL,
+  [email] VARCHAR(255) NULL,
+  [password] VARCHAR(255) NULL,
+  [create_date] BIGINT NULL,
+  [access_date] BIGINT NULL,
+  [ecas_email] VARCHAR(255) NULL,
   [ecas_username] VARCHAR(255) NULL,
-  [type]          INT          NULL     DEFAULT NULL,
-  [verified]      SMALLINT      NOT NULL DEFAULT 0,
+  [type] INT NULL DEFAULT NULL,
+  [verified] SMALLINT NOT NULL DEFAULT 0,
+  [lang] VARCHAR(255) NOT NULL DEFAULT 0,
   PRIMARY KEY ([id])
 );
-
 -- -----------------------------------------------------
 -- Table `dbo`.`rights`
 -- -----------------------------------------------------
-CREATE TABLE dbo.rights (
-  [id]        INT          NOT NULL IDENTITY,
-  [userId]    INT          NULL,
+CREATE TABLE dbo.rights
+(
+  [id] INT NOT NULL IDENTITY,
+  [userId] INT NULL,
   [rightdesc] VARCHAR(255) NULL,
-  [type]      INT          NULL     DEFAULT NULL,
+  [type] INT NULL DEFAULT NULL,
   PRIMARY KEY ([id])
 );
-
 -- -----------------------------------------------------
 -- Table `dbo`.`laus`
 -- -----------------------------------------------------
-CREATE TABLE dbo.laus (
-  [id]               INT          NOT NULL IDENTITY,
-  [country_code]     VARCHAR(255) NULL,
-  [nuts3]            VARCHAR(255) NULL,
-  [lau1]             VARCHAR(255) NULL,
-  [lau2]             VARCHAR(255) NULL,
-  [_change]          VARCHAR(255) NULL,
-  [name1]            VARCHAR(255) NULL,
-  [name2]            VARCHAR(255) NULL,
-  [pop]              VARCHAR(255) NULL,
-  [area]             VARCHAR(255) NULL,
+CREATE TABLE dbo.laus
+(
+  [id] INT NOT NULL IDENTITY,
+  [country_code] VARCHAR(255) NULL,
+  [nuts3] VARCHAR(255) NULL,
+  [lau1] VARCHAR(255) NULL,
+  [lau2] VARCHAR(255) NULL,
+  [_change] VARCHAR(255) NULL,
+  [name1] VARCHAR(255) NULL,
+  [name2] VARCHAR(255) NULL,
+  [pop] VARCHAR(255) NULL,
+  [area] VARCHAR(255) NULL,
   [physical_address] VARCHAR(255) NULL,
   PRIMARY KEY ([id])
 );
-
 -- -----------------------------------------------------
 -- Table `dbo`.`municipalities`
 -- -----------------------------------------------------
-CREATE TABLE dbo.municipalities (
-  [id]          INT          NOT NULL IDENTITY,
-  [name]        VARCHAR(255) NULL,
-  [address]     VARCHAR(255) NULL,
+CREATE TABLE dbo.municipalities
+(
+  [id] INT NOT NULL IDENTITY,
+  [name] VARCHAR(255) NULL,
+  [address] VARCHAR(255) NULL,
   [address_num] VARCHAR(255) NULL,
   [postal_code] VARCHAR(255) NULL,
-  [country]     VARCHAR(255) NULL,
-  [lau]         INT          NULL,
+  [country] VARCHAR(255) NULL,
+  [lau] INT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_municipalities_laus]
@@ -69,24 +71,23 @@ CREATE TABLE dbo.municipalities (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-
 CREATE INDEX [fk_municipalities_laus1_idx] ON dbo.municipalities ([lau] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`registrations`
 -- -----------------------------------------------------
-
-CREATE TABLE dbo.registrations (
-  [id]              INT          NOT NULL IDENTITY,
-  [_user]           INT          NOT NULL,
-  [municipality]    INT          NOT NULL,
-  [role]            VARCHAR(500) NULL,
-  [_status]         INT       NOT NULL, --  0=HOLD; 1=KO; 2=OK
-  [legal_file1]     VARCHAR(max)     NULL,
-  [legal_file2]     VARCHAR(max)     NULL,
-  [legal_file3]     VARCHAR(max)     NULL,
-  [legal_file4]     VARCHAR(max)     NULL,
-  [ip_registration] VARCHAR(30)  NULL,
+CREATE TABLE dbo.registrations
+(
+  [id] INT NOT NULL IDENTITY,
+  [_user] INT NOT NULL,
+  [municipality] INT NOT NULL,
+  [role] VARCHAR(500) NULL,
+  [_status] INT NOT NULL,
+  --  0=HOLD; 1=KO; 2=OK
+  [legal_file1] VARCHAR(max) NULL,
+  [legal_file2] VARCHAR(max) NULL,
+  [legal_file3] VARCHAR(max) NULL,
+  [legal_file4] VARCHAR(max) NULL,
+  [ip_registration] VARCHAR(30) NULL,
   [organisation_id] INT DEFAULT NULL,
   [association_name] VARCHAR(max) DEFAULT NULL,
   PRIMARY KEY ([id])
@@ -102,56 +103,55 @@ CREATE TABLE dbo.registrations (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-
 CREATE INDEX [fk_user_idx] ON dbo.registrations ([_user] ASC);
 CREATE INDEX [fk_municipality_idx] ON dbo.registrations ([municipality] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`calls`
 -- -----------------------------------------------------
-CREATE TABLE dbo.calls (
-  [id]         INT          NOT NULL IDENTITY,
-  [event]      VARCHAR(500) NULL,
-  [start_date] BIGINT       NULL,
-  [end_date]   BIGINT       NULL,
+CREATE TABLE dbo.calls
+(
+  [id] INT NOT NULL IDENTITY,
+  [event] VARCHAR(500) NULL,
+  [start_date] BIGINT NULL,
+  [end_date] BIGINT NULL,
   PRIMARY KEY ([id])
 );
-
 -- -----------------------------------------------------
 -- Table `dbo`.`nuts`
 -- -----------------------------------------------------
-CREATE TABLE dbo.nuts (
-  [id]           INT          NOT NULL IDENTITY,
-  [code]         VARCHAR(255) NULL,
-  [label]        VARCHAR(255) NULL,
-  [level]        INT          NULL,
+CREATE TABLE dbo.nuts
+(
+  [id] INT NOT NULL IDENTITY,
+  [code] VARCHAR(255) NULL,
+  [label] VARCHAR(255) NULL,
+  [level] INT NULL,
   [country_code] VARCHAR(255) NULL,
-  [_order]       INT          NULL,
-  [sorting]      INT          NULL,
+  [_order] INT NULL,
+  [sorting] INT NULL,
   PRIMARY KEY ([id])
 );
-
 -- -----------------------------------------------------
 -- Table `dbo`.`threads`
 -- -----------------------------------------------------
-CREATE TABLE dbo.threads (
-  [id]        INT          NOT NULL IDENTITY,
-  [title]     VARCHAR(255) NULL,
-  [reason]    VARCHAR(255) NULL,
-  [type]      INT          NOT NULL,
-  [mediation] INT                   DEFAULT 0,
+CREATE TABLE dbo.threads
+(
+  [id] INT NOT NULL IDENTITY,
+  [title] VARCHAR(255) NULL,
+  [reason] VARCHAR(255) NULL,
+  [type] INT NOT NULL,
+  [mediation] INT DEFAULT 0,
   PRIMARY KEY ([id])
 );
-
 -- -----------------------------------------------------
 -- Table `dbo`.`thread_messages`
 -- -----------------------------------------------------
-CREATE TABLE dbo.thread_messages (
-  [id]          INT        NOT NULL IDENTITY,
-  [thread]      INT        NOT NULL,
-  [author]      INT        NOT NULL,
-  [message]     VARCHAR(max) NULL,
-  [create_date] BIGINT     NULL,
+CREATE TABLE dbo.thread_messages
+(
+  [id] INT NOT NULL IDENTITY,
+  [thread] INT NOT NULL,
+  [author] INT NOT NULL,
+  [message] VARCHAR(max) NULL,
+  [create_date] BIGINT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_thread_messages_threads]
@@ -165,31 +165,30 @@ CREATE TABLE dbo.thread_messages (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-
 CREATE INDEX [fk_thread_idx] ON dbo.thread_messages ([thread] ASC);
 CREATE INDEX [fk_author_idx] ON dbo.thread_messages ([author] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`suppliers`
 -- -----------------------------------------------------
-CREATE TABLE dbo.suppliers (
-  [id]                   INT          NOT NULL IDENTITY,
-  [name]                 VARCHAR(255) NULL,
-  [address]              VARCHAR(255) NULL,
-  [vat]                  VARCHAR(255) NULL,
-  [bic]                  VARCHAR(255) NULL,
-  [account_number]       VARCHAR(255) NULL,
-  [website]              VARCHAR(255) NULL,
-  [contact_name]         VARCHAR(255) NULL,
-  [contact_surname]      VARCHAR(255) NULL,
+CREATE TABLE dbo.suppliers
+(
+  [id] INT NOT NULL IDENTITY,
+  [name] VARCHAR(255) NULL,
+  [address] VARCHAR(255) NULL,
+  [vat] VARCHAR(255) NULL,
+  [bic] VARCHAR(255) NULL,
+  [account_number] VARCHAR(255) NULL,
+  [website] VARCHAR(255) NULL,
+  [contact_name] VARCHAR(255) NULL,
+  [contact_surname] VARCHAR(255) NULL,
   [contact_phone_prefix] VARCHAR(255) NULL,
   [contact_phone_number] VARCHAR(255) NULL,
-  [contact_email]        VARCHAR(255) NULL,
-  [logo]                 VARCHAR(max)     NULL,
-  [_user]                INT          NULL,
-  [legal_file1]          VARCHAR(max)     NULL,
-  [legal_file2]          VARCHAR(max)     NULL,
-  [_status]              INT      NULL,
+  [contact_email] VARCHAR(255) NULL,
+  [logo] VARCHAR(max) NULL,
+  [_user] INT NULL,
+  [legal_file1] VARCHAR(max) NULL,
+  [legal_file2] VARCHAR(max) NULL,
+  [_status] INT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_suppliers_users]
@@ -198,28 +197,27 @@ CREATE TABLE dbo.suppliers (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-
 CREATE INDEX [fk_suppliers_users_idx] ON dbo.suppliers ([_user] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`applications`
 -- -----------------------------------------------------
-CREATE TABLE dbo.applications (
-  [id]              INT     NOT NULL IDENTITY,
-  [call_id]         INT     NOT NULL,
-  [registration]    INT     NOT NULL,
-  [supplier]        INT     NULL,
-  [voucher_awarded] SMALLINT NULL     DEFAULT 0,
-  [date]            BIGINT  NULL,
-  [lef_export]      BIGINT  NULL,
-  [lef_import]      BIGINT  NULL,
-  [lef_status]      INT     NULL     DEFAULT 0,
-  [bc_export]       BIGINT  NULL,
-  [bc_import]       BIGINT  NULL,
-  [bc_status]       INT     NULL     DEFAULT 0,
-  [lc_export]       BIGINT  NULL,
-  [lc_import]       BIGINT  NULL,
-  [lc_status]       INT     NULL     DEFAULT 0,
+CREATE TABLE dbo.applications
+(
+  [id] INT NOT NULL IDENTITY,
+  [call_id] INT NOT NULL,
+  [registration] INT NOT NULL,
+  [supplier] INT NULL,
+  [voucher_awarded] SMALLINT NULL DEFAULT 0,
+  [date] BIGINT NULL,
+  [lef_export] BIGINT NULL,
+  [lef_import] BIGINT NULL,
+  [lef_status] INT NULL DEFAULT 0,
+  [bc_export] BIGINT NULL,
+  [bc_import] BIGINT NULL,
+  [bc_status] INT NULL DEFAULT 0,
+  [lc_export] BIGINT NULL,
+  [lc_import] BIGINT NULL,
+  [lc_status] INT NULL DEFAULT 0,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_applications_calls]
@@ -238,22 +236,21 @@ CREATE TABLE dbo.applications (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 )
- ;
-
+;
 CREATE INDEX [fk_call_idx] ON dbo.applications ([call_id] ASC);
 CREATE INDEX [fk_registration_idx] ON dbo.applications ([registration] ASC);
 CREATE INDEX [fk_applications_supplier_idx] ON dbo.applications ([supplier] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`mayors`
 -- -----------------------------------------------------
-CREATE TABLE dbo.mayors (
-  [id]           INT          NOT NULL IDENTITY,
-  [name]         VARCHAR(255) NULL,
-  [surname]      VARCHAR(255) NULL,
-  [treatment]    VARCHAR(255) NULL,
-  [email]        VARCHAR(255) NULL,
-  [municipality] INT          NULL,
+CREATE TABLE dbo.mayors
+(
+  [id] INT NOT NULL IDENTITY,
+  [name] VARCHAR(255) NULL,
+  [surname] VARCHAR(255) NULL,
+  [treatment] VARCHAR(255) NULL,
+  [email] VARCHAR(255) NULL,
+  [municipality] INT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_mayors_municipalities]
@@ -262,17 +259,16 @@ CREATE TABLE dbo.mayors (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
- ;
-
+;
 CREATE INDEX [fk_mayors_municipalities_idx] ON dbo.mayors ([municipality] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`representations`
 -- -----------------------------------------------------
-CREATE TABLE dbo.representations (
-  [id]           INT NOT NULL IDENTITY,
+CREATE TABLE dbo.representations
+(
+  [id] INT NOT NULL IDENTITY,
   [municipality] INT NOT NULL,
-  [mayor]        INT NOT NULL,
+  [mayor] INT NOT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_representations_municipalities]
@@ -286,22 +282,21 @@ CREATE TABLE dbo.representations (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 )
- ;
-
+;
 CREATE INDEX [fk_municipality_idx] ON dbo.representations ([municipality] ASC);
 CREATE INDEX [fk_mayor_idx] ON dbo.representations ([mayor] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`access_points`
 -- -----------------------------------------------------
-CREATE TABLE dbo.access_points (
-  [id]            INT          NOT NULL IDENTITY,
-  [municipality]  INT          NULL,
-  [name]          VARCHAR(255) NULL,
-  [product_name]  VARCHAR(255) NULL,
-  [model_number]  VARCHAR(255) NULL,
+CREATE TABLE dbo.access_points
+(
+  [id] INT NOT NULL IDENTITY,
+  [municipality] INT NULL,
+  [name] VARCHAR(255) NULL,
+  [product_name] VARCHAR(255) NULL,
+  [model_number] VARCHAR(255) NULL,
   [serial_number] VARCHAR(255) NULL,
-  [indoor]        SMALLINT      NULL     DEFAULT 0,
+  [indoor] SMALLINT NULL DEFAULT 0,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_access_points_municipalities]
@@ -310,17 +305,16 @@ CREATE TABLE dbo.access_points (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
- ;
-
+;
 CREATE INDEX [fk_municipality_idx] ON dbo.access_points ([municipality] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`supplied_regions`
 -- -----------------------------------------------------
-CREATE TABLE dbo.supplied_regions (
-  [id]       INT NOT NULL IDENTITY,
+CREATE TABLE dbo.supplied_regions
+(
+  [id] INT NOT NULL IDENTITY,
   [supplier] INT NOT NULL,
-  [region]   INT NOT NULL,
+  [region] INT NOT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_supplier_regions_suppliers]
@@ -334,38 +328,37 @@ CREATE TABLE dbo.supplied_regions (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
- ;
-
+;
 CREATE INDEX [fk_supplier_regions_supplier_idx] ON dbo.supplied_regions ([supplier] ASC);
 CREATE INDEX [fk_supplier_regions_region_idx] ON dbo.supplied_regions ([region] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`helpdesk_issues`
 -- -----------------------------------------------------
-CREATE TABLE dbo.helpdesk_issues (
-  [id]           INT          NOT NULL IDENTITY,
-  [from_email]   VARCHAR(255) NULL,
-  [assigned_to]  VARCHAR(255) NULL,
-  [topic]        VARCHAR(255) NULL,
-  [portal]       VARCHAR(255) NULL,
+CREATE TABLE dbo.helpdesk_issues
+(
+  [id] INT NOT NULL IDENTITY,
+  [from_email] VARCHAR(255) NULL,
+  [assigned_to] VARCHAR(255) NULL,
+  [topic] VARCHAR(255) NULL,
+  [portal] VARCHAR(255) NULL,
   [member_state] VARCHAR(255) NULL,
-  [summary]      VARCHAR(max)   NULL,
-  [create_date]  BIGINT       NULL,
-  [_status]      INT          NOT NULL DEFAULT 0,
-  [_ticket]      INT          NOT NULL DEFAULT 0,
+  [summary] VARCHAR(max) NULL,
+  [create_date] BIGINT NULL,
+  [_status] INT NOT NULL DEFAULT 0,
+  [_ticket] INT NOT NULL DEFAULT 0,
   PRIMARY KEY ([id])
 )
- ;
-
+;
 -- -----------------------------------------------------
 -- Table `dbo`.`helpdesk_comments`
 -- -----------------------------------------------------
-CREATE TABLE dbo.helpdesk_comments (
-  [id]          INT          NOT NULL IDENTITY,
-  [issue]       INT          NULL,
-  [from_email]  VARCHAR(255) NULL,
-  [comment]     VARCHAR(max)   NULL,
-  [create_date] BIGINT       NULL,
+CREATE TABLE dbo.helpdesk_comments
+(
+  [id] INT NOT NULL IDENTITY,
+  [issue] INT NULL,
+  [from_email] VARCHAR(255) NULL,
+  [comment] VARCHAR(max) NULL,
+  [create_date] BIGINT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_helpdesk_comments_issues]
@@ -374,19 +367,18 @@ CREATE TABLE dbo.helpdesk_comments (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
- ;
-
+;
 CREATE INDEX [fk_helpdesk_comments_issue_idx] ON dbo.helpdesk_comments ([issue] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`timelines`
 -- -----------------------------------------------------
-CREATE TABLE dbo.timelines (
-  [id]          INT          NOT NULL IDENTITY,
-  [call_id]     INT          NULL,
+CREATE TABLE dbo.timelines
+(
+  [id] INT NOT NULL IDENTITY,
+  [call_id] INT NULL,
   [description] VARCHAR(255) NULL,
-  [start_date]  BIGINT       NULL,
-  [end_date]    BIGINT       NULL,
+  [start_date] BIGINT NULL,
+  [end_date] BIGINT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_timelines_call_id]
@@ -395,20 +387,19 @@ CREATE TABLE dbo.timelines (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
- ;
-
+;
 CREATE INDEX [fk_timelines_call_id_idx] ON dbo.timelines ([call_id] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`temp_tokens`
 -- -----------------------------------------------------
-CREATE TABLE dbo.temp_tokens (
-  [id]          BIGINT       NOT NULL IDENTITY,
-  [token]       VARCHAR(255) NULL,
-  [email]       VARCHAR(255) NULL,
-  [create_date] BIGINT       NULL,
-  [expiry_date] BIGINT       NULL,
-  [_user]       INT          NULL,
+CREATE TABLE dbo.temp_tokens
+(
+  [id] BIGINT NOT NULL IDENTITY,
+  [token] VARCHAR(255) NULL,
+  [email] VARCHAR(255) NULL,
+  [create_date] BIGINT NULL,
+  [expiry_date] BIGINT NULL,
+  [_user] INT NULL,
   PRIMARY KEY ([id])
  ,
   CONSTRAINT [fk_temp_tokens_users]
@@ -417,37 +408,40 @@ CREATE TABLE dbo.temp_tokens (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
- ;
-
+;
 CREATE INDEX [fk_temp_tokens_users_idx] ON dbo.temp_tokens ([_user] ASC);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`organizations`
 -- -----------------------------------------------------
-CREATE TABLE dbo.organizations (
-  [id]      INT          NOT NULL IDENTITY,
-  [name]    VARCHAR(255) NULL,
-  [type]    VARCHAR(255) NULL,
+CREATE TABLE dbo.organizations
+(
+  [id] INT NOT NULL IDENTITY,
+  [name] VARCHAR(255) NULL,
+  [type] VARCHAR(255) NULL,
   [country] VARCHAR(255) NULL,
   PRIMARY KEY ([id])
 )
- ;
-INSERT INTO dbo.organizations (name, type, country)
-VALUES ('Everis', 'private', 'ES');
-
+;
+INSERT INTO dbo.organizations
+  (name, type, country)
+VALUES
+  ('Everis', 'private', 'ES');
 -- -----------------------------------------------------
 -- Table `dbo`.`voucher_management`
 -- -----------------------------------------------------
-CREATE TABLE voucher_management (
-  [id]           INT          NOT NULL IDENTITY,
-  [call_id]      INT          NOT NULL,
+CREATE TABLE voucher_management
+(
+  [id] INT NOT NULL IDENTITY,
+  [call_id] INT NOT NULL,
   [member_state] VARCHAR(255) NULL,
-  [minimum]      INT          NULL,
-  [maximum]      INT          NULL,
+  [minimum] INT NULL,
+  [maximum] INT NULL,
   PRIMARY KEY ([id])
 )
- ;
-INSERT INTO voucher_management (call_id, member_state, minimum, maximum) VALUES
+;
+INSERT INTO voucher_management
+  (call_id, member_state, minimum, maximum)
+VALUES
   (1, 'Austria', 15, 80),
   (1, 'Belgium', 15, 80),
   (1, 'Bulgaria', 15, 80),
@@ -475,13 +469,13 @@ INSERT INTO voucher_management (call_id, member_state, minimum, maximum) VALUES
   (1, 'Slovenia', 15, 80),
   (1, 'Spain', 15, 80),
   (1, 'Sweden', 15, 80);
-
 -- -----------------------------------------------------
 -- Table `dbo`.`user_threads`
 -- -----------------------------------------------------
-CREATE TABLE dbo.user_threads (
-  [id]     INT NOT NULL IDENTITY,
-  [_user]  INT NOT NULL,
+CREATE TABLE dbo.user_threads
+(
+  [id] INT NOT NULL IDENTITY,
+  [_user] INT NOT NULL,
   [thread] INT NOT NULL,
   PRIMARY KEY ([id])
 );
