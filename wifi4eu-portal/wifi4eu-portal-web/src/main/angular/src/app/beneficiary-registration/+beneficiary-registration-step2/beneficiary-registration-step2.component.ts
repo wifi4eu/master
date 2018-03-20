dc1d084e97bed83324f77e487d1082943b36663e
dsebastm <lucky8619>
2018-03-20 11:11:30 +0100

    Regex updated

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/beneficiary-registration/+beneficiary-registration-step2/beneficiary-registration-step2.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/beneficiary-registration/+beneficiary-registration-step2/beneficiary-registration-step2.component.ts
@@ -36,7 +36,7 @@ export class BeneficiaryRegistrationStep2Component implements OnChanges {
     private readonly MAX_LENGTH = 2;
     private css_class_municipalities: string[] = ['notValid'];
     private css_class_email: string[] = ['notValid'];
-    private emailPattern = new RegExp(/^[a-z0-9_-]+(?:\.[a-z0-9_-]+)*@(?:([a-z0-9][-]?){2,320}?\.)+[a-z0-9]{2,6}?$/i);
+    private emailPattern = new RegExp("(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])");
 
     private userEcas: UserDTOBase;
 
