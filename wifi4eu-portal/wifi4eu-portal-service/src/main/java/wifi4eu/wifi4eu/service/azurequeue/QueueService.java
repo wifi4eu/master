aaae0d316c28f49cf39af1d273402fdcdada8da5
dsebastm <lucky8619>
2018-03-26 13:26:03 +0200

    Azure Queue service n resource

---
+++ wifi4eu-portal/wifi4eu-portal-service/src/main/java/wifi4eu/wifi4eu/service/azurequeue/QueueService.java
@@ -0,0 +1,28 @@
+package wifi4eu.wifi4eu.service.azurequeue;
+
+
+import com.microsoft.azure.storage.StorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wifi4eu.wifi4eu.azure.queue.AzureQueue;
+import wifi4eu.wifi4eu.common.dto.model.AzureQueueDTO;
+
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+
+@Service
+public class QueueService {
+
+    @Autowired
+    AzureQueue azureQueue;
+
+    public QueueService() {}
+
+
+    public void addMessageAzureQueue(AzureQueueDTO azureQueueDTO) throws StorageException, InvalidKeyException, URISyntaxException {
+        if (azureQueueDTO != null && azureQueueDTO.getMessage() != null && !azureQueueDTO.getMessage().isEmpty()) {
+            azureQueue.addMessageAzureQueue( azureQueueDTO.getMessage() );
+        }
+    }
+
+}
