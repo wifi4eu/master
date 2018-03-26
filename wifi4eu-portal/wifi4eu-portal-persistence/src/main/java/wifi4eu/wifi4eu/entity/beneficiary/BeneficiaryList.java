eb848757691160a3faebc2c93767ed37d94895ce
Ruben Garcia Tamayo <ruben.garcia.tamayo@everis.com>
2018-03-26 14:59:45 +0200

    pageable on a custom query

---
+++ wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/entity/beneficiary/BeneficiaryList.java
@@ -0,0 +1,4 @@
+package wifi4eu.wifi4eu.entity.beneficiary;
+
+public class BeneficiaryList {
+}
---
+++ wifi4eu-portal/wifi4eu-portal-service/src/main/java/wifi4eu/wifi4eu/mapper/beneficiary/BeneficiaryListMapper.java
@@ -0,0 +1,4 @@
+package wifi4eu.wifi4eu.mapper.beneficiary;
+
+public interface BeneficiaryListMapper {
+}
