bae3509560a99e3362bce6f7802a62ffbf2c0872
Roger Fernandez Guri <roger.fernandez.guri@everis.com>
2018-04-12 19:26:39 +0200

    Hide list of suppliers menu item and add population script

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/app.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/app.component.ts
@@ -141,7 +141,11 @@ export class AppComponent {
                 new UxLayoutLink({
                     label: this.menuTranslations.get('itemMenu.suppReg'),
                     url: '/supplier-registration'
-                })
+                }),
+                // new UxLayoutLink({
+                //   label: this.menuTranslations.get('itemMenu.listSuppliers'),
+                //   url: 'list-suppliers'
+                // })
             ];
             this.children[1] = [
                 /* new UxLayoutLink({
@@ -151,7 +155,11 @@ export class AppComponent {
                 new UxLayoutLink({
                     label: this.menuTranslations.get('itemMenu.myAccount'),
                     url: '/supplier-portal/profile'
-                })
+                }),
+                // new UxLayoutLink({
+                //   label: this.menuTranslations.get('itemMenu.listSuppliers'),
+                //   url: 'list-suppliers'
+                // })
             ];
             this.children[2] = [
                 new UxLayoutLink({
@@ -161,7 +169,11 @@ export class AppComponent {
                 new UxLayoutLink({
                     label: this.menuTranslations.get('itemMenu.appPortal'),
                     url: '/beneficiary-portal/voucher'
-                })
+                }),
+                // new UxLayoutLink({
+                //   label: this.menuTranslations.get('itemMenu.listSuppliers'),
+                //   url: 'list-suppliers'
+                // })
             ];
             this.children[3] = [
                 new UxLayoutLink({
@@ -171,7 +183,11 @@ export class AppComponent {
                 new UxLayoutLink({
                     label: this.menuTranslations.get('itemMenu.appPortal'),
                     url: '/beneficiary-portal/voucher'
-                })
+                }),
+                // new UxLayoutLink({
+                //   label: 'Registered suppliers',
+                //   url: 'list-suppliers'
+                // })
             ];
             this.children[4] = [
                 new UxLayoutLink({
