b9f670fb604341d1c5d81b74d65e90664689f129
dcarrasg <danaiel.carrasco.gimenez.sa@everis.com>
2018-04-12 12:27:38 +0200

    Version with digicon export import.

--- wifi4eu-portal/wifi4eu-portal-service/src/main/java/wifi4eu/wifi4eu/service/exportImport/ExportImportRegistrationDataService.java
+++ wifi4eu-portal/wifi4eu-portal-service/src/main/java/wifi4eu/wifi4eu/service/exportImport/ExportImportRegistrationDataService.java
@@ -6,9 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import wifi4eu.wifi4eu.common.dto.model.MunicipalityDTO;
-import wifi4eu.wifi4eu.mapper.municipality.MunicipalityMapper;
-import wifi4eu.wifi4eu.repository.municipality.MunicipalityRepository;
 import wifi4eu.wifi4eu.common.dto.model.ExportImportRegistrationDataDTO;
 import wifi4eu.wifi4eu.mapper.exportImport.ExportImportRegistrationDataMapper;
 import wifi4eu.wifi4eu.repository.exportImport.ExportImportRegistrationDataReporsitory;
@@ -16,6 +13,7 @@ import wifi4eu.wifi4eu.service.exportImport.excelFile.CreateExcelFile;
 import wifi4eu.wifi4eu.service.exportImport.excelFile.ReadExcelFile;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class ExportImportRegistrationDataService {
@@ -33,7 +31,7 @@ public class ExportImportRegistrationDataService {
            CreateExcelFile cF=new CreateExcelFile();
            String [] header={"EU Rank", "Country Rank", "Country Name", "Municipality name", "Issue", "Number of registrations"};
            String [][] document=new String[exportImportRegistrationDataList.size()][7];
-           //Crear operaciones para eurank, countryrank, issue i number Rgistration
+           Integer [][] countryCount=new Integer[exportImportRegistrationDataList.size()][2];
             for(int i=0; i<exportImportRegistrationDataList.size(); i++){
                 String country=exportImportRegistrationDataList.get(i).getCountryName();
                 String municipailty=exportImportRegistrationDataList.get(i).getMunicipalityName();
@@ -41,14 +39,20 @@ public class ExportImportRegistrationDataService {
                 int countMunicipality=0;
                 for(int u=0; u<exportImportRegistrationDataList.size(); u++){
                     if(country.equals(exportImportRegistrationDataList.get(u).getCountryName())){
-                        countCountry++;
+                        countryCount[u][0]=countCountry++;
+                        countryCount[u][1]=exportImportRegistrationDataList.get(i).getEuRank();
+
                     }
                     if(municipailty.equals(exportImportRegistrationDataList.get(u).getMunicipalityName())){
                         countMunicipality++;
                     }
                 }
                 document[i][0]=String.valueOf(exportImportRegistrationDataList.get(i).getEuRank());
-                document[i][1]=String.valueOf(countCountry);
+                for(int w=0; w<countryCount.length; w++){
+                    if(exportImportRegistrationDataList.get(i).getEuRank()==countryCount[w][1]){
+                        document[i][1] = String.valueOf(countryCount[w][0]);
+                    }
+                }
                 document[i][2]=exportImportRegistrationDataList.get(i).getCountryName();
                 document[i][3]=exportImportRegistrationDataList.get(i).getMunicipalityName();
                 document[i][4]=String.valueOf(exportImportRegistrationDataList.get(i).getEuRank());
