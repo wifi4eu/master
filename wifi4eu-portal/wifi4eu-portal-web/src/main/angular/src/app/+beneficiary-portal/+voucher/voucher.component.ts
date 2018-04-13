f6099ecd944a62526c957dcc604236878176d3a5
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2018-04-13 12:57:39 +0200

    Check voucher assignment timestamp.
    WIFIFOREU-2038

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+voucher/voucher.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+voucher/voucher.component.ts
@@ -98,7 +98,7 @@ export class VoucherComponent {
                                                     }
                                                     this.loadingButtons.push(false);
                                                     let date = new Date(this.currentCall.startDate);
-                                                    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+                                                    // date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
                                                     this.dateNumber = ('0' + date.getUTCDate()).slice(-2) + "/" + ('0' + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
                                                     this.hourNumber = ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2);
                                                     if ((this.currentCall.startDate - new Date().getTime()) <= 0) {
