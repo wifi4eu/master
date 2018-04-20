bd298bfa0997100f7d1f76e0f9c5b8ef14133b34
David Santacana Rubireta <dsantaca@everis.com>
2018-04-20 13:37:32 +0200

    added a TODO task

--- wifi4eu-supplier/wifi4eu-supplier-persistence/src/main/java/wifi4eu.wifi4eu/repository/beneficiary/BeneficiaryDisplayedListRepository.java
+++ wifi4eu-supplier/wifi4eu-supplier-persistence/src/main/java/wifi4eu.wifi4eu/repository/beneficiary/BeneficiaryDisplayedListRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public interface BeneficiaryDisplayedListRepository extends CrudRepository<BeneficiaryDisplayedList, Integer> {
 
+    //TODO change the current query to retrieve the beneficiaries associated to supplier
+
     @Query(value = "SELECT m.name, m.id FROM municipalities m INNER JOIN registrations r ON r.municipality = m.id INNER JOIN users u ON u.id = r._user WHERE r._status = 2", nativeQuery = true)
     List<BeneficiaryDisplayedList> findBeneficiariesList();
 
