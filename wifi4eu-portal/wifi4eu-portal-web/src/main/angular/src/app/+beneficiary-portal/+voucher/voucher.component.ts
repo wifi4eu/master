ee97e3d5f68e92f2eac7846aefb2dbf6a8cb1add
Xavier Pandis <xavier.pandis.garcia@everis.com>
2018-04-24 16:14:16 +0200

    post to rabbitmq

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+voucher/voucher.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+voucher/voucher.component.ts
@@ -27,8 +27,7 @@ import { QueueComponent } from "../../azure/Queue";
     RegistrationApi,
     MunicipalityApi,
     MayorApi,
-    AzurequeueApi,
-    Http
+    AzurequeueApi
   ]
 })
 export class VoucherComponent {
@@ -273,42 +272,37 @@ export class VoucherComponent {
 
         event.target.style.pointerEvents = "none";
         event.target.style.opacity = "0.5";
-        event.target.disabled = true;
+        event.target.disabled = true;      
 
-        this.http
-          .post(this.rabbitmqURI, { message: m })
-          .toPromise()
-          .then(
-            () => {
-              this.loadingButtons[registrationNumber] = true;
-              this.voucherApplied = "greyImage";
-              this.voucherCompetitionState = 3;
+        this.http.post(this.rabbitmqURI, {message: m}).subscribe((response) => {
+          this.loadingButtons[registrationNumber] = true;
+          this.voucherApplied = "greyImage";
+          this.voucherCompetitionState = 3;
 
-              var oneHourLater = new Date();
-              oneHourLater.setMinutes(oneHourLater.getMinutes() + 5);
-              var timestamp = Math.floor(oneHourLater.getTime() / 1000);
+          var oneHourLater = new Date();
+          oneHourLater.setMinutes(oneHourLater.getMinutes() + 5);
+          var timestamp = Math.floor(oneHourLater.getTime() / 1000);
 
-              var queueStored = {
-                expires_in: timestamp,
-                idRegistration: this.registrations[registrationNumber].id,
-                call: this.currentCall.id
-              };
-              this.storedRegistrationQueues.push(queueStored);
-              this.localStorage.set(
-                "registrationQueue",
-                JSON.stringify(this.storedRegistrationQueues)
-              );
-              this.sharedService.growlTranslation(
-                "Your request for voucher has been submitted successfully. Wifi4Eu will soon let you know if you got a voucher for free wi-fi.",
-                "benefPortal.voucher.statusmessage5",
-                "success"
-              );
-            },
-            err => {
-              this.errorMessage = err;
-              this.displayError = true;
-            }
+          var queueStored = {
+            expires_in: timestamp,
+            idRegistration: this.registrations[registrationNumber].id,
+            call: this.currentCall.id
+          };
+          this.storedRegistrationQueues.push(queueStored);
+          this.localStorage.set(
+            "registrationQueue",
+            JSON.stringify(this.storedRegistrationQueues)
           );
+          this.sharedService.growlTranslation(
+            "Your request for voucher has been submitted successfully. Wifi4Eu will soon let you know if you got a voucher for free wi-fi.",
+            "benefPortal.voucher.statusmessage5",
+            "success"
+          );
+        },(error) => {
+          this.errorMessage = error;
+          this.displayError = true;
+        });
+      
       }
     }
   }
