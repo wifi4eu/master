ba6766ba30f80125cb4c62194609b2fe415d956d
dsebastm <lucky8619>
2018-04-25 10:45:14 +0200

    Added session emulation with mocked user

--- wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/filter/UserFilterMocked.java
+++ wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/filter/UserFilterMocked.java
@@ -12,8 +12,10 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 
 public class UserFilterMocked extends OncePerRequestFilter {
 
@@ -31,20 +33,28 @@ public class UserFilterMocked extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         try {
-            UserContext user = new UserContext(UserConstants.MOCKED_USER_NAME);
-            user.setEmail(UserConstants.MOCKED_MAIL);
-            user.setDomain(UserConstants.MOCKED_DOMAIN);
-            user.setPerId(UserConstants.MOCKED_PER_ID);
-            user.setDetailedUser(UserConstants.MOCKED_DETAILED_USER);
-            user.setFirstName(UserConstants.MOCKED_FIRST_NAME);
-            user.setLastName(UserConstants.MOCKED_LAST_NAME);
+            HttpSession session = request.getSession();
+            long activeSession = session.getLastAccessedTime() - session.getCreationTime();
+            long timestampSeconds = TimeUnit.MILLISECONDS.toSeconds(activeSession);
 
-            user.setRoleList(new LinkedList<RoleDTO>());
+            if (timestampSeconds < session.getMaxInactiveInterval()) {
+                UserContext user = new UserContext(UserConstants.MOCKED_USER_NAME);
+                user.setEmail(UserConstants.MOCKED_MAIL);
+                user.setDomain(UserConstants.MOCKED_DOMAIN);
+                user.setPerId(UserConstants.MOCKED_PER_ID);
+                user.setDetailedUser(UserConstants.MOCKED_DETAILED_USER);
+                user.setFirstName(UserConstants.MOCKED_FIRST_NAME);
+                user.setLastName(UserConstants.MOCKED_LAST_NAME);
 
-            UserHolder.setUser(user);
+                user.setRoleList(new LinkedList<RoleDTO>());
 
-            filterChain.doFilter(request, response);
+                UserHolder.setUser(user);
 
+                filterChain.doFilter(request, response);
+            } else  {
+                String requestUri = request.getRequestURI();
+                logger.info("Unauthenticated request: " + requestUri);
+            }
         } catch (Exception e) {
             throw new AppException(e);
         } finally {
