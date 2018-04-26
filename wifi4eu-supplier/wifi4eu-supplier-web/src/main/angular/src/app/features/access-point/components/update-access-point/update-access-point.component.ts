9eab273e8b87711bd07ef7bb9e225f082d768d70
Sara Henriques Mendes <sara.sofia.henriques.mendes@everis.com>
2018-04-26 12:10:51 +0200

    fix model for the edit and add modal

--- wifi4eu-supplier/wifi4eu-supplier-web/src/main/angular/src/app/features/access-point/components/update-access-point/update-access-point.component.ts
+++ wifi4eu-supplier/wifi4eu-supplier-web/src/main/angular/src/app/features/access-point/components/update-access-point/update-access-point.component.ts
@@ -12,29 +12,32 @@ export class UpdateAccessPoint implements OnChanges {
   @Input('accessPoint') accessPoint: AccessPointBase = new AccessPointBase();
   @Input('isEdit') isEdit: boolean = false;
   @ViewChild('accessPointForm') form: NgForm;
+
+  private unmodifiedAccessPoint: AccessPointBase = new AccessPointBase();
   private modalTitle: string;
 
-  private locationTypes =  [	{label:'Town Hall / Administrative building', value :'Town Hall / Administrative building'},
-  {label:'Health Centre / Hospital', value :'Health Centre / Hospital'},
-  {label:'Square', value :'Square'},
-  {label:'Park', value :'Park'},
-  {label:'Street / Pedestrian street', value :'Street / Pedestrian street'},
-  {label:'Tramway or Bus Station / Stop', value :'Tramway or Bus Station / Stop'},
-  {label:'Metro Station', value :'Metro Station'},
-  {label:'Railway Station', value :'Railway Station'},
-  {label:'Airport', value :'Airport'},
-  {label:'Sport Hall / Stadium', value :'Sport Hall / Stadium'},
-  {label:'School / Education or Research Centre / University', value :'School / Education or Research Centre / University'},
-  {label:'Library', value :'Library'},
-  {label:'Museum / Cultural Centre', value :'Museum / Cultural Centre'},
-  {label:'Site of touristic interest / Archeological Site', value :'Site of touristic interest / Archeological Site'},
-  {label:'Shopping Mall', value :'Shopping Mall'},
-  {label:'Other', value :'Other'}]
+  private locationTypes = [{ label: 'Town Hall / Administrative building', value: 'Town Hall / Administrative building' },
+  { label: 'Health Centre / Hospital', value: 'Health Centre / Hospital' },
+  { label: 'Square', value: 'Square' },
+  { label: 'Park', value: 'Park' },
+  { label: 'Street / Pedestrian street', value: 'Street / Pedestrian street' },
+  { label: 'Tramway or Bus Station / Stop', value: 'Tramway or Bus Station / Stop' },
+  { label: 'Metro Station', value: 'Metro Station' },
+  { label: 'Railway Station', value: 'Railway Station' },
+  { label: 'Airport', value: 'Airport' },
+  { label: 'Sport Hall / Stadium', value: 'Sport Hall / Stadium' },
+  { label: 'School / Education or Research Centre / University', value: 'School / Education or Research Centre / University' },
+  { label: 'Library', value: 'Library' },
+  { label: 'Museum / Cultural Centre', value: 'Museum / Cultural Centre' },
+  { label: 'Site of touristic interest / Archeological Site', value: 'Site of touristic interest / Archeological Site' },
+  { label: 'Shopping Mall', value: 'Shopping Mall' },
+  { label: 'Other', value: 'Other' }]
 
-  private deviceTypes = [{label:'indoor', value: '1'}, {label:'outdoor', value: '0'}]
+  private deviceTypes = [{ label: 'indoor', value: '1' }, { label: 'outdoor', value: '0' }]
 
-  private regexLocation : string = '^([-+]?)([0-9]{1,2})(((.)([0-9]{6})))$';
-  private regexMacAddress : string = '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$';
+  private regexLocation: string = '^([-+]?)([0-9]{1,2})(((.)([0-9]{6})))$';
+  private regexMacAddress: string = '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$';
+  private isSubmitted: boolean = false;
 
   constructor(private uxService: UxService, private translateService: TranslateService) {
   }
@@ -47,6 +50,7 @@ export class UpdateAccessPoint implements OnChanges {
       this.accessPoint = new AccessPointBase();
     } else {
       modalJsonString = 'updateAccessPoint.editTitle';
+      Object.assign(this.unmodifiedAccessPoint, this.accessPoint);
     }
 
     this.translateService.get(modalJsonString).subscribe(
@@ -58,7 +62,11 @@ export class UpdateAccessPoint implements OnChanges {
 
   closeUpdateAccessPoint() {
     this.uxService.closeModal('updateAccessPoint');
-    this.form.resetForm();
+    if (!this.isSubmitted && this.isEdit) {
+      Object.assign(this.accessPoint, this.unmodifiedAccessPoint);
+    } else if (!this.isEdit) {
+      this.form.form.reset();
+    }
   }
 
   onSubmit(form) {
@@ -71,6 +79,8 @@ export class UpdateAccessPoint implements OnChanges {
         alert('submit edit modal (update updateAccessPoint)');
       }
     }
+
+    this.isSubmitted = true;
     this.closeUpdateAccessPoint();
   }
 
