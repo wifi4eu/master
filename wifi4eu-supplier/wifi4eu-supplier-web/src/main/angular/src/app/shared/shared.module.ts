13b57e1fcb7951ef3d1b6c7b40138ced871992d1
Sara Henriques Mendes <sara.sofia.henriques.mendes@everis.com>
2018-04-30 17:34:25 +0200

    empty strings validation

--- wifi4eu-supplier/wifi4eu-supplier-web/src/main/angular/src/app/shared/shared.module.ts
+++ wifi4eu-supplier/wifi4eu-supplier-web/src/main/angular/src/app/shared/shared.module.ts
@@ -4,16 +4,19 @@ import { RouterModule } from '@angular/router';
 import { UxAllModule } from '@eui/ux-commons';
 
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { EmptyStringsValidator } from '../core/directives/validate-empty-string.directive';
 
 @NgModule({
     imports: [
         UxAllModule,
     ],
     declarations: [
+        EmptyStringsValidator
     ],
     exports: [
         UxAllModule,
-        TranslateModule
+        TranslateModule,
+        EmptyStringsValidator
     ]
 })
 export class SharedModule {
