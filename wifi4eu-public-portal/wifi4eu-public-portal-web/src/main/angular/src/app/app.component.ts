bc53fe40a60f92b6c02986ef62e5112368781abd
dsebastm <lucky8619>
2018-05-02 11:02:26 +0200

    Updated polling session time

--- wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/app/app.component.ts
+++ wifi4eu-public-portal/wifi4eu-public-portal-web/src/main/angular/src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent {
         this.updateFooterDate();
         this.updateMenuTranslations();
 
-        const sessionPolling = 60005;
+        const sessionPolling = 60030;
         Observable.interval(sessionPolling)
             .takeWhile(() => !this.sessionExpired)
             .subscribe(execution => {
