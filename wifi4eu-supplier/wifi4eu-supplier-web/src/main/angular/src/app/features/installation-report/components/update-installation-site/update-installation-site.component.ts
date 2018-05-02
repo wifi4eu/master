a146256697a4c75284df64647715474af2d60a56
Sara Henriques Mendes <sara.sofia.henriques.mendes@everis.com>
2018-05-02 11:30:02 +0200

    insert add installation site, missing municipality id fix

--- wifi4eu-supplier/wifi4eu-supplier-web/src/main/angular/src/app/features/installation-report/components/update-installation-site/update-installation-site.component.ts
+++ wifi4eu-supplier/wifi4eu-supplier-web/src/main/angular/src/app/features/installation-report/components/update-installation-site/update-installation-site.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, OnChanges, SimpleChange, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { Component, Input, ViewChild, OnChanges, SimpleChange, SimpleChanges, Output, EventEmitter, OnInit, DoCheck } from '@angular/core';
 import { UxService } from '@eui/ux-commons';
 import { PatternValidator, NgForm } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
@@ -11,7 +11,7 @@ import { ErrorHandlingService } from '../../../../core/services/error.service';
   selector: 'update-installation-site',
   templateUrl: './update-installation-site.component.html'
 })
-export class UpdateInstallationSite implements OnChanges {
+export class UpdateInstallationSite implements OnChanges, DoCheck {
   @Input('installationSite') installationSite: InstallationSiteBase = null;
   @Input('isEdit') isEdit: boolean = false;
   @Output() onSubmitted: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -28,13 +28,16 @@ export class UpdateInstallationSite implements OnChanges {
     private installationSiteApi: InstallationsiteApi,  private errorHandlingService: ErrorHandlingService) {
   }
 
+  ngDoCheck() {
+    if(this.installationSite.municipality != this.modifiedInstallationSite.municipality){
+      Object.assign(this.modifiedInstallationSite, this.installationSite);
+    }
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     let modalJsonString: string;
     let buttonJsonString: string;
     this.isSubmitted = false;
-
-    Object.assign(this.modifiedInstallationSite, this.installationSite);
-
     if (!this.isEdit) {
       modalJsonString = 'installationReport.addSite';
       buttonJsonString= 'shared.add';
@@ -67,7 +70,6 @@ export class UpdateInstallationSite implements OnChanges {
   }
 
   onSubmit(form) {
-
     if (form.form.valid) {
       this.installationSiteApi.updateInstallationSite(this.modifiedInstallationSite).subscribe((response: ResponseDTOBase) => {
         if (response.success) {
