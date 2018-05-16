7767d453676eaed6d8dd35422811bbdab2003385
Sara Henriques Mendes <sara.sofia.henriques.mendes@everis.com>
2018-05-16 10:42:55 +0200

    registration null fix

--- wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/entity/registration/Registration.java
+++ wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/entity/registration/Registration.java
@@ -51,9 +51,8 @@ public class Registration {
     @Column(name = "organisation_id")
     private int organisationId;
 
-    @ManyToOne
-    @JoinColumn(name = "id_status_beneficiary")
-    private BeneficiaryStatus idStatusBeneficiary;
+    @Column(name = "id_status_beneficiary")
+    private int idStatusBeneficiary;
 
     @Column(name = "compliance")
     private boolean compliance;
@@ -91,7 +90,7 @@ public class Registration {
     public Registration() {
     }
 
-    public Registration(User user, Municipality municipality, String role, int status, String legalFile1, String legalFile2, String legalFile3, String legalFile4, String ipRegistration, String associationName, int organisationId, BeneficiaryStatus idStatusBeneficiary, boolean compliance, String shortMemberState, String memberState, String call, int actionToBeTaken, int actionTaken, boolean beneficiaryIndicator, boolean wifiIndicator, boolean conformity, Timestamp firstFalseCheck, Timestamp dateRegistered) {
+    public Registration(User user, Municipality municipality, String role, int status, String legalFile1, String legalFile2, String legalFile3, String legalFile4, String ipRegistration, String associationName, int organisationId, int idStatusBeneficiary, boolean compliance, String shortMemberState, String memberState, String call, int actionToBeTaken, int actionTaken, boolean beneficiaryIndicator, boolean wifiIndicator, boolean conformity, Timestamp firstFalseCheck, Timestamp dateRegistered) {
         this.user = user;
         this.municipality = municipality;
         this.role = role;
@@ -213,11 +212,11 @@ public class Registration {
         this.organisationId = organisationId;
     }
 
-    public BeneficiaryStatus getIdStatusBeneficiary() {
+    public int getIdStatusBeneficiary() {
         return idStatusBeneficiary;
     }
 
-    public void setIdStatusBeneficiary(BeneficiaryStatus idStatusBeneficiary) {
+    public void setIdStatusBeneficiary(int idStatusBeneficiary) {
         this.idStatusBeneficiary = idStatusBeneficiary;
     }
 
