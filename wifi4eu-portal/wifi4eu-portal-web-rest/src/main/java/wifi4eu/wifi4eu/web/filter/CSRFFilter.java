81c91278e920dfa84d06f748a9bf949d3ae63309
JORDI MAGRIÑA CORTES <jordi.magrina.cortes@everis.com>
2018-05-31 00:29:13 +0200

    Replaced autowired

--- wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/filter/CSRFFilter.java
+++ wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/filter/CSRFFilter.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.DigestUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import wifi4eu.wifi4eu.common.dto.model.UserDTO;
 import wifi4eu.wifi4eu.common.ecas.UserHolder;
@@ -12,6 +14,7 @@ import wifi4eu.wifi4eu.common.security.UserContext;
 import wifi4eu.wifi4eu.service.user.UserService;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,7 +24,7 @@ public class CSRFFilter extends OncePerRequestFilter {
 
     Logger _log = LoggerFactory.getLogger(CSRFFilter.class);
 
-    @Autowired
+    //@Autowired
     private UserService userService;
 
     @Override
@@ -36,16 +39,22 @@ public class CSRFFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        _log.error("[i] doFilterInternal");
-        _log.error("Servlet path: " + request.getServletPath());
-        _log.error("doFilterInternal : XSRF : getRequestURL: " + request.getRequestURL().toString());
-        _log.error("METHOD: " + request.getMethod());
+        _log.debug("[i] doFilterInternal");
+        _log.debug("Servlet path: " + request.getServletPath());
+        _log.debug("doFilterInternal : XSRF : getRequestURL: " + request.getRequestURL().toString());
+        _log.debug("METHOD: " + request.getMethod());
+
+        if(userService == null){
+            ServletContext servletContext = getServletContext();
+            WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+            userService = webApplicationContext.getBean(UserService.class);
+        }
 
         if(!request.getMethod().equalsIgnoreCase("GET")
                 && !request.getRequestURL().toString().contains("/api/user/ecaslogin")){
 
             String XSRFTOKEN = request.getHeader("X-XSRF-TOKEN");
-            _log.error("[i] X-XSRF-TOKEN: " + XSRFTOKEN);
+            _log.debug("[i] X-XSRF-TOKEN: " + XSRFTOKEN);
 
             if(XSRFTOKEN != null && XSRFTOKEN.length() > 0) {
                 UserContext userContext = UserHolder.getUser();
@@ -54,20 +63,20 @@ public class CSRFFilter extends OncePerRequestFilter {
                     UserDTO user = userService.getUserByUserContext(userContext);
 
                     if (user != null) {
-                        _log.error("User stored Token: " + user.getCsrfToken());
-                        _log.error("Comparision: " + XSRFTOKEN.equalsIgnoreCase(user.getCsrfToken()));
+                        _log.debug("User stored Token: " + user.getCsrfToken());
+                        _log.debug("Comparision: " + XSRFTOKEN.equalsIgnoreCase(user.getCsrfToken()));
 
                         if (!XSRFTOKEN.equalsIgnoreCase(user.getCsrfToken())) {
                             response.sendError(HttpStatus.UNAUTHORIZED.value());
                             return;
                         }
                     } else {
-                        _log.error("user is NULL");
+                        _log.debug("user is NULL");
                         response.sendError(HttpStatus.UNAUTHORIZED.value());
                         return;
                     }
                 } else {
-                    _log.error("userContext is NULL");
+                    _log.debug("userContext is NULL");
                     response.sendError(HttpStatus.UNAUTHORIZED.value());
                     return;
                 }
@@ -77,7 +86,7 @@ public class CSRFFilter extends OncePerRequestFilter {
             }
         }
 
-        _log.error("[f] doFilterInternal");
+        _log.debug("[f] doFilterInternal");
         filterChain.doFilter(request, response);
     }
 }
