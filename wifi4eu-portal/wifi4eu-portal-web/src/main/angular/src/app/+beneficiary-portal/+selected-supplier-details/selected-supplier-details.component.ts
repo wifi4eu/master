375a2b644923f30e6301475e182a24eb7d501500
Santiago Ramos Romeu <santiago.ramos.romeu@everis.com>
2018-05-31 18:42:04 +0200

    Select supplier redirects to the list of available suppliers for the beneficiary region

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+selected-supplier-details/selected-supplier-details.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+selected-supplier-details/selected-supplier-details.component.ts
@@ -3,9 +3,10 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { LocalStorageService } from "angular-2-local-storage";
+import { Router } from '@angular/router';
 
 // Project Api's
-import { MunicipalityApi, RegistrationApi, CallApi, ApplicationApi, CallDTOBase, RegistrationDTOBase, ApplicationDTOBase, SupplierApi, SupplierDTOBase } from "../../shared/swagger";
+import { MunicipalityApi, RegistrationApi, CallApi, ApplicationApi, CallDTOBase, RegistrationDTOBase, ApplicationDTOBase, SupplierApi, SupplierDTOBase, ResponseDTOBase } from "../../shared/swagger";
 
 // Project DTO's
 import { MunicipalityDTOBase } from "../../shared/swagger/model/MunicipalityDTO";
@@ -43,86 +44,83 @@ export class SelectedSupplierDetailsComponent {
   constructor(
     private route: ActivatedRoute,
     private localStorage: LocalStorageService,
+    private router: Router,
     private municipalityApi: MunicipalityApi,
     private registrationApi: RegistrationApi,
     private callApi: CallApi,
     private applicationApi: ApplicationApi,
     private supplierApi: SupplierApi
   ) {
-    /* Observable<string> */
-    const id: any = route.params.map(p => p.id);
-    this.municipalityId = id.source.value.municipalityId;
-    console.log("The municiplaity id is ", id.source.value.municipalityId);
-    
-    this.municipalityApi.getMunicipalityById(this.municipalityId).subscribe(
-      (municipality: MunicipalityDTOBase) => {
-        this.municipality = municipality;
-        console.log("Obtained municipality is ", municipality);
-      });    
 
-      
-      
-      
-      
-      let storedUser = this.localStorage.get('user');
-      this.user = storedUser ? JSON.parse(storedUser.toString()) : null;
-      let storedRegistrations = this.localStorage.get('registrationQueue') ? JSON.parse(this.localStorage.get('registrationQueue').toString()) : null;
-      this.storedRegistrationQueues = storedRegistrations ? storedRegistrations : [];
-    
-      if (this.user != null) {
-        this.registrationApi.getRegistrationsByUserId(this.user.id, new Date().getTime()).subscribe(
-          (registrations: RegistrationDTOBase[]) => {
-            this.registration = registrations[0];
-            
-            // Check if there are Calls
-          this.callApi.allCalls().subscribe(
-            (calls: CallDTOBase[]) => {
-              // Get current application of the beneficiary
-                  this.applicationApi.getApplicationByCallIdAndRegistrationId(calls[0].id, this.registration.id).subscribe(
-                    (application: ApplicationDTOBase) => {
-                      this.application = application;
-                      console.log("Application is: ", this.application);
-                      this.getSupplierDetails();  
-                    }
-                  );
+  /* Observable<string> */
+  const id: any = route.params.map(p => p.id);
+  this.municipalityId = id.source.value.municipalityId;
+  
+  this.municipalityApi.getMunicipalityById(this.municipalityId).subscribe(
+    (municipality: MunicipalityDTOBase) => {
+      this.municipality = municipality;
+    }
+  );    
+
+  let storedUser = this.localStorage.get('user');
+  this.user = storedUser ? JSON.parse(storedUser.toString()) : null;
+  let storedRegistrations = this.localStorage.get('registrationQueue') ? JSON.parse(this.localStorage.get('registrationQueue').toString()) : null;
+  this.storedRegistrationQueues = storedRegistrations ? storedRegistrations : [];
+
+  if (this.user != null) {
+    this.registrationApi.getRegistrationsByUserId(this.user.id, new Date().getTime()).subscribe(
+      (registrations: RegistrationDTOBase[]) => {
+        this.registration = registrations[0];
+        
+        // Check if there are Calls
+      this.callApi.allCalls().subscribe(
+        (calls: CallDTOBase[]) => {
+          // Get current application of the beneficiary
+              this.applicationApi.getApplicationByCallIdAndRegistrationId(calls[0].id, this.registration.id).subscribe(
+                (application: ApplicationDTOBase) => {
+                  this.application = application;
+                  console.log("Application is: ", this.application);
+                  this.getSupplierDetails();  
                 }
               );
-              
             }
           );
+          
         }
+      );
+    }
         
-        // End of constructor  
-      }
+  // End of constructor  
+  }
       
-      /*  -- METHODS -- */
-      getSupplierDetails() {
-        this.supplierApi.getSupplierDetailsById(this.application.supplierId).subscribe(
-          (supplier: SupplierDTOBase) => {            
-            this.supplier = supplier;
-          }
-        );
-        // Get the date when supplier was selected
-        this.getStringDate(this.application.date);
+  /*  -- METHODS -- */
+  getSupplierDetails() {
+    this.supplierApi.getSupplierDetailsById(this.application.supplierId).subscribe(
+      (supplier: SupplierDTOBase) => {            
+        this.supplier = supplier;
       }
+    );
+    // Get the date when supplier was selected
+    this.getStringDate(this.application.date);
+  }
 
-      /* Get displayed string date from epoch number */
-      private getStringDate(epoch) {
-        this.selectionDate = new Date(epoch);
-        this.localeDate = this.selectionDate.toLocaleDateString().split(' ');
-        this.displayedDate = this.localeDate[0];
-      }
+  /* Get displayed string date from epoch number */
+  private getStringDate(epoch) {
+    this.selectionDate = new Date(epoch);
+    this.localeDate = this.selectionDate.toLocaleDateString().split(' ');
+    this.displayedDate = this.localeDate[0];
+  }
 
 
-      /* Allow beneficiary to change selected supplier */
-      private changeSupplier() {
+  /* Allow beneficiary to change selected supplier */
+  private changeSupplier() {
+    this.router.navigate(['/beneficiary-portal/select-supplier', this.municipality.id]);
+  }
 
-      }
+  /* Assign supplier to the beneficiary application */
+  private assignSupplier() {
 
-      /* Assign supplier to the beneficiary application */
-      private assignSupplier() {
-        
-      }
+  }
 
 // End of exported class
 }
