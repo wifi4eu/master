dd4a69388e3775c69cf31b1a898f047b2e136851
Xavier Pandis <xavier.pandis.garcia@everis.com>
2018-06-01 16:48:19 +0200

    fix excel extension

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+dgconn-portal/+voucher/voucher.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+dgconn-portal/+voucher/voucher.component.ts
@@ -156,7 +156,7 @@ export class DgConnVoucherComponent {
     this.downloadingExcel = false;
     this.voucherApi.exportExcelVoucherSimulation(this.callVoucherAssignment.id, this.selectedCountry, this.searchedMunicipality === null || this.searchedMunicipality === "" ? 'All' : this.searchedMunicipality, this.page, this.sizePage, this.sortField, this.sortDirection).subscribe((response) => {
       let blob = new Blob([response], {type: "application/vnd.ms-excel"});
-      FileSaver.saveAs(blob, `voucher-simulation-${this.callSelected.event}`);
+      FileSaver.saveAs(blob, `voucher-simulation-${this.callSelected.event}.xls`);
       this.loadingSimulation = false;
       this.downloadingExcel = false;
     }, (error) => {
