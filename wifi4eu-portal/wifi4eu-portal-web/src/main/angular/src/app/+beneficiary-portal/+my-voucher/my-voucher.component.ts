ed24014e03c3f8ba74556eb6ae13772f2b71667e
Santiago Ramos Romeu <santiago.ramos.romeu@everis.com>
2018-06-04 11:23:49 +0200

    created my-voucher folder with html and ts file

---
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+my-voucher/my-voucher.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'my-voucher-component',
+    templateUrl: 'my-voucher.component.html',
+    providers: [] 
+})
+
+export class MyVoucherComponent {
+    /* --- PROPERTIES --- */
+    constructor() {
+
+    }
+
+    /* --- METHODS --- */
+}
