ae44bc5cbb4911534ea3a0b8ce4e0de04f9a0e33
Victor Moral <victor.moral.guijo@everis.com>
2018-06-12 13:39:30 +0200

    Changed ng path, commented angular/cli installation and parametrized environment profile

--- wifi4eu-dependencies/wifi4eu-jenkins.sh
+++ wifi4eu-dependencies/wifi4eu-jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Install angular"
-npm install -g @angular/cli@latest
+#echo "Install angular"
+#npm install -g @angular/cli@latest
 echo "Check angular version"
-/var/quark/binaries/node/node-v6.11.5-linux-x64/lib/node_modules/@angular/cli/bin/ng -v
+/var/quark/binaries/node/node-v8.9.3-linux-x64/lib/node_modules/@angular/cli/bin/ng -v
 echo "Config EC Dev Ops repo"
 npm config set "//ecdevops.eu/repository/npm-all/:_authToken=828d555c-441f-3ee2-8ec5-8fe715c389f2"
 npm config set registry https://ecdevops.eu/repository/npm-all/
@@ -13,4 +13,4 @@ npm install --prefix ./wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/
 echo "Install ECAS dependency"
 mvn install:install-file -Dfile=wifi4eu-dependencies/ecas-tomcat-8.0-4.22.0.jar  -DgroupId=eu.europa.ec.digit.iam.ecas.client -DartifactId=ecas-tomcat-8.0 -Dversion=4.22.0 -Dpackaging=jar
 echo "Compile project"
-mvn clean install -U -Png-build-jenkins -PTEST
+mvn clean install -U -Png-build-jenkins -P$1
