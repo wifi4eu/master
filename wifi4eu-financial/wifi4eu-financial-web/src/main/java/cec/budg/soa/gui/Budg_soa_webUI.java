e1eaa40478681c8271302dab4dee4b096ff73215
dcarrasg <danaiel.carrasco.gimenez.sa@everis.com>
2018-06-14 13:13:32 +0200

    Version generate LEF.

--- wifi4eu-financial/wifi4eu-financial-web/src/main/java/cec/budg/soa/gui/Budg_soa_webUI.java
+++ wifi4eu-financial/wifi4eu-financial-web/src/main/java/cec/budg/soa/gui/Budg_soa_webUI.java
@@ -53,8 +53,10 @@ public class Budg_soa_webUI extends UI {
 			InitialContext ic = new InitialContext();
 //			soaTubeWSClient = (SoatubeWSClientLocal)ic.lookup("java:comp/env/ejb/SoatubeWSClient");
 //			jmsProducer = (JmsProducerLocal)ic.lookup("java:comp/env/ejb/JmsProducer");
-			jmsProducer = (JmsProducerLocal)ic.lookup("java:global/wifi4eu-financial/wifi4eu-financial-web/JmsProducer");
-			soaTubeWSClient = (SoatubeWSClientLocal)ic.lookup("java:global/wifi4eu-financial/wifi4eu-financial-web/SoatubeWSClient");
+//			jmsProducer = (JmsProducerLocal)ic.lookup("java:global/wifi4eu-financial/wifi4eu-financial-web/JmsProducer");
+//			soaTubeWSClient = (SoatubeWSClientLocal)ic.lookup("java:global/wifi4eu-financial/wifi4eu-financial-web/SoatubeWSClient");
+			jmsProducer = null;
+			soaTubeWSClient = null;
 
 		} catch (Exception e) {
 			e.printStackTrace();
