1d90f4aa4d10cbce83c28ef45fcd5cd7960fae5d
David Romero Rodriguez <david.romero.rodriguez@everis.com>
2018-06-29 13:38:55 +0200

    Code cleanup in frontend part

--- wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/src/app/+dgconn-portal/+exportImport/exportImport.component.ts
+++ wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/src/app/+dgconn-portal/+exportImport/exportImport.component.ts
@@ -2,14 +2,13 @@ import {Component} from "@angular/core";
 import {animate, style, transition, trigger} from "@angular/animations";
 import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import {Observable} from "rxjs";
-import * as FileSaver from 'file-saver';
 import {ExportImportApi} from "../../shared/swagger/api/ExportImportApi";
 import {SharedService} from "../../shared/shared.service";
 import {ApplicationApi} from "../../shared/swagger/api/ApplicationApi";
 import {CallApi} from "../../shared/swagger/api/CallApi";
 import {ResponseDTOBase} from '../../shared/swagger/model/ResponseDTO';
 import {TranslateService} from "ng2-translate";
-
+import * as FileSaver from 'file-saver';
 
 @Component({
     templateUrl: 'exportImport.component.html',
@@ -32,8 +31,6 @@ import {TranslateService} from "ng2-translate";
 })
 
 export class DgConnExportImportComponent {
-     private exportEnabled: boolean = false;
-     private jsonFile: File;
      private processingOperation: boolean = false;
 
      constructor(private http: Http, private exportImportApi: ExportImportApi, private sharedService: SharedService, private translateService: TranslateService) {
@@ -53,22 +50,6 @@ export class DgConnExportImportComponent {
                 this.processingOperation = false;
             }
         );
-//            this.exportImportApi.exportRegistrationData().subscribe(
-//                (response: ResponseDTO)  => {
-//                    if(response.success){
-//                        this.sharedService.growlTranslation("Your file have been exported correctly!", "dgconn.dashboard.card.messageExport", "success");
-//                        this.translateService.get("dgconn.dashboard.card.messageExport").subscribe(
-//                            (translation: string) => {
-//                                if (translation) {
-//                                    window.alert(translation);
-//                                }
-//                            }
-//                        );
-//                    }
-//                },
-//                error => {
-//                }
-//            );
      }
 
      private importRegistrationData(){
@@ -86,23 +67,6 @@ export class DgConnExportImportComponent {
                 this.processingOperation = false;
             }
         );
-
-//            this.exportImportApi.importRegistrationData().subscribe(
-//                (response: ResponseDTO)  => {
-//                    if(response.success){
-//                        this.sharedService.growlTranslation("Your file have been imported correctly!", "dgconn.dashboard.card.messageImport", "success");
-//                        this.translateService.get("dgconn.dashboard.card.messageImport").subscribe(
-//                            (translation: string) => {
-//                                if (translation) {
-//                                    window.alert(translation);
-//                                }
-//                            }
-//                        );
-//                    }
-//                },
-//                error => {
-//                }
-//            );
      }
 
       private exportBeneficiaryInformation() {
@@ -122,24 +86,6 @@ export class DgConnExportImportComponent {
                 this.processingOperation = false;
             }
         );
-//             var myWindow =window.open("http://localhost:8080/wifi4eu/alertExport.jsp","mywindow","status=1,width=350,height=150");
-//             this.exportImportApi.exportBeneficiaryInformation().subscribe(
-//                 (response: ResponseDTO) => {
-//                     if (response.success) {
-//                         let blob = new Blob([response.data], {type: 'application/json'});
-//                         FileSaver.saveAs(blob, "ExportBeneficiaryInformation.json");
-//                         myWindow.close();
-//                         window.alert("Export succesful!");
-//                     } else {
-//                         myWindow.close();
-//                         window.alert("Export failed!");
-//                     }
-//                 }, error => {
-//                     console.log(error);
-//                     myWindow.close();
-//                     window.alert("Something went wrong.");
-//                 }
-//             );
       }
 
      private exportBudgetaryCommitment() {
@@ -159,24 +105,6 @@ export class DgConnExportImportComponent {
                 this.processingOperation = false;
             }
         );
-//              var myWindow =window.open("http://localhost:8080/wifi4eu/alertExport.jsp","mywindow","status=1,width=350,height=150");
-//              this.exportImportApi.exportBudgetaryCommitment().subscribe(
-//                  (response: ResponseDTO) => {
-//                      if (response.success) {
-//                          let blob = new Blob([response.data], {type: 'application/json'});
-//                          FileSaver.saveAs(blob, "ExportBudgetaryCommitment.json");
-//                          myWindow.close();
-//                          window.alert("Export succesful!");
-//                      } else {
-//                          myWindow.close();
-//                          window.alert("Export failed!");
-//                      }
-//                  }, error => {
-//                      console.log(error);
-//                      myWindow.close();
-//                      window.alert("Something went wrong.");
-//                  }
-//              );
      }
 
      private importLegalEntityFBCValidate(event: any) {
@@ -203,33 +131,5 @@ export class DgConnExportImportComponent {
                 reader.readAsText(file);
             }
         }
-//             this.exportEnabled = false;
-//             if (event && event.target && event.target.files && event.target.files.length == 1) {
-//                 this.jsonFile = event.target.files['0'];
-//                 let reader = new FileReader();
-//                 reader.onload = (e) => {
-//                     var myWindow =window.open("http://localhost:8080/wifi4eu/alertImport.jsp","mywindow","status=1,width=350,height=150");
-//                     this.exportImportApi.importLegalEntityFBCValidate(reader.result).subscribe(
-//                         (response: ResponseDTO) => {
-//                             if (response.success) {
-//                                 this.exportEnabled = true;
-//                                 //this.sharedService.growlTranslation("Your file have been imported correctly!", "dgconn.dashboard.card.messageImport", "success");
-//                                 //this.translateService.get("dgconn.dashboard.card.messageImport").subscribe(
-//                                 myWindow.close();
-//                                 window.alert("Import succesful!");
-//                             } else {
-//                                 myWindow.close();
-//                                 window.alert("Import failed!");
-//                             }
-//                         }, error => {
-//                             console.log(error);
-//                             myWindow.close();
-//                             window.alert("Import failed! Make sure that the file you are uploading has the correct format.");
-//                         }
-//                     );
-//                 }
-//                 reader.readAsText(this.jsonFile);
-//                 //reader.readAsBinaryString(this.excelFile);
-//             }
      }
 }
