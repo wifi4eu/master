0a229eb77c1db4ca6a3b98c5d1854d615bce9ab6
Lucía Vivero <lucia.vivero.ferreirua@everis.com>
2018-07-04 15:07:49 +0200

    fix validation imputs on frontend and conditionals on backend

--- wifi4eu-common/src/main/java/wifi4eu/wifi4eu/common/utils/BeneficiaryValidator.java
+++ wifi4eu-common/src/main/java/wifi4eu/wifi4eu/common/utils/BeneficiaryValidator.java
@@ -13,27 +13,27 @@ public class BeneficiaryValidator {
         UserDTO user = beneficiaryDTO.getUser();
 
         if (beneficiaryDTO.isRepresentsMultipleMunicipalities()) {
-            if (beneficiaryDTO.getAssociationName() != null 
-                || beneficiaryDTO.getAssociationName().trim().isEmpty()) {
-                    throw new Exception("Association name field is empty!");
+            if (beneficiaryDTO.getAssociationName() != null
+                    && beneficiaryDTO.getAssociationName().trim().isEmpty()) {
+                throw new Exception("Association name field is empty!");
             }
         }
         for (MayorDTO mayorDTO : beneficiaryDTO.getMayors()) {
             if (mayorDTO.getEmail() != null || mayorDTO.getName() != null || mayorDTO.getSurname() != null
-                 || mayorDTO.getEmail().trim().isEmpty() || mayorDTO.getName().trim().isEmpty() || mayorDTO.getSurname().trim().isEmpty()) {
-                    throw new Exception("Some beneficiary field is empty!");
+                    && mayorDTO.getEmail().trim().isEmpty() || mayorDTO.getName().trim().isEmpty() || mayorDTO.getSurname().trim().isEmpty()) {
+                throw new Exception("Some beneficiary field is empty!");
             }
         }
         for (MunicipalityDTO municipalityDTO : beneficiaryDTO.getMunicipalities()) {
-            if (municipalityDTO.getAddress() != null || municipalityDTO.getAddressNum() != null || municipalityDTO.getName() != null || municipalityDTO.getCountry() != null || municipalityDTO.getPostalCode() != null 
-                || municipalityDTO.getAddress().trim().isEmpty() || municipalityDTO.getAddressNum().trim().isEmpty() || municipalityDTO.getName().trim().isEmpty() || municipalityDTO.getCountry().trim().isEmpty() || municipalityDTO.getPostalCode().trim().isEmpty()) {
-                    throw new Exception("Some municipality field is empty!");
+            if (municipalityDTO.getAddress() != null || municipalityDTO.getAddressNum() != null || municipalityDTO.getName() != null || municipalityDTO.getCountry() != null || municipalityDTO.getPostalCode() != null
+                    && municipalityDTO.getAddress().trim().isEmpty() || municipalityDTO.getAddressNum().trim().isEmpty() || municipalityDTO.getName().trim().isEmpty() || municipalityDTO.getCountry().trim().isEmpty() || municipalityDTO.getPostalCode().trim().isEmpty()) {
+                throw new Exception("Some municipality field is empty!");
             }
         }
 
-        if (user.getAddress() != null || user.getAddressNum() != null || user.getName() != null || user.getEmail() != null || user.getSurname() != null || user.getPostalCode() != null 
-                || user.getAddress().trim().isEmpty() || user.getAddressNum().trim().isEmpty() || user.getName().trim().isEmpty() || user.getEmail().trim().isEmpty()  || user.getSurname().trim().isEmpty() || user.getPostalCode().trim().isEmpty()) {
-                throw new Exception("Some user field is empty!");
-            }
+        if (user.getAddress() != null || user.getAddressNum() != null || user.getName() != null || user.getEmail() != null || user.getSurname() != null || user.getPostalCode() != null
+                && user.getAddress().trim().isEmpty() || user.getAddressNum().trim().isEmpty() || user.getName().trim().isEmpty() || user.getEmail().trim().isEmpty() || user.getSurname().trim().isEmpty() || user.getPostalCode().trim().isEmpty()) {
+            throw new Exception("Some user field is empty!");
         }
+    }
 }
--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/supplier-registration/+supplier-registration-step3/supplier-registration-step3.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/supplier-registration/+supplier-registration-step3/supplier-registration-step3.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, Output, EventEmitter} from "@angular/core";
+import {Component, Input, Output, EventEmitter, ViewChild} from "@angular/core";
 import {SupplierDTOBase} from "../../shared/swagger/model/SupplierDTO";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'supplier-registration-step3', templateUrl: 'supplier-registration-step3.component.html'
@@ -10,6 +11,7 @@ export class SupplierRegistrationStep3Component {
     @Output() private supplierChange: EventEmitter<SupplierDTOBase>;
     @Output() private onNext: EventEmitter<number>;
     @Output() private onBack: EventEmitter<number>;
+    @ViewChild('supplierForm') private supplierForm: NgForm;
     private confirmEmailField: string = '';
     private emailMatches: boolean = false;
     private emailPattern = new RegExp("(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])");
@@ -50,6 +52,31 @@ export class SupplierRegistrationStep3Component {
         && this.supplier.contactPhoneNumber != null && this.supplier.contactPhonePrefix != null
              && this.supplier.contactSurname.trim() != "" && this.supplier.contactName.trim() != "" && this.supplier.contactPhoneNumber.trim() != "" && this.supplier.contactPhonePrefix.trim() != ""){
                 this.buttonEnabled = true;
+        }
+
+         //custom name validator
+        if(this.supplier.contactName != null && this.supplier.contactName.trim() != ""){
+            setTimeout(()=>{this.supplierForm.controls['contactName'].setErrors(null);} ,5);
+        }else {
+            setTimeout(()=>{this.supplierForm.controls['contactName'].setErrors({'invalid': true});} ,5);
+        }
+         //custom surname validator
+        if(this.supplier.contactSurname != null && this.supplier.contactSurname.trim() != ""){
+            setTimeout(()=>{this.supplierForm.controls['contactSurname'].setErrors(null);} ,5);
+        }else {
+            setTimeout(()=>{this.supplierForm.controls['contactSurname'].setErrors({'invalid': true});} ,5);
+        }
+         //custom surname validator
+        if(this.supplier.contactPhoneNumber != null && this.supplier.contactPhoneNumber.trim() != ""){
+            setTimeout(()=>{this.supplierForm.controls['contactPhoneNumber'].setErrors(null);} ,5);
+        }else {
+            setTimeout(()=>{this.supplierForm.controls['contactPhoneNumber'].setErrors({'invalid': true});} ,5);
+        }
+         //custom surname validator
+         if(this.supplier.contactPhonePrefix != null && this.supplier.contactPhonePrefix.trim() != ""){
+            setTimeout(()=>{this.supplierForm.controls['contactPhonePrefix'].setErrors(null);} ,5);
+        }else {
+            setTimeout(()=>{this.supplierForm.controls['contactPhonePrefix'].setErrors({'invalid': true});} ,5);
         }
     }
 }
