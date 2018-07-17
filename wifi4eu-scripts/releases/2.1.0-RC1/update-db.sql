34d577733d409dc358fc48708651a880a433c208
sanchege <gsancheg@everis.com>
2018-07-17 16:20:27 +0200

    More sql for 2.1.0-RC1

--- wifi4eu-scripts/releases/2.1.0-RC1/update-db.sql
+++ wifi4eu-scripts/releases/2.1.0-RC1/update-db.sql
@@ -11,3 +11,8 @@ INSERT INTO [dbo].[registration_users]
           ,[_user])
    SELECT u.ecas_email, 1, r.id, 1, u.id from registrations r inner join users u on r._user = u.id where u.ecas_email is not null;
 ALTER TABLE [registrations] ALTER COLUMN _user INT null;
+
+update [dbo].[legal_files]
+set [upload_time] = (select dateadd(s, convert(bigint, r.upload_time) / 1000, convert(datetime, '1-1-1970 00:00:00')))
+from [dbo].[legal_files]  inner join
+[dbo].[registrations] r on r.id = registration;
