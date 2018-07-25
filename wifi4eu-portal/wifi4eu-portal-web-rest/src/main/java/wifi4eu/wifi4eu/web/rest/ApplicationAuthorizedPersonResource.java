4448fcb37b7e2c9eda446bc7c3c561a35c8f99f4
Lucía Vivero <lucia.vivero.ferreirua@everis.com>
2018-07-25 10:45:45 +0200

    add security on resources

--- wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/rest/ApplicationAuthorizedPersonResource.java
+++ wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/rest/ApplicationAuthorizedPersonResource.java
@@ -4,11 +4,17 @@ package wifi4eu.wifi4eu.web.rest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import wifi4eu.wifi4eu.common.dto.model.ApplicationAuthorizedPersonDTO;
+import wifi4eu.wifi4eu.common.dto.model.UserDTO;
 import wifi4eu.wifi4eu.common.dto.rest.ResponseDTO;
+import wifi4eu.wifi4eu.common.ecas.UserHolder;
+import wifi4eu.wifi4eu.common.security.UserContext;
 import wifi4eu.wifi4eu.service.application.ApplicationAuthorizedPersonService;
+import wifi4eu.wifi4eu.service.user.UserService;
 
 @CrossOrigin(origins = "*")
 @Controller
@@ -18,10 +24,18 @@ public class ApplicationAuthorizedPersonResource {
     @Autowired
     ApplicationAuthorizedPersonService applicationAuthorizedPersonService;
 
+    @Autowired
+    UserService userService;
+
     @ApiOperation(value = "Authorized")
     @RequestMapping(value = "/Authorized", method = RequestMethod.PUT, produces = "application/json")
     @ResponseBody
     public ResponseDTO editUser(@RequestBody ApplicationAuthorizedPersonDTO request) {
+        UserContext userContext = UserHolder.getUser();
+        UserDTO userConnected = userService.getUserByUserContext(userContext);
+        if (userConnected == null || userConnected.getType() == 1) {
+            throw new AccessDeniedException(HttpStatus.NOT_FOUND.getReasonPhrase());
+        }
         ResponseDTO response = new ResponseDTO();
         response.setSuccess(false);
         response.setData("Not implemented");
@@ -33,6 +47,11 @@ public class ApplicationAuthorizedPersonResource {
     @RequestMapping(value = "/Authorized", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public ApplicationAuthorizedPersonDTO findByApplicationAndUserId(@RequestParam("applicationId") final Integer applicationId, @RequestParam("userId") final Integer userId) {
+        UserContext userContext = UserHolder.getUser();
+        UserDTO userConnected = userService.getUserByUserContext(userContext);
+        if (userConnected == null || userConnected.getType() == 1) {
+            throw new AccessDeniedException(HttpStatus.NOT_FOUND.getReasonPhrase());
+        }
         return applicationAuthorizedPersonService.findByApplicationAndAuthorisedPerson(applicationId, userId);
     }
 
