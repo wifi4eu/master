0b703bbab4f86c761933646dbec381e87c069e83
Alex Perez Sahuquillo <aperesah@everis.com>
2018-07-26 12:29:16 +0200

    Clean useless files

--- wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/rest/AccessPointResource.java
+++ wifi4eu-portal/wifi4eu-portal-web-rest/src/main/java/wifi4eu/wifi4eu/web/rest/AccessPointResource.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import wifi4eu.wifi4eu.common.dto.rest.ResponseDTO;
 import wifi4eu.wifi4eu.entity.access_point.AccessPoint;
 import wifi4eu.wifi4eu.service.access_point.AccessPointService;
-import wifi4eu.wifi4eu.util.AzureTest;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.Response;
 import java.io.IOException;
 import java.util.Map;
 
@@ -23,22 +20,7 @@ public class AccessPointResource {
 
     @Autowired
     AccessPointService accessPointService;
-
-    @Autowired
-    AzureTest azureTest;
-
-
-    @ApiOperation(value = "Get supplier by specific user id")
-    @RequestMapping(value = "/azure", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public ResponseDTO testingIt() throws IOException {
-        azureTest.testingIt();
-        ResponseDTO responseDTO = new ResponseDTO();
-        responseDTO.setSuccess(true);
-        responseDTO.setData("");
-        return responseDTO;
-    }
-
+    
     @ApiOperation(value = "Get all Access Points per installation site ID")
     @RequestMapping(value = "/list", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
