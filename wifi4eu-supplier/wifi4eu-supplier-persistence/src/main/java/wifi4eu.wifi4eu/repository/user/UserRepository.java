84d8e569ca3edd2ecc3307e4423683e5d43e8ce3
David Santacana Rubireta <dsantaca@everis.com>
2018-07-27 15:50:09 +0200

    justified native queries

--- wifi4eu-supplier/wifi4eu-supplier-persistence/src/main/java/wifi4eu.wifi4eu/repository/user/UserRepository.java
+++ wifi4eu-supplier/wifi4eu-supplier-persistence/src/main/java/wifi4eu.wifi4eu/repository/user/UserRepository.java
@@ -10,6 +10,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByEcasUsername(String ecasUsername);
 
-    @Query(value = "select u.* from users u inner join registration_users ru on ru._user = u.id where ru.main = 1 and ru._user = ?#{[0]}", nativeQuery = true)
+    @Query(value = "select u.* from users u " +
+            "inner join registration_users ru on ru._user = u.id where ru.main = 1 and ru._user = ?#{[0]}", nativeQuery = true)
     User findMainUserFromRegistration(Integer registrationId);
 }
