eeefd5a8fce29ffc584481adb428b5f093adeb75
Dionysios Beaktoros <dionysios.bekatoros.sa@everis.com>
2018-07-30 12:02:29 +0200

    Removed old 'angular' project

--- wifi4eu-abac/src/main/angular/src/app/shared/components/failure/failure.component.ts
+++
@@ -1,5 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({selector: 'failure-component', templateUrl: 'failure.component.html'})
-export class FailureComponent {
-}
--- wifi4eu-abac/src/main/angular/src/app/shared/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { AppSearchInputComponent } from './layout/app-search-input/app-search-input.component';
-export { AppSearchInputComponent } from './layout/app-search-input/app-search-input.component';
-
-export var APP_DIRECTIVES = [
-    AppSearchInputComponent
-];
