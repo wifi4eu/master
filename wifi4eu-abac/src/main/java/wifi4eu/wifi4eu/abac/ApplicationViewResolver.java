b7f16629103c4d8487212215f8269bb7b781ef06
Dionysios Beaktoros <dionysios.bekatoros.sa@everis.com>
2018-07-30 17:05:19 +0200

    Fixed issue with /screen/ urls. Added paging to monitoring table. Limited width of pages content.

---
+++ wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/ApplicationViewResolver.java
@@ -0,0 +1,23 @@
+package wifi4eu.wifi4eu.abac;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan
+public class ApplicationViewResolver implements WebMvcConfigurer{
+	
+	
+	
+	@Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
