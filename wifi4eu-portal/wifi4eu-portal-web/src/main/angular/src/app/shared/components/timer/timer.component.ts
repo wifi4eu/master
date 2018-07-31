dda8638e5c6e0d5e1b1ebaaf9897572fd1d2bea4
Alex Perez Sahuquillo <aperesah@everis.com>
2018-07-31 20:01:14 +0200

    Timer fixed

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/shared/components/timer/timer.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/shared/components/timer/timer.component.ts
@@ -15,6 +15,7 @@ export class TimerComponent {
     private seconds: number;
     @Input('baseURLApi') baseURLApi : string;
     private timeGate: string = "/time";
+    private urlTime: string = "https://wifi4eu-dev-queue.everincloud.com";
 
     constructor(private http: Http, private sharedService: SharedService, private callApi: CallApi) {
     }
@@ -54,7 +55,8 @@ export class TimerComponent {
     }
 
     private getTime() {
-        let url = this.baseURLApi + this.timeGate;
+        // let url = this.baseURLApi + this.timeGate;
+        let url = this.urlTime + this.timeGate;
         this.http.get(url).subscribe(
             response => {
                 if (response.status == 200 && !isNaN(parseInt(response.text()))) {
