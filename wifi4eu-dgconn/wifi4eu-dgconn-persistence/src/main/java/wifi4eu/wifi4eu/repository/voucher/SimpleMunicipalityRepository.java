fe326192602ae24eb9f7069372d60b01295152aa
Alex Perez Sahuquillo <aperesah@everis.com>
2018-08-02 10:56:58 +0200

    Fix voucher simulation query

--- wifi4eu-dgconn/wifi4eu-dgconn-persistence/src/main/java/wifi4eu/wifi4eu/repository/voucher/SimpleMunicipalityRepository.java
+++ wifi4eu-dgconn/wifi4eu-dgconn-persistence/src/main/java/wifi4eu/wifi4eu/repository/voucher/SimpleMunicipalityRepository.java
@@ -12,6 +12,9 @@ public interface SimpleMunicipalityRepository extends CrudRepository<SimpleMunic
     @Query(value = "SELECT municipalities.id, municipalities.country, municipalities.lau, municipalities.name FROM municipalities INNER JOIN registrations ON municipalities.id = registrations.municipality INNER JOIN applications ON registrations.id = applications.registration WHERE municipalities.country IS NOT NULL AND municipalities.name IS NOT NULL", nativeQuery = true)
     List<SimpleMunicipality> findAllMunicipalitiesFromApplications();
 
-    @Query(value = "SELECT municipalities.id, municipalities.country, municipalities.lau, municipalities.name FROM municipalities INNER JOIN registrations ON municipalities.id = registrations.municipality INNER JOIN applications ON registrations.id = applications.registration WHERE municipalities.country IS NOT NULL AND municipalities.name IS NOT NULL AND municipalities.id = ?1", nativeQuery = true)
+    // @Query(value = "SELECT municipalities.id, municipalities.country, municipalities.lau, municipalities.name FROM municipalities INNER JOIN registrations ON municipalities.id = registrations.municipality INNER JOIN applications ON registrations.id = applications.registration WHERE municipalities.country IS NOT NULL AND municipalities.name IS NOT NULL AND municipalities.id = ?1", nativeQuery = true)
+    // SimpleMunicipality findMunicipalityById(Integer municipalityId);
+
+    @Query(value = "SELECT municipalities.id, municipalities.country, municipalities.lau, municipalities.name FROM municipalities WHERE municipalities.country IS NOT NULL AND municipalities.name IS NOT NULL AND municipalities.id = ?1", nativeQuery = true)
     SimpleMunicipality findMunicipalityById(Integer municipalityId);
 }
