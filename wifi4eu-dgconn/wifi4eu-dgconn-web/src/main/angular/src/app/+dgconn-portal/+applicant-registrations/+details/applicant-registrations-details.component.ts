8de7a8213129548288fa6e8b5ea2af2be50a50ee
jgimolin <javier.gil.molinero@everis.com>
2018-08-07 11:34:31 +0200

    WIFIFOREU-2840 Clean code

--- wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/src/app/+dgconn-portal/+applicant-registrations/+details/applicant-registrations-details.component.ts
+++ wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/src/app/+dgconn-portal/+applicant-registrations/+details/applicant-registrations-details.component.ts
@@ -288,7 +288,6 @@ export class DgConnApplicantRegistrationsDetailsComponent {
                     }
                 }
             );
-
         } else {
             this.loadingData = false;
         }
