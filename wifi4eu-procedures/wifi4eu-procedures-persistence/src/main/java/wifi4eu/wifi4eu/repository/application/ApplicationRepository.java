b88ebacc1d27069ccfb39ab645e886b5575b379c
Santiago Ramos Romeu <santiago.ramos.romeu@everis.com>
2018-08-07 08:37:14 +0200

    modified  notation of parameters for better consistency with the rest of the app

--- wifi4eu-procedures/wifi4eu-procedures-persistence/src/main/java/wifi4eu/wifi4eu/repository/application/ApplicationRepository.java
+++ wifi4eu-procedures/wifi4eu-procedures-persistence/src/main/java/wifi4eu/wifi4eu/repository/application/ApplicationRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface ApplicationRepository extends CrudRepository<Application,Integer> {
     // @Query(value = "SELECT distinct a.* FROM applications a JOIN registration_users ru ON a.registration = ru.registration JOIN users u ON ru._user = u.id WHERE u.ecas_email NOT IN (SELECT l.toAddress FROM log_emails l WHERE l.action = 'createApplication' and sent_date > ?2 ) AND a.date < ?1 order by date asc", nativeQuery = true)
     // List<Application> findByCreateApplicationEmailNotSent(long currentTime, long startOfCall);
-    @Query(value = "SELECT distinct a.*, r.municipality FROM applications a JOIN registrations r ON a.registration = r.id WHERE r.municipality NOT IN (SELECT l.municipalityId FROM log_emails l WHERE l.action = 'createApplication' and sent_date > ?3 ) AND a.date < ?2 AND call_id = ?1 ORDER BY date ASC", nativeQuery = true)
+    @Query(value = "SELECT distinct a.*, r.municipality FROM applications a JOIN registrations r ON a.registration = r.id WHERE r.municipality NOT IN (SELECT l.municipalityId FROM log_emails l WHERE l.action = 'createApplication' and sent_date > ?#{[2]} ) AND a.date < ?#{[1]} AND call_id = ?#{[0]} ORDER BY date ASC", nativeQuery = true)
     List<Application> findByCreateApplicationEmailNotSent(Integer callId, long currentTime, long startOfCall);
 }
