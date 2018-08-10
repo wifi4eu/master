610fc4f6de6bfee3f82954cb86d058df71a393fc
Franklin <franklinfk@gmail.com>
2018-08-10 17:56:14 +0200

    First commit

--- wifi4eu-apply/src/main/java/wifi4eu/wifi4eu/apply/App.java
+++ wifi4eu-apply/src/main/java/wifi4eu/wifi4eu/apply/App.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import java.io.IOException;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
@@ -16,22 +20,41 @@ public class App extends SpringBootServletInitializer implements CommandLineRunn
 	@Autowired
 	private Environment environment;
 
-
+	@Autowired
+	private MasterCommitter masterCommitter;
 
 	@Override
 	public void run(String... args) throws Exception {
 
 		Config.init(environment);
-
+		
+		this.masterCommitter.commit1();
+		
+/*
 		QueueConsumer conn = new QueueConsumer();
 
 		Thread thread = new Thread(conn);
 
 		thread.start();
+*/
 	}
 
 	public static void main(String[] args) throws IOException {
 
 		SpringApplication.run(App.class, args);
 	}
+
+
+//	@Bean
+//	public DataSource dataSource() {
+//	        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//	        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+//	        dataSourceBuilder.url("jdbc:sqlite:journal");
+//	        return dataSourceBuilder.build();   
+//	}
+
 }
+
+
+
+
