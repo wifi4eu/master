37269c21cd618f1cdc4938bc584232bb8e07608e
JORDI MAGRIÑA CORTES <jordi.magrina.cortes@everis.com>
2018-08-16 19:21:38 +0200

    Changed queue environment for acc

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/environments/environment.acc.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/environments/environment.acc.ts
@@ -6,6 +6,6 @@
 export const environment = {
     production: false,
     logoutUrl: "https://ecas.acceptance.ec.europa.eu/cas/logout",
-    applyVoucherUrl: "https://wifi4eu-dev-queue.everincloud.com",
+    applyVoucherUrl: "https://wifi4eu-acc-queue.everincloud.com",
     currentEnvironment: "acc"
 };
