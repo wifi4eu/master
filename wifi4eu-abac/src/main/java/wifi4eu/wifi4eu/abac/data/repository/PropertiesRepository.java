adeb8c7600edcb3ef5e7f3819ea82c3aa76e479a
Alexnadru ANDREI <Alexandru.ANDREI@ext.ec.europa.eu>
2018-08-23 17:35:02 +0200

    improve countersigned doc

--- wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/data/repository/PropertiesRepository.java
+++ wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/data/repository/PropertiesRepository.java
@@ -5,5 +5,5 @@ import wifi4eu.wifi4eu.abac.data.entity.Properties;
 
 public interface PropertiesRepository extends CrudRepository<Properties, Long> {
 
-    String findFirstByNameEquals(String key);
+    Properties findFirstByNameEquals(String key);
 }
--- wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/service/PropertiesService.java
+++ wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/service/PropertiesService.java
@@ -2,7 +2,10 @@ package wifi4eu.wifi4eu.abac.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import wifi4eu.wifi4eu.abac.data.entity.Properties;
 import wifi4eu.wifi4eu.abac.data.repository.PropertiesRepository;
+
+import java.util.Optional;
 
 @Service
 public class PropertiesService {
@@ -11,6 +14,7 @@ public class PropertiesService {
     private PropertiesRepository propertiesRepository;
 
     public String findPropertyByKey(String key){
-        return propertiesRepository.findFirstByNameEquals(key);
+        Optional<Properties> property = Optional.ofNullable(propertiesRepository.findFirstByNameEquals(key));
+        return property.orElse(new Properties()).getValue();
     }
 }
