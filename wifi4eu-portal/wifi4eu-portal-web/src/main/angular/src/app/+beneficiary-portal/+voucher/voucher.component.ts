a977cf6c058c1bd5882f3dea8676147068c4929d
David Santacana Rubireta <dsantaca@everis.com>
2018-09-03 15:57:27 +0200

    added new label on apply button when there's no call

--- wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+voucher/voucher.component.ts
+++ wifi4eu-portal/wifi4eu-portal-web/src/main/angular/src/app/+beneficiary-portal/+voucher/voucher.component.ts
@@ -11,8 +11,9 @@ import { SharedService } from "../../shared/shared.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Http, RequestOptions, Headers } from "@angular/http";
 import { ApplyVoucherBase } from '../../shared/swagger/model/ApplyVoucher'
-import { CookieService} from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service';
 import { environment } from '../../../environments/environment';
+import { ApplicationDTOBase } from '../../shared/swagger';
 
 @Component({
     templateUrl: 'voucher.component.html',
@@ -37,6 +38,8 @@ export class VoucherComponent {
     private endHour: string;
     private uploadDateTime = {};
     private uploadHourTime = {};
+    private applyDateTime = {};
+    private applyHourTime = {};
     private uploadDate: string[] = [];
     private uploadHour: string[] = [];
     private displayError = false;
@@ -48,6 +51,7 @@ export class VoucherComponent {
     private csrfTokenCookieName: string = "XSRF-TOKEN";
     private nameCookieApply: string = "hasRequested";
     private allRequestCompleted = false;
+    private application: ApplicationDTOBase;
 
     private httpOptions = {
         headers: new Headers({
@@ -62,9 +66,9 @@ export class VoucherComponent {
             this.callCustomApi.getCallForApply().subscribe(
                 (call: CallCustomBase) => {
                     this.currentCall = call;
-                    if (this.currentCall){
+                    if (this.currentCall) {
                         this.voucherCompetitionState = this.currentCall.voucherCompetitionState;
-                        if (this.voucherCompetitionState == 2){
+                        if (this.voucherCompetitionState == 2) {
                             this.openedCalls = "greyImage";
                         }
                         this.loadVoucherData();
@@ -80,40 +84,43 @@ export class VoucherComponent {
         }
     }
 
-    private isVoucherApplied(idRegistration:number){
-        if (this.cookieService.check(this.nameCookieApply+"_"+idRegistration)){
-            if (this.cookieService.get(this.nameCookieApply+"_"+idRegistration) == "true"){
+    private isVoucherApplied(idRegistration: number) {
+        if (this.cookieService.check(this.nameCookieApply + "_" + idRegistration)) {
+            if (this.cookieService.get(this.nameCookieApply + "_" + idRegistration) == "true") {
                 return true;
             }
         }
         return false;
     }
 
-    private loadVoucherDataWithoutCall(callId){
-      this.allRequestCompleted = false;
-      this.applyVoucherApi.getDataForApplyVoucherByUserIdAndCallId(this.user.id,callId)
-      .finally(() => this.allRequestCompleted = true)
-      .subscribe(
-        (applyVoucher: ApplyVoucherBase[]) => {
-            this.applyVouchersData = applyVoucher;
-            for (let i = 0; i < this.applyVouchersData.length; i++){
-                if (this.applyVouchersData[i].filesUploaded == 1){
-                    let uploaddate = new Date(this.applyVouchersData[i].uploadTime);
-                    this.uploadDateTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getUTCDate()).toString().slice(-2) + "/" + ('0' + (uploaddate.getMonth() + 1)).slice(-2) + "/" + uploaddate.getFullYear();
-                    this.uploadHourTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getHours()).toString().slice(-2) + ":" + ('0' + uploaddate.getMinutes()).slice(-2);
+    private loadVoucherDataWithoutCall(callId) {
+        this.allRequestCompleted = false;
+        this.applyVoucherApi.getDataForApplyVoucherByUserIdAndCallId(this.user.id, callId)
+            .finally(() => this.allRequestCompleted = true)
+            .subscribe(
+                (applyVoucher: ApplyVoucherBase[]) => {
+                    this.applyVouchersData = applyVoucher;
+                    for (let i = 0; i < this.applyVouchersData.length; i++) {
+                        if (this.applyVouchersData[i].filesUploaded == 1) {
+                            let uploaddate = new Date(this.applyVouchersData[i].uploadTime);
+                            let applydate = new Date(this.applyVouchersData[i].applyTime)
+                            this.uploadDateTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getUTCDate()).toString().slice(-2) + "/" + ('0' + (uploaddate.getMonth() + 1)).slice(-2) + "/" + uploaddate.getFullYear();
+                            this.uploadHourTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getHours()).toString().slice(-2) + ":" + ('0' + uploaddate.getMinutes()).slice(-2);
+                            this.applyDateTime[this.applyVouchersData[i].idMunicipality] = ('0' + applydate.getUTCDate()).toString().slice(-2) + "/" + ('0' + (applydate.getMonth() + 1)).slice(-2) + "/" + applydate.getFullYear();
+                            this.applyHourTime[this.applyVouchersData[i].idMunicipality] = ('0' + applydate.getHours()).toString().slice(-2) + ":" + ('0' + applydate.getMinutes()).slice(-2);                           
+                        }
+                    }
+                },
+                error => {
+                    this.sharedService.growlTranslation(
+                        "An error occurred while trying to retrieve the data from the server. Please, try again later.",
+                        "shared.error.api.generic",
+                        "error"
+                    );
                 }
-            }
-        },
-        error => {
-          this.sharedService.growlTranslation(
-            "An error occurred while trying to retrieve the data from the server. Please, try again later.",
-            "shared.error.api.generic",
-            "error"
-          );
-        }
-      );
+            );
     }
-    private loadVoucherData(){
+    private loadVoucherData() {
         this.allRequestCompleted = false;
         let startDateCall = new Date(this.currentCall.startDate);
         let endDateCall = new Date(this.currentCall.endDate);
@@ -121,27 +128,27 @@ export class VoucherComponent {
         this.startHour = ('0' + (startDateCall.getUTCHours() + 2)).slice(-2) + ":" + ('0' + startDateCall.getUTCMinutes()).slice(-2);
         this.endDate = ('0' + endDateCall.getUTCDate()).slice(-2) + "/" + ('0' + (endDateCall.getUTCMonth() + 1)).slice(-2) + "/" + endDateCall.getUTCFullYear();
         this.endHour = ('0' + (endDateCall.getUTCHours() + 2)).slice(-2) + ":" + ('0' + endDateCall.getUTCMinutes()).slice(-2);
-        this.applyVoucherApi.getDataForApplyVoucherByUserIdAndCallId(this.user.id,this.currentCall.id)
-        .finally(() => this.allRequestCompleted = true)
-        .subscribe(
-            (applyVoucher: ApplyVoucherBase[]) => {
-                this.applyVouchersData = applyVoucher;
-                for (let i = 0; i < this.applyVouchersData.length; i++){
-                    if (this.applyVouchersData[i].filesUploaded == 1){
-                        let uploaddate = new Date(this.applyVouchersData[i].uploadTime);
-                        this.uploadDateTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getUTCDate()).toString().slice(-2) + "/" + ('0' + (uploaddate.getMonth() + 1)).slice(-2) + "/" + uploaddate.getFullYear();
-                        this.uploadHourTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getHours()).toString().slice(-2) + ":" + ('0' + uploaddate.getMinutes()).slice(-2);
+        this.applyVoucherApi.getDataForApplyVoucherByUserIdAndCallId(this.user.id, this.currentCall.id)
+            .finally(() => this.allRequestCompleted = true)
+            .subscribe(
+                (applyVoucher: ApplyVoucherBase[]) => {
+                    this.applyVouchersData = applyVoucher;
+                    for (let i = 0; i < this.applyVouchersData.length; i++) {
+                        if (this.applyVouchersData[i].filesUploaded == 1) {
+                            let uploaddate = new Date(this.applyVouchersData[i].uploadTime);
+                            this.uploadDateTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getUTCDate()).toString().slice(-2) + "/" + ('0' + (uploaddate.getMonth() + 1)).slice(-2) + "/" + uploaddate.getFullYear();
+                            this.uploadHourTime[this.applyVouchersData[i].idMunicipality] = ('0' + uploaddate.getHours()).toString().slice(-2) + ":" + ('0' + uploaddate.getMinutes()).slice(-2);
+                        }
                     }
+                },
+                error => {
+                    this.sharedService.growlTranslation(
+                        "An error occurred while trying to retrieve the data from the server. Please, try again later.",
+                        "shared.error.api.generic",
+                        "error"
+                    );
                 }
-            },
-            error => {
-              this.sharedService.growlTranslation(
-                "An error occurred while trying to retrieve the data from the server. Please, try again later.",
-                "shared.error.api.generic",
-                "error"
-              );
-            }
-        );
+            );
     }
 
     private goToDocuments(municipalityId: number) {
@@ -153,16 +160,16 @@ export class VoucherComponent {
         // voucherCompetitionState is 2 then call is open
         // or when timer component emits that has finished
         if (this.voucherCompetitionState == 2) {
-            if (applyVoucher && applyVoucher.idRegistration && applyVoucher.idMunicipality && this.currentCall.id && this.user.id){
-                if (applyVoucher.conditionAgreement && applyVoucher.filesUploaded){
-                    let urlQueue = this.rabbitmqURI + this.currentCall.id+"/apply/"+applyVoucher.idRegistration+"/"+this.user.id+"/"+applyVoucher.idMunicipality;
+            if (applyVoucher && applyVoucher.idRegistration && applyVoucher.idMunicipality && this.currentCall.id && this.user.id) {
+                if (applyVoucher.conditionAgreement && applyVoucher.filesUploaded) {
+                    let urlQueue = this.rabbitmqURI + this.currentCall.id + "/apply/" + applyVoucher.idRegistration + "/" + this.user.id + "/" + applyVoucher.idMunicipality;
                     // put the following code to set the cookie and test the validation with true or false values
                     // this.cookieService.set(this.nameCookieApply, 'true');
                     let headers = new Headers();
-                    headers.append('X-XSRF-TOKEN', this.cookieService.get(this.csrfTokenCookieName)); 
-                    this.http.get(urlQueue, {headers: headers}).subscribe(
+                    headers.append('X-XSRF-TOKEN', this.cookieService.get(this.csrfTokenCookieName));
+                    this.http.get(urlQueue, { headers: headers }).subscribe(
                         data => {
-                            this.cookieService.set(this.nameCookieApply+"_"+applyVoucher.idRegistration,'true');
+                            this.cookieService.set(this.nameCookieApply + "_" + applyVoucher.idRegistration, 'true');
                             this.voucherApplied = "greyImage";
                             this.sharedService.growlTranslation(
                                 "Your request for voucher has been submitted successfully. Wifi4Eu will soon let you know if you got a voucher for free wi-fi.",
@@ -171,7 +178,7 @@ export class VoucherComponent {
                             );
                         },
                         error => {
-                            this.cookieService.set(this.nameCookieApply+"_"+applyVoucher.idRegistration,'false');
+                            this.cookieService.set(this.nameCookieApply + "_" + applyVoucher.idRegistration, 'false');
                             this.sharedService.growlTranslation(
                                 "An error occurred and your application could not be received.",
                                 "shared.registration.update.error",
@@ -184,7 +191,7 @@ export class VoucherComponent {
                         "Please accept the conditions agreement and upload all the required documents to proceed with the apply for voucher",
                         "shared.registration.update.erroragreement",
                         "error"
-                    ); 
+                    );
                 }
             } else {
                 this.sharedService.growlTranslation(
@@ -196,6 +203,12 @@ export class VoucherComponent {
         } else if (this.voucherCompetitionState == 1) {
             // trying to apply before the opening of the call
             this.displayCallClosed = true;
+        } else if (this.voucherCompetitionState == null || this.voucherCompetitionState == 0){
+            this.sharedService.growlTranslation(
+                "An error occurred and your application could not be received.",
+                "shared.registration.update.error",
+                "error"
+            );
         }
     }
 
@@ -214,10 +227,10 @@ export class VoucherComponent {
         conditionsAgreement.registrationId = applyVoucherData.idRegistration;
         applyVoucherData.conditionAgreement == 0 ? conditionsAgreement.status = 1 : conditionsAgreement.status = 0;
         this.conditionsAgreementApi.changeConditionsAgreementStatus(conditionsAgreement).subscribe(
-            (data : ResponseDTOBase) => {
+            (data: ResponseDTOBase) => {
                 if (data.success) {
-                    for (let i = 0; i < this.applyVouchersData.length; i++){
-                        if (applyVoucherData.idRegistration == this.applyVouchersData[i].idRegistration){
+                    for (let i = 0; i < this.applyVouchersData.length; i++) {
+                        if (applyVoucherData.idRegistration == this.applyVouchersData[i].idRegistration) {
                             this.applyVouchersData[i].conditionAgreement = data.data;
                             break;
                         }
