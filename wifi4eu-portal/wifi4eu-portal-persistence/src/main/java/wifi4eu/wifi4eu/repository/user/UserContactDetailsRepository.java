6275186b00d59e35a964ce62dfdd1afb49411cf3
Sara Henriques Mendes <sara.sofia.henriques.mendes@everis.com>
2018-09-05 17:25:41 +0200

    WIFIFOREU-3380 putting the query

--- wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/repository/user/UserContactDetailsRepository.java
+++ wifi4eu-portal/wifi4eu-portal-persistence/src/main/java/wifi4eu/wifi4eu/repository/user/UserContactDetailsRepository.java
@@ -7,7 +7,8 @@ import wifi4eu.wifi4eu.entity.user.UserContactDetails;
 import java.util.List;
 
 public interface UserContactDetailsRepository extends CrudRepository<UserContactDetails, Integer> {
-
+    //ADD CONTACT
+    //@Query(value = "select u.id as id, u.name, u.surname, u.address, u.address_num, u.postal_code, u.city, u.country, u.ecas_email as email, ru.main as main, u.type from users u inner join registration_users ru on ru._user = u.id where ru.registration = ?#{[0]}", nativeQuery = true)
     @Query(value = "select u.id as id, u.name, u.surname, u.address, u.address_num, u.postal_code, u.city, u.country, u.ecas_email as email, ru.main as main, u.type from users u inner join registration_users ru on ru._user = u.id where ru.registration = ?#{[0]} and ru.main=1", nativeQuery = true)
     List<UserContactDetails> findUsersContactDetailsByRegistrationId(Integer registrationId);
 
