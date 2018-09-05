80ad3234b04d5bd674a225fef3147e81e613f0c7
JORDI MAGRIÑA CORTES <jordi.magrina.cortes@everis.com>
2018-09-05 20:27:58 +0200

    Removed all the timeout and the websock operation

--- wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/src/app/app.component.ts
+++ wifi4eu-dgconn/wifi4eu-dgconn-web/src/main/angular/src/app/app.component.ts
@@ -91,7 +91,7 @@ export class AppComponent {
             .takeUntil(this.ngUnSubscribe)
             .subscribe(execution => {
                 // This will be called every 10 seconds until `stopCondition` flag is set to true
-                this.isSessionExpired();
+                //this.isSessionExpired();
             });
     }
 
