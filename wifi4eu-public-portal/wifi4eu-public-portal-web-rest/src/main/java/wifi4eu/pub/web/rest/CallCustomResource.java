bd088cf7815e8a88da971d7e850e2170f81a75c9
David Santacana Rubireta <dsantaca@everis.com>
2018-09-06 13:03:37 +0200

    changed call method to get the active or the closest call, timer reformated

---
+++ wifi4eu-public-portal/wifi4eu-public-portal-web-rest/src/main/java/wifi4eu/pub/web/rest/CallCustomResource.java
@@ -0,0 +1,40 @@
+package wifi4eu.pub.web.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import wifi4eu.wifi4eu.common.dto.rest.ResponseDTO;
+import wifi4eu.wifi4eu.entity.call.CallCustom;
+import wifi4eu.wifi4eu.service.call.CallCustomService;
+
+@CrossOrigin(origins = "*")
+@Controller
+@Api(value = "/callcustom", description = "Call object REST API services")
+@RequestMapping("callcustom")
+public class CallCustomResource {
+
+    @Autowired
+    CallCustomService callCustomService;
+
+    @ApiOperation(value = "Get the call to apply")
+    @RequestMapping(value = "/applyvoucher", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public CallCustom getCallForApply() {
+        return callCustomService.getCallForApply();
+    }
+
+    @ApiOperation(value = "Edit CallCustom")
+    @RequestMapping(value  = "/edit", method = RequestMethod.PUT, produces = "application/json")
+    @ResponseBody
+    public ResponseDTO editUser(@RequestBody CallCustom request){
+        ResponseDTO response = new ResponseDTO();
+        response.setSuccess(false);
+        response.setData("Not implemented");
+        return response;
+    }
+
+
+
+}
