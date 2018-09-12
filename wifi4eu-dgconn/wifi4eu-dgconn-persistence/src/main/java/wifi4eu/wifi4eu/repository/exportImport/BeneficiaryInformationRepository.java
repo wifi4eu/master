42f0619f3c78f8132769f140949a63da86dfdc93
tsitosi <Siarhei.TSITOU@ext.ec.europa.eu>
2018-09-12 11:57:12 +0200

    Fixes NPE and adds mun_abacName and mun_abacReference to the query

--- wifi4eu-dgconn/wifi4eu-dgconn-persistence/src/main/java/wifi4eu/wifi4eu/repository/exportImport/BeneficiaryInformationRepository.java
+++ wifi4eu-dgconn/wifi4eu-dgconn-persistence/src/main/java/wifi4eu/wifi4eu/repository/exportImport/BeneficiaryInformationRepository.java
@@ -11,14 +11,16 @@ public interface BeneficiaryInformationRepository extends CrudRepository<Benefic
 	@Query(value = "select r.id as id, m.id as mun_id, m.name as mun_name, concat(m.address, ', ', m.address_num) as mun_address, "
 			+ "m.postal_code as mun_postalCode, l.name2 as mun_city, l.country_code as mun_countryCodeISO, "
 			+ "ga.document_language as mun_languageCodeISO, r.id as mun_registrationNumber, ga.id as doc_portalId, "
-			+ "ga.date_signature as doc_date, ga.document_location as doc_location, 'GRANT AGREEMENT' as doc_type "
+			+ "ga.date_signature as doc_date, ga.document_location as doc_location, 'GRANT AGREEMENT' as doc_type, "
+			+ "ma.abacReference as mun_abacReference, ma.abacStandarName as mun_abacName "
 			+ "from applications as a "
 			+ "left join registrations as r on a.registration = r.id "
 			+ "left join municipalities as m on r.municipality = m.id "
+			+ "left join municipalities_abac as ma on ma.municipality = m.id "
 			+ "left join laus as l on m.lau = l.id "
 			+ "left join GRANT_AGREEMENT as ga on ga.application_id = a.id "
 			+ "where m.name is not null "
-			+ "and ga.date_signature is not null "
-			+ "and ga.date_counter_signature is null", nativeQuery = true)
+			/*+ "and ga.date_signature is not null "
+			+ "and ga.date_counter_signature is null"*/, nativeQuery = true)
 	List<BeneficiaryInformation> getBeneficiariesInformationSignedAndNotCounterSigned();
 }
