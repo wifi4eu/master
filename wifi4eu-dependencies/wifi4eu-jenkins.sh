b3e1c77c80f89e79cb819d0c86f0da993fb435ef
Victor Moral <victor.moral.guijo@everis.com>
2018-09-14 09:28:53 +0200

    Updated new ng path of the new Jenkins server

--- wifi4eu-dependencies/wifi4eu-jenkins.sh
+++ wifi4eu-dependencies/wifi4eu-jenkins.sh
@@ -2,7 +2,7 @@
 #echo "Install angular"
 #npm install -g @angular/cli@latest
 echo "Check angular version"
-/var/quark/binaries/node/node-v8.9.3-linux-x64/lib/node_modules/@angular/cli/bin/ng -v
+/usr/lib/node_modules/@angular/cli/bin/ng -v
 #echo "Config EC Dev Ops repo" - IT'S NOT NECESSARY: a proxy npm repository is configured (https://steps.everis.com/nexus/content/repositories/WIFIEU.Thirparty/)
 #npm config set "//ecdevops.eu/repository/npm-all/:_authToken=828d555c-441f-3ee2-8ec5-8fe715c389f2"
 #npm config set registry https://ecdevops.eu/repository/npm-all/
@@ -22,10 +22,10 @@ fi
 if [[ $2 = "" || $2 = *"wifi4eu-financial"* ]]; then
 npm install --prefix ./wifi4eu-financial/wifi4eu-financial-web/src/main/angular/
 fi
-echo "Install ecas-tomcat-8.0 dependency"
-mvn install:install-file -Dfile=wifi4eu-dependencies/ecas-tomcat-8.0-4.24.0-20180320.004350-3.jar  -DgroupId=eu.europa.ec.digit.iam.ecas.client -DartifactId=ecas-tomcat-8.0 -Dversion=4.24.0 -Dpackaging=jar
-echo "Install cns-client dependency"
-mvn install:install-file -Dfile=wifi4eu-dependencies/cns-client-1.1.jar  -DgroupId=eu.europa.ec.digit.cns -DartifactId=cns-client -Dversion=1.1 -Dpackaging=jar
+#echo "Install ecas-tomcat-8.0 dependency"
+#mvn install:install-file -Dfile=wifi4eu-dependencies/ecas-tomcat-8.0-4.24.0-20180320.004350-3.jar  -DgroupId=eu.europa.ec.digit.iam.ecas.client -DartifactId=ecas-tomcat-8.0 -Dversion=4.24.0 -Dpackaging=jar
+#echo "Install cns-client dependency"
+#mvn install:install-file -Dfile=wifi4eu-dependencies/cns-client-1.1.jar  -DgroupId=eu.europa.ec.digit.cns -DartifactId=cns-client -Dversion=1.1 -Dpackaging=jar
 echo "Compile project"
 echo "Environment: $1"
 echo "Modules: $2"
