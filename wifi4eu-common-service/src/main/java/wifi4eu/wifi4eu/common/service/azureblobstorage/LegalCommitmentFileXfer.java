0a69ca6c904abab3a5f5aba1c5fcb9401ddd7dce
Franklin Kaswiner <fkaswine@everis.com>
2018-09-18 11:28:07 +0200

    LegalCommimentXfer

---
+++ wifi4eu-common-service/src/main/java/wifi4eu/wifi4eu/common/service/azureblobstorage/LegalCommitmentFileXfer.java
@@ -0,0 +1,25 @@
+package wifi4eu.wifi4eu.common.service.azureblobstorage;
+
+import java.io.File;
+
+public class LegalCommitmentFileXfer {
+	public void prepareFile(LegalCommitmentData legalCommitmentData ) {
+		
+	}
+	
+	private File prepareBeneficiaryInformationFile() {
+		File file = null;
+		
+		return file;
+	}
+
+	private File prepareBeneficiaryDocumentsFile() {
+		File file = null;
+		
+		return file;
+	}
+	
+	public static class LegalCommitmentData {
+		
+	}
+}
