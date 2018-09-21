fbd49b1e2c590b7b93644a9149b5e7896aa46a07
FERREIRA DE OLIVEIRA Rafael <rafael.ferreira-de-oliveira@ext.ec.europa.eu>
2018-09-21 11:18:20 +0200

    removed unused method

--- wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/data/repository/LegalEntityRepository.java
+++ wifi4eu-abac/src/main/java/wifi4eu/wifi4eu/abac/data/repository/LegalEntityRepository.java
@@ -31,16 +31,6 @@ public interface LegalEntityRepository extends CrudRepository<LegalEntity, Integ
 	void updateFinancialLegalEntitiesStatuses();
 	
 	@Query(value =
-			"SELECT new wifi4eu.wifi4eu.abac.data.dto.MonitoringRow(le, bc, lc, lefdoc, lcdoc) " +
-			"FROM LegalEntity le " +
-			"LEFT JOIN BudgetaryCommitment bc on bc.legalEntity.id = le.id " +
-			"LEFT JOIN LegalCommitment lc on lc.legalEntity.id = le.id " +
-			"LEFT JOIN Document lefdoc on lefdoc.legalEntity.id = le.id and lefdoc.type='IDENTIFICATION_FORM' " + 
-			"LEFT JOIN Document lcdoc on lcdoc.legalEntity.id = le.id and lcdoc.type='COUNTERSIGNED_GRANT_AGREEMENT' "
-	)
-	List<MonitoringRow> findMonitoringData_old();
-	
-	@Query(value =
 			"SELECT new wifi4eu.wifi4eu.abac.data.dto.MonitoringRow(" +
 				"le.id, le.country, le.officialName, le.registrationNumber, le.wfStatus, le.abacFelId, " +
 				"bc.wfStatus, bc.commitmentLevel2Key, " +
