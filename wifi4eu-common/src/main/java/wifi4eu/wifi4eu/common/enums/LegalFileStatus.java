044b5fa16ae661820d8bffa1f3247e5e7aa16097
David Santacana Rubireta <dsantaca@everis.com>
2018-09-21 12:13:00 +0200

    WIP

--- wifi4eu-common/src/main/java/wifi4eu/wifi4eu/common/enums/LegalFileStatus.java
+++ wifi4eu-common/src/main/java/wifi4eu/wifi4eu/common/enums/LegalFileStatus.java
@@ -1,18 +1,4 @@
 package wifi4eu.wifi4eu.common.enums;
 
-public enum LegalFileStatus {
-    UNTOUCHED(0),
-    CORRECTION_REQUESTED(1),
-    VALID(2),
-    INVALID(3);
-
-    private int status;
-
-    LegalFileStatus(int status) {
-        this.status = status;
-    }
-
-    public int getValue() {
-        return this.status;
-    }
+public enum LegalFileNewStatus {
 }
