ac0740018c9ba00633f93a45ad638fa285fc9992
Lucia Vivero Ferreirua <lucia.vivero.ferreirua@everis.com>
2018-09-24 12:48:57 +0200

    add password to save preselection list functionality

--- wifi4eu-dgconn/wifi4eu-dgconn-service/src/main/java/wifi4eu/wifi4eu/service/security/INEAPermissionChecker.java
+++ wifi4eu-dgconn/wifi4eu-dgconn-service/src/main/java/wifi4eu/wifi4eu/service/security/INEAPermissionChecker.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 public class INEAPermissionChecker {
     public static final String freezePsswd = "wKrzhY75BkeMXQv5";
     public static final String notificationsPsswd = "dh3qTgye2eHPBQRL";
+    public static final String savePrelistPsswd = "HgQ*!Q*6GXd=7TVA";
 }
