e1e29fb892cb14ab3195a2c1356b6b0de0d8dec1
David Santacana Rubireta <dsantaca@everis.com>
2018-09-25 16:32:49 +0200

    Fixed a small bug with the sorting system on this column

--- wifi4eu-dgconn/wifi4eu-dgconn-service/src/main/java/wifi4eu/wifi4eu/service/application/ApplicationService.java
+++ wifi4eu-dgconn/wifi4eu-dgconn-service/src/main/java/wifi4eu/wifi4eu/service/application/ApplicationService.java
@@ -396,7 +396,7 @@ public class ApplicationService {
                 }
                 break;
                 */
-            case "supportingDocuments":
+            case "supportingdocuments":
                 if (pagingSortingData.getOrderType() == -1) {
                     if (name != null) {
                         if (name.trim().length() > 0) {
