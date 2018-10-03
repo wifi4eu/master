cc10a36bab59b1f63523255f5b64287d274f5969
Sara Henriques Mendes <sara.sofia.henriques.mendes@everis.com>
2018-10-03 13:13:40 +0200

    WIFIFOREU-3639 changes in the organizations

--- wifi4eu-scripts/src/database/laus_v1.5.15/laus_v1.5.15.sql
+++ wifi4eu-scripts/src/database/laus_v1.5.15/laus_v1.5.15.sql
@@ -4,8 +4,12 @@ update nuts set label = 'Border' where code = 'IE011';
 update nuts set label = 'Scottish Borders' where code= 'UKM24';
 
 
---THIS SCRIPT WAS RUN ON ACC on 02/10
 --select * from organizations where country='IE' and id=25
-update organizations set name='Dún Laoghaire – Rathdown' where country='IE' and id=25;
+update organizations set name='Dún Laoghaire - Rathdown' where country='IE' and id=25;
+--select * from organizations where country='UK' and id=89;
+update organizations set name='Combined Authority' where country='UK' and id=89;
+--select * from organizations where country='UK' and id=91
+update organizations set name='County Council' where country='UK' and id=91
+
 
 
